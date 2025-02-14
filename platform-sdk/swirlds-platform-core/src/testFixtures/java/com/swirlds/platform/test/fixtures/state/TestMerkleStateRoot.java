11c226409187655648b3f9c51e58a80a5bd29ee9
Ivan Malygin <ivan@swirldslabs.com>
2025-02-14 13:42:41 -0500

    Added separate CLASS_ID for TestMerkleStateRoot
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/state/TestMerkleStateRoot.java
+++ platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/state/TestMerkleStateRoot.java
@@ -21,6 +21,8 @@ import com.swirlds.state.merkle.MerkleStateRoot;
 
 public class TestMerkleStateRoot extends MerkleStateRoot<TestMerkleStateRoot> implements MerkeNodeState {
 
+    private static final long CLASS_ID = 0x8e300b0dfdafbb1cL;
+
     public TestMerkleStateRoot() {
         // empty constructor
     }
@@ -32,5 +34,10 @@ public class TestMerkleStateRoot extends MerkleStateRoot<TestMerkleStateRoot> im
     @Override
     protected TestMerkleStateRoot copyingConstructor() {
         return new TestMerkleStateRoot(this);
+    }
+
+    @Override
+    public long getClassId() {
+        return CLASS_ID;
     }
 }
