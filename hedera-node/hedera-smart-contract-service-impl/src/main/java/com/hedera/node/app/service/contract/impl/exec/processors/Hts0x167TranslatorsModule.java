5f9fad8a6ad86a112057f0109a1e79068d83338e
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2025-02-20 14:25:11 +0200

    fix: register translators
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/processors/Hts0x167TranslatorsModule.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/processors/Hts0x167TranslatorsModule.java
@@ -19,6 +19,7 @@ package com.hedera.node.app.service.contract.impl.exec.processors;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.common.CallTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCallAttempt;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokentype.address_0x167.TokenTypeTranslator;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.update.address_0x167.UpdateTranslator;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.IntoSet;
@@ -37,6 +38,15 @@ public interface Hts0x167TranslatorsModule {
     @IntoSet
     @Named("HtsTranslators")
     static CallTranslator<HtsCallAttempt> provideTokenTypeTranslator(@NonNull final TokenTypeTranslator translator) {
+        return translator;
+    }
+
+    @Provides
+    @Singleton
+    @IntoSet
+    @Named("HtsTranslators")
+    static CallTranslator<HtsCallAttempt> provideUpdateTokenCommonTranslator(
+            @NonNull final UpdateTranslator translator) {
         return translator;
     }
 }
