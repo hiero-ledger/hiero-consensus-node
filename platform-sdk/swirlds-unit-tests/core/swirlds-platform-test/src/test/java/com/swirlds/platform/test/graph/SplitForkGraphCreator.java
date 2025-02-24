c70b46f95acb09335cddd591c3ec3c23de790312
mxtartaglia <maxi@swirldslabs.com>
2025-02-24 15:24:17 -0300

    chore: remove coupling between SyncTests and SplitForkGraphCreator
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/graph/SplitForkGraphCreator.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/graph/SplitForkGraphCreator.java
@@ -7,7 +7,6 @@ import static com.swirlds.platform.test.graph.OtherParentMatrixFactory.createShu
 
 import com.swirlds.platform.system.address.AddressBook;
 import com.swirlds.platform.test.fixtures.event.emitter.StandardEventEmitter;
-import com.swirlds.platform.test.sync.SyncTestParams;
 import java.util.List;
 import java.util.Random;
 
@@ -15,35 +14,36 @@ import java.util.Random;
  * <p>This class manipulates the event generator to force the creation of a split fork, where each node has one branch
  * of a fork. Neither node knows that there is a fork until they sync.</p>
  *
- * <p>Graphs will have {@link SyncTestParams#getNumCommonEvents()} events that do not fork, then the
- * split fork occurs. Events generated after the {@link SyncTestParams#getNumCommonEvents()} will not select the creator
+ * <p>Graphs will have {@code numCommonEvents} events that do not fork, then the
+ * split fork occurs. Events generated after the {@code numCommonEvents} will not select the creator
  * with the split fork as an other parent to prevent more split forks from occurring. The creator with the split fork may
  * select any other creator's event as an other parent.</p>
  */
 public class SplitForkGraphCreator {
 
     public static void createSplitForkConditions(
-            final SyncTestParams params,
             final StandardEventEmitter generator,
             final int creatorToFork,
-            final int otherParent) {
+            final int otherParent,
+            final int numCommonEvents,
+            final int numNetworkNodes) {
 
-        forceNextCreator(params, generator, creatorToFork);
-        forceNextOtherParent(params, generator, creatorToFork, otherParent);
+        forceNextCreator(generator, creatorToFork, numCommonEvents);
+        forceNextOtherParent(generator, creatorToFork, otherParent, numCommonEvents, numNetworkNodes);
     }
 
     private static void forceNextCreator(
-            final SyncTestParams params, final StandardEventEmitter emitter, final int creatorToFork) {
+            final StandardEventEmitter emitter, final int creatorToFork, final int numCommonEvents) {
         final AddressBook addressBook = emitter.getGraphGenerator().getAddressBook();
         final int numberOfSources = addressBook.getSize();
         for (int i = 0; i < numberOfSources; i++) {
             final boolean sourceIsCreatorToFork = i == creatorToFork;
             emitter.getGraphGenerator().getSource(addressBook.getNodeId(i)).setNewEventWeight((r, index, prev) -> {
-                if (index < params.getNumCommonEvents()) {
+                if (index < numCommonEvents) {
                     return 1.0;
-                } else if (index == params.getNumCommonEvents() && sourceIsCreatorToFork) {
+                } else if (index == numCommonEvents && sourceIsCreatorToFork) {
                     return 1.0;
-                } else if (index > params.getNumCommonEvents()) {
+                } else if (index > numCommonEvents) {
                     return 1.0;
                 } else {
                     return 0.0;
@@ -53,14 +53,15 @@ public class SplitForkGraphCreator {
     }
 
     private static void forceNextOtherParent(
-            final SyncTestParams params,
             final StandardEventEmitter emitter,
             final int creatorToShun,
-            final int nextOtherParent) {
+            final int nextOtherParent,
+            final int numCommonEvents,
+            final int numNetworkNodes) {
 
         final int numSources = emitter.getGraphGenerator().getNumberOfSources();
 
-        final List<List<Double>> balancedMatrix = createBalancedOtherParentMatrix(params.getNumNetworkNodes());
+        final List<List<Double>> balancedMatrix = createBalancedOtherParentMatrix(numNetworkNodes);
 
         final List<List<Double>> forcedOtherParentMatrix = createForcedOtherParentMatrix(numSources, nextOtherParent);
 
@@ -69,10 +70,10 @@ public class SplitForkGraphCreator {
 
         emitter.getGraphGenerator()
                 .setOtherParentAffinity((Random r, long eventIndex, List<List<Double>> previousValue) -> {
-                    if (eventIndex < params.getNumCommonEvents()) {
+                    if (eventIndex < numCommonEvents) {
                         // Before the split fork, use the normal matrix
                         return balancedMatrix;
-                    } else if (eventIndex == params.getNumCommonEvents()) {
+                    } else if (eventIndex == numCommonEvents) {
                         // At the event to create the fork, force the other parent
                         return forcedOtherParentMatrix;
                     } else {
