b5b4890a584ef0e02ca95b21143ca361d8c53a13
mxtartaglia <maxi@swirldslabs.com>
2025-02-24 17:41:37 -0300

    chore: move sync tests to platform-core
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/sync/SyncFilteringTest.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/sync/SyncFilteringTest.java
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-package com.swirlds.platform.test.sync;
+package com.swirlds.platform.sync;
 
 import static com.swirlds.common.test.fixtures.RandomUtils.getRandomPrintSeed;
 import static org.junit.jupiter.api.Assertions.assertEquals;
