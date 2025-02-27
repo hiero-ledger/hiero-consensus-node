7ec7a8049379bc8c20a82273c488206384bf14ce
Tim Farber-Newman <tim.farber-newman@swirldslabs.com>
2025-02-27 14:06:05 -0600

    add JavaDoc; revert branch detector changes; only update parents birth round if they aren't ancient
    Signed-off-by: Tim Farber-Newman <tim.farber-newman@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
@@ -9,7 +9,6 @@ import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.suites.regression.system.LifecycleTest;
 import java.util.Map;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
 
@@ -17,7 +16,6 @@ import org.junit.jupiter.api.Tag;
 public class AncientAgeBirthRoundTest implements LifecycleTest {
 
     @HapiTest
-    @Disabled("Disabled due to expected failures while birth round ancient age is completed")
     final Stream<DynamicTest> upgradeToUseBirthRounds() {
         /*
         Note: This test should be run in subprocess mode. This is done by executing it with the 'testSubprocess' task:
