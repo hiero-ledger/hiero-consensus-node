96fb1508f60a8090da237b192b6842b60dd5b248
mxtartaglia <maxi@swirldslabs.com>
2025-02-28 19:36:30 -0300

    chore: reintroducing gui in test-fixtures
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- example-apps/swirlds-hashgraph-gui/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-module com.swirlds.hashgraph.gui {
-    requires com.swirlds.common.test.fixtures;
-    requires com.swirlds.common;
-    requires com.swirlds.platform.core.test.fixtures;
-    requires com.swirlds.platform.core;
-    requires com.github.spotbugs.annotations;
-}
