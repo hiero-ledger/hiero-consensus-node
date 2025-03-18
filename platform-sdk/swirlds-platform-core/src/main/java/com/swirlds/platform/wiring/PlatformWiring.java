4aef18e2962dfdbc3ec6a1b48e6c77cf1ff32138
Timo Brandstätter <timo@swirldslabs.com>
2025-03-18 19:20:13 +0100

    enable FEB
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/PlatformWiring.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/PlatformWiring.java
@@ -33,6 +33,7 @@ import com.swirlds.platform.components.consensus.ConsensusEngine;
 import com.swirlds.platform.components.transaction.system.ScopedSystemTransaction;
 import com.swirlds.platform.consensus.EventWindow;
 import com.swirlds.platform.event.AncientMode;
+import com.swirlds.platform.event.FutureEventBuffer;
 import com.swirlds.platform.event.PlatformEvent;
 import com.swirlds.platform.event.branching.BranchDetector;
 import com.swirlds.platform.event.branching.BranchReporter;
@@ -114,6 +115,7 @@ public class PlatformWiring {
     private final ComponentWiring<StateSigner, StateSignatureTransaction> stateSignerWiring;
     private final PcesReplayerWiring pcesReplayerWiring;
     private final ComponentWiring<InlinePcesWriter, PlatformEvent> pcesInlineWriterWiring;
+    private final ComponentWiring<FutureEventBuffer, PlatformEvent> futureEventBufferWiring;
     private final ComponentWiring<TransactionPrehandler, Queue<ScopedSystemTransaction<StateSignatureTransaction>>>
             applicationTransactionPrehandlerWiring;
     private final ComponentWiring<StateSignatureCollector, List<ReservedSignedState>> stateSignatureCollectorWiring;
@@ -223,6 +225,7 @@ public class PlatformWiring {
         pcesReplayerWiring = PcesReplayerWiring.create(model);
 
         pcesInlineWriterWiring = new ComponentWiring<>(model, InlinePcesWriter.class, config.pcesInlineWriter());
+        futureEventBufferWiring = new ComponentWiring<>(model, FutureEventBuffer.class, config.futureEventBuffer());
 
         eventWindowManagerWiring =
                 new ComponentWiring<>(model, EventWindowManager.class, DIRECT_THREADSAFE_CONFIGURATION);
@@ -287,7 +290,8 @@ public class PlatformWiring {
                 statusStateMachineWiring,
                 branchDetectorWiring,
                 branchReporterWiring,
-                pcesInlineWriterWiring);
+                pcesInlineWriterWiring,
+                futureEventBufferWiring);
 
         wire();
     }
@@ -323,6 +327,7 @@ public class PlatformWiring {
                 transactionResubmitterWiring.getInputWire(TransactionResubmitter::updateEventWindow));
         eventWindowOutputWire.solderTo(branchDetectorWiring.getInputWire(BranchDetector::updateEventWindow), INJECT);
         eventWindowOutputWire.solderTo(branchReporterWiring.getInputWire(BranchReporter::updateEventWindow), INJECT);
+        eventWindowOutputWire.solderTo(futureEventBufferWiring.getInputWire(FutureEventBuffer::updateEventWindow), INJECT);
     }
 
     /**
@@ -382,15 +387,17 @@ public class PlatformWiring {
         final OutputWire<PlatformEvent> splitOrphanBufferOutput = orphanBufferWiring.getSplitOutput();
 
         splitOrphanBufferOutput.solderTo(pcesInlineWriterWiring.getInputWire(InlinePcesWriter::writeEvent));
+
+        pcesInlineWriterWiring.getOutputWire().solderTo(futureEventBufferWiring.getInputWire(FutureEventBuffer::addEvent));
         // make sure that an event is persisted before being sent to consensus, this avoids the situation where we
         // reach consensus with events that might be lost due to a crash
-        pcesInlineWriterWiring.getOutputWire().solderTo(consensusEngineWiring.getInputWire(ConsensusEngine::addEvent));
+        futureEventBufferWiring.getOutputWire().solderTo(consensusEngineWiring.getInputWire(ConsensusEngine::addEvent));
         // make sure events are persisted before being gossipped, this prevents accidental branching in the case
         // where an event is created, gossipped, and then the node crashes before the event is persisted.
         // after restart, a node will not be aware of this event, so it can create a branch
-        pcesInlineWriterWiring.getOutputWire().solderTo(gossipWiring.getEventInput(), INJECT);
+        futureEventBufferWiring.getOutputWire().solderTo(gossipWiring.getEventInput(), INJECT);
         // avoid using events as parents before they are persisted
-        pcesInlineWriterWiring
+        futureEventBufferWiring
                 .getOutputWire()
                 .solderTo(eventCreationManagerWiring.getInputWire(EventCreationManager::registerEvent));
 
