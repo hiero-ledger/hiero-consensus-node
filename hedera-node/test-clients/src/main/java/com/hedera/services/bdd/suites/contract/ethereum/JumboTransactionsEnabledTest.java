afe383b938c75bf9132ab73f90567482df19c284
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-03-25 13:41:21 +0200

    Fix tests
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/ethereum/JumboTransactionsEnabledTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/ethereum/JumboTransactionsEnabledTest.java
@@ -46,11 +46,7 @@ public class JumboTransactionsEnabledTest implements LifecycleTest {
     @BeforeAll
     static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
         testLifecycle.doAdhoc(
-                cryptoCreate(RELAYER).balance(ONE_MILLION_HBARS),
-                uploadInitCode(CONTRACT),
-                contractCreate(CONTRACT),
-                uploadInitCode(CONTRACT),
-                contractCreate(CONTRACT));
+                cryptoCreate(RELAYER).balance(ONE_MILLION_HBARS), uploadInitCode(CONTRACT), contractCreate(CONTRACT));
     }
 
     @HapiTest
