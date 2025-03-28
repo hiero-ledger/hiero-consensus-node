6ba0d39a77299954c163667eee9c4ba12dbffa48
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-03-28 19:44:57 +0100

    Fixed unit test
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/utility/ByteUtils.java
+++ platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/utility/ByteUtils.java
@@ -153,7 +153,7 @@ public final class ByteUtils {
                 if (index >= data.length) {
                     break;
                 }
-                result += (byte) ((data[index] & 0xff) << (8 * (1 - offset)));
+                result += (data[index] & 0xff) << (8 * (1 - offset));
             }
             return result;
         }
