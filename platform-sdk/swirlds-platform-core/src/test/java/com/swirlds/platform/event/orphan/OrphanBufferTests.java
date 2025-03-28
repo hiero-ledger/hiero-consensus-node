d1089f8f0a87a7e4e839a76e6cfb77833703e42f
Lazar Petrovic <lpetrovic05@gmail.com>
2025-03-28 14:54:28 +0100

    s
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/orphan/OrphanBufferTests.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/orphan/OrphanBufferTests.java
@@ -242,14 +242,10 @@ class OrphanBufferTests {
             latestConsensusRound += maybeAdvanceRound.apply(random);
             final AncientMode ancientMode =
                     useBirthRoundForAncient ? AncientMode.BIRTH_ROUND_THRESHOLD : AncientMode.GENERATION_THRESHOLD;
-            final long ancientThreshold = useBirthRoundForAncient
-                    ? Math.max(1, latestConsensusRound - 26 + 1)
-                    : minimumGenerationNonAncient;
+            final long ancientThreshold =
+                    useBirthRoundForAncient ? Math.max(1, latestConsensusRound - 26 + 1) : minimumGenerationNonAncient;
             final EventWindow eventWindow = new EventWindow(
-                    latestConsensusRound,
-                    ancientThreshold,
-                    1 /* ignored in this context */,
-                    ancientMode);
+                    latestConsensusRound, ancientThreshold, 1 /* ignored in this context */, ancientMode);
             unorphanedEvents.addAll(orphanBuffer.setEventWindow(eventWindow));
 
             for (final PlatformEvent unorphanedEvent : unorphanedEvents) {
