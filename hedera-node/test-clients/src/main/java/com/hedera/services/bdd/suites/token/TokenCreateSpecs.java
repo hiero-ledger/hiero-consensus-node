30b601f46abbda6e0708e416b5de778864113a1c
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2025-04-01 10:42:16 +0300

    test: add hapi tests
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/token/TokenCreateSpecs.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/token/TokenCreateSpecs.java
@@ -22,6 +22,7 @@ import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoDelete;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenAssociate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenDelete;
 import static com.hedera.services.bdd.spec.transactions.token.CustomFeeSpecs.fixedHbarFee;
 import static com.hedera.services.bdd.spec.transactions.token.CustomFeeSpecs.fixedHbarFeeInheritingRoyaltyCollector;
 import static com.hedera.services.bdd.spec.transactions.token.CustomFeeSpecs.fixedHtsFee;
@@ -79,6 +80,7 @@ import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_HAS_NO_S
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_NAME_TOO_LONG;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_NOT_ASSOCIATED_TO_FEE_COLLECTOR;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_SYMBOL_TOO_LONG;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_WAS_DELETED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TRANSACTION_HAS_UNKNOWN_FIELDS;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TRANSFERS_NOT_ZERO_SUM_FOR_TOKEN;
 import static com.hederahashgraph.api.proto.java.TokenType.FUNGIBLE_COMMON;
@@ -127,6 +129,8 @@ public class TokenCreateSpecs {
     private static final String B_TOKEN = "TokenB";
     private static final String FIRST_USER = "Client1";
     private static final String SENTINEL_VALUE = "0.0.0";
+
+    private static final long ONE_MONTH_IN_SECONDS = 2592000;
 
     @HapiTest
     final Stream<DynamicTest> getInfoIdVariantsTreatedAsExpected() {
@@ -1034,6 +1038,37 @@ public class TokenCreateSpecs {
                         .initialSupply(1000L)
                         .withCustom(fixedHbarFee(1, account))
                         .hasKnownStatus(INVALID_CUSTOM_FEE_COLLECTOR));
+    }
+
+    @HapiTest
+    final Stream<DynamicTest> autoRenewLessThenAMonth() {
+        return hapiTest(
+                cryptoCreate(AUTO_RENEW_ACCOUNT).balance(0L),
+                tokenCreate("token")
+                        .autoRenewAccount(AUTO_RENEW_ACCOUNT)
+                        .autoRenewPeriod(ONE_MONTH_IN_SECONDS - 1)
+                        .hasKnownStatus(INVALID_RENEWAL_PERIOD));
+    }
+
+    @HapiTest
+    final Stream<DynamicTest> createWithDeletedFixedToken() {
+        var token = "token";
+        var token2 = "token2";
+
+        return hapiTest(
+                newKeyNamed(ADMIN_KEY),
+                cryptoCreate(TOKEN_TREASURY),
+                newKeyNamed("tokenAdmin"),
+                tokenCreate(token)
+                        .tokenType(FUNGIBLE_COMMON)
+                        .supplyType(TokenSupplyType.INFINITE)
+                        .adminKey(ADMIN_KEY)
+                        .initialSupply(0)
+                        .treasury(TOKEN_TREASURY),
+                tokenDelete(token),
+                tokenCreate(token2)
+                        .withCustom(fixedHtsFee(1, token, TOKEN_TREASURY))
+                        .hasKnownStatus(TOKEN_WAS_DELETED));
     }
 
     private final long hbarAmount = 1_234L;
