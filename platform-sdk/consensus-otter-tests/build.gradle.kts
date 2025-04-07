c1fe70ba2be7ba36ec5a2d8ee7ffb050e97f050b
Timo Brandstätter <timo@swirldslabs.com>
2025-04-07 14:44:03 +0200

    change to build.gradle.kts
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/build.gradle.kts
+++ platform-sdk/consensus-otter-tests/build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     id("org.hiero.gradle.feature.test-fixtures")
 }
 
+testModuleInfo {
+    requires("com.swirlds.logging")
+    requires("org.hiero.otter.fixtures")
+}
+
 description = "Consensus Otter Tests"
--- platform-sdk/consensus-otter-tests/src/test/java/module-info.java
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-module hedera.services.consensus.otter.test {
-    requires com.swirlds.logging;
-    requires org.hiero.otter.fixtures;
-}
