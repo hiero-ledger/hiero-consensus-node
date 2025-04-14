4a9277659adcf3997fe00be9c03942f41a10ae24
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-04-14 23:29:01 +0200

    Spotless
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/state/iss/DefaultIssDetector.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/state/iss/DefaultIssDetector.java
@@ -569,10 +569,8 @@ public class DefaultIssDetector implements IssDetector {
             hashFinder.writePartitionData(sb);
             writeSkippedLogCount(sb, skipCount);
 
-            final String mnemonic = selfHash == null? "null" : Mnemonics.generateMnemonic(selfHash);
-            logger.fatal(
-                    EXCEPTION.getMarker(),
-                    new IssPayload(sb.toString(), round, mnemonic, "", true));
+            final String mnemonic = selfHash == null ? "null" : Mnemonics.generateMnemonic(selfHash);
+            logger.fatal(EXCEPTION.getMarker(), new IssPayload(sb.toString(), round, mnemonic, "", true));
         }
     }
 
