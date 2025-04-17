fe337cedf3b4d1927a12ee9cda6df17a6a2eb42a
Timo Brandstätter <timo@swirldslabs.com>
2025-04-17 23:53:24 +0200

    add javadoc
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/validator/LogMarkerFilter.java
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/validator/LogMarkerFilter.java
@@ -7,8 +7,32 @@ import org.apache.logging.log4j.Marker;
 import org.hiero.otter.fixtures.Validator.LogFilter;
 import org.hiero.otter.fixtures.logging.StructuredLog;
 
+/**
+ * A {@link LogFilter} that filters log messages based on their {@link Marker}.
+ *
+ * <p>This filter allows inclusion or exclusion of log messages depending on whether
+ * their marker is present in a given set of markers.
+ *
+ * @param include if {@code true}, messages whose marker is <b>not</b> in the set are filtered out;
+ *                if {@code false}, messages whose marker <b>is</b> in the set are filtered out
+ * @param markers the set of markers used for filtering
+ */
 public record LogMarkerFilter(boolean include, @NonNull Set<Marker> markers) implements LogFilter {
 
+    /**
+     * Determines whether the given log message should be filtered based on its marker.
+     *
+     * <ul>
+     *   <li>If {@code include} is {@code true}, this returns {@code true} (filter out)
+     *       when the message's marker is <b>not</b> in the {@code markers} set.</li>
+     *   <li>If {@code include} is {@code false}, this returns {@code true} when the message's
+     *       marker <b>is</b> in the {@code markers} set.</li>
+     * </ul>
+     *
+     * @param logMsg the structured log message to evaluate
+     *
+     * @return {@code true} if the message should be filtered out, {@code false} otherwise
+     */
     @Override
     public boolean filter(@NonNull final StructuredLog logMsg) {
         return include != markers.contains(logMsg.marker());
--- platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/validator/LogNodeFilter.java
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/validator/LogNodeFilter.java
@@ -6,7 +6,32 @@ import java.util.Set;
 import org.hiero.otter.fixtures.Validator.LogFilter;
 import org.hiero.otter.fixtures.logging.StructuredLog;
 
+/**
+ * A {@link LogFilter} that filters log messages based on their {@code selfId}.
+ *
+ * <p>This filter allows inclusion or exclusion of log messages depending on whether
+ * their {@code nodeId} is present in a given set of node IDs.
+ *
+ * @param include if {@code true}, messages whose node ID is <b>not</b> in the set are filtered out;
+ *                if {@code false}, messages whose node ID <b>is</b> in the set are filtered out
+ * @param nodeIds the set of node IDs used for filtering
+ */
 public record LogNodeFilter(boolean include, @NonNull Set<Long> nodeIds) implements LogFilter {
+
+    /**
+     * Determines whether the given log message should be filtered based on its node ID.
+     *
+     * <ul>
+     *   <li>If {@code include} is {@code true}, this returns {@code true} (filter out)
+     *       when the message's node ID is <b>not</b> in the {@code nodeIds} set.</li>
+     *   <li>If {@code include} is {@code false}, this returns {@code true} when the message's
+     *       node ID <b>is</b> in the {@code nodeIds} set.</li>
+     * </ul>
+     *
+     * @param logMsg the structured log message to evaluate
+     *
+     * @return {@code true} if the message should be filtered out, {@code false} otherwise
+     */
     @Override
     public boolean filter(@NonNull final StructuredLog logMsg) {
         return include != nodeIds.contains(logMsg.nodeId());
