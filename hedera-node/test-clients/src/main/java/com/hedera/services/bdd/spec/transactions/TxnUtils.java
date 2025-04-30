f84aa161e61ef4c2d57c25c7385f45820251e06d
ibankov <ivan.bankov@limechain.tech>
2025-04-30 13:04:17 +0300

    fix asContractId()
    Signed-off-by: ibankov <ivan.bankov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnUtils.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnUtils.java
@@ -334,7 +334,13 @@ public class TxnUtils {
                     .setEvmAddress(ByteString.copyFrom(CommonUtils.unhex(effS)))
                     .build();
         }
-        return isIdLiteral(s) ? asContract(s) : lookupSpec.registry().getContractId(s);
+        if (isNumericLiteral(s)) {
+            return asContract(lookupSpec.shard(), lookupSpec.realm(), Long.parseLong(s));
+        }
+        if (isIdLiteral(s)) {
+            return asContract(s);
+        }
+        return lookupSpec.registry().getContractId(s);
     }
 
     public static String txnToString(final Transaction txn) {
