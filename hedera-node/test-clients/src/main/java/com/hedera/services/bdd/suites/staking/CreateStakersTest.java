987032beb849320272d9f8d14a4414c664e36a4d
Michael Tinker <michael.tinker@swirldslabs.com>
2025-05-05 11:29:08 -0500

    Add rebalanceStakes() op
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/staking/CreateStakersTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/staking/CreateStakersTest.java
@@ -4,11 +4,18 @@ package com.hedera.services.bdd.suites.staking;
 import static com.hedera.services.bdd.junit.TestTags.CRYPTO;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.dsl.operations.transactions.TouchBalancesOperation.touchBalanceOf;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoUpdate;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.doingContextual;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.ensureStakingActivated;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.inParallel;
+import static com.hedera.services.bdd.suites.HapiSuite.GENESIS;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_BILLION_HBARS;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_MILLION_HBARS;
+import static java.util.stream.Collectors.toSet;
 
 import com.hedera.services.bdd.junit.HapiTest;
+import com.hedera.services.bdd.spec.HapiSpec;
+import com.hedera.services.bdd.spec.SpecOperation;
 import com.hedera.services.bdd.spec.dsl.annotations.Account;
 import com.hedera.services.bdd.spec.dsl.entities.SpecAccount;
 import java.util.stream.Stream;
@@ -34,6 +41,15 @@ public class CreateStakersTest {
     @HapiTest
     final Stream<DynamicTest> createStakers() {
         return hapiTest(
-                ensureStakingActivated(), touchBalanceOf(NODE0_STAKER, NODE1_STAKER, NODE2_STAKER, NODE3_STAKER));
+                ensureStakingActivated(),
+                touchBalanceOf(NODE0_STAKER, NODE1_STAKER, NODE2_STAKER, NODE3_STAKER),
+                // Give all stakers the 0.0.2 key to easily transfer their balances later
+                inParallel(Stream.of(NODE0_STAKER, NODE1_STAKER, NODE2_STAKER, NODE3_STAKER)
+                        .map(staker -> cryptoUpdate(staker.name()).key(GENESIS))
+                        .toArray(SpecOperation[]::new)),
+                doingContextual(
+                        spec -> HapiSpec.setStakerIds(Stream.of(NODE0_STAKER, NODE1_STAKER, NODE2_STAKER, NODE3_STAKER)
+                                .map(staker -> spec.registry().getAccountID(staker.name()))
+                                .collect(toSet()))));
     }
 }
