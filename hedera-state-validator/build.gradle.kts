e276940c42d00b3a32ff1b70bdaaa6836f62b758
Artur Kugal <artur.kugal@swirldslabs.com>
2025-05-13 20:29:39 -0400

    Move to java module
    Signed-off-by: Artur Kugal <artur.kugal@swirldslabs.com>

--- hedera-state-validator/build.gradle.kts
+++ hedera-state-validator/build.gradle.kts
@@ -4,53 +4,6 @@ plugins {
     id("org.hiero.gradle.feature.shadow")
 }
 
-dependencies {
-    implementation("info.picocli:picocli:4.7.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
-    runtimeOnly("org.apache.logging.log4j:log4j-core:2.17.2")
-
-    // used to write json report
-    implementation("com.google.code.gson:gson:2.10")
-}
-
-mainModuleInfo {
-    requires("com.hedera.node.app")
-    requires("com.hedera.node.app.test.fixtures")
-
-    requires("com.fasterxml.jackson.databind")
-    requires("com.hedera.node.app.hapi.utils")
-    requires("com.hedera.node.app.service.addressbook.impl")
-    requires("com.hedera.node.app.service.consensus.impl")
-    requires("com.hedera.node.app.service.contract.impl")
-    requires("com.hedera.node.app.service.file.impl")
-    requires("com.hedera.node.app.service.network.admin.impl")
-    requires("com.hedera.node.app.service.schedule.impl")
-    requires("com.hedera.node.app.service.token.impl")
-    requires("com.hedera.node.app.service.util.impl")
-    requires("com.hedera.node.app.spi")
-    requires("com.hedera.node.app.spi.test.fixtures")
-    requires("com.hedera.node.config")
-    requires("com.hedera.node.hapi")
-    requires("com.hedera.pbj.runtime")
-    requires("com.swirlds.base")
-    requires("com.swirlds.common")
-    requires("com.swirlds.config.api")
-    requires("com.swirlds.config.extensions")
-    requires("com.swirlds.metrics.api")
-    requires("com.swirlds.platform.core")
-    requires("com.swirlds.state.api")
-    requires("com.swirlds.state.impl")
-    requires("com.swirlds.virtualmap")
-    requires("com.swirlds.merkledb")
-
-    // Define the individual libraries
-    // JUnit Bundle
-    requires("org.junit.jupiter.api")
-    requires("org.junit.platform.launcher")
-    requires("org.junit.platform.commons")
-    requires("org.junit.platform.engine")
-    requires("org.junit.jupiter.params")
-    runtimeOnly("org.junit.jupiter.engine")
-}
+mainModuleInfo { runtimeOnly("org.junit.jupiter.engine") }
 
 application.mainClass = "com.hedera.statevalidation.StateOperatorCommand"
--- hedera-state-validator/src/main/java/com/hedera/statevalidation/validators/merkledb/StateAnalyzer.java
+++ hedera-state-validator/src/main/java/com/hedera/statevalidation/validators/merkledb/StateAnalyzer.java
@@ -38,7 +38,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -102,8 +101,6 @@ public class StateAnalyzer {
         vmReportUpdater.accept(vmReport, storageReport);
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @NotNull
     private static StorageReport createStoreReport(DataFileCollection dfc, Function<ReadableSequentialData, ?> deser) {
         LongList itemCountByPath = new LongListHeap(50_000_000, CONFIGURATION);
         List<DataFileReader> readers = dfc.getAllCompletedFiles();
