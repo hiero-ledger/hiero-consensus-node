5f112326babba0572f83c4811879307d9007de2c
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-05-16 17:15:08 +0200

    Assert if maximum birth round in PcesFile is limited
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/assertions/SingleNodeFilesResultAssert.java
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/assertions/SingleNodeFilesResultAssert.java
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.otter.fixtures.assertions;
 
+import static org.assertj.core.api.Assertions.fail;
+
+import com.swirlds.common.io.IOIterator;
 import edu.umd.cs.findbugs.annotations.Nullable;
+import java.io.IOException;
 import org.assertj.core.api.AbstractAssert;
+import org.hiero.consensus.model.event.PlatformEvent;
 import org.hiero.otter.fixtures.result.SingleNodeFilesResult;
 
 /**
@@ -10,6 +15,7 @@ import org.hiero.otter.fixtures.result.SingleNodeFilesResult;
  *
  * <p>Provides custom assertions for validating log results of a single node.
  */
+@SuppressWarnings("UnusedReturnValue")
 public class SingleNodeFilesResultAssert extends AbstractAssert<SingleNodeFilesResultAssert, SingleNodeFilesResult> {
 
     protected SingleNodeFilesResultAssert(@Nullable final SingleNodeFilesResult actual) {
@@ -24,5 +30,23 @@ public class SingleNodeFilesResultAssert extends AbstractAssert<SingleNodeFilesR
      */
     public static SingleNodeFilesResultAssert assertThat(@Nullable SingleNodeFilesResult actual) {
         return new SingleNodeFilesResultAssert(actual);
+    }
+
+    public SingleNodeFilesResultAssert hasBirthRoundsInPcesFilesLessThan(long expected) {
+        isNotNull();
+
+        long maxBirthRound = 0L;
+        try {
+            for (final IOIterator<PlatformEvent> it = actual.pcesEvents(); it.hasNext(); ) {
+                final PlatformEvent event = it.next();
+                maxBirthRound = Math.max(maxBirthRound, event.getBirthRound());
+            }
+            if (maxBirthRound > expected) {
+                fail("Expected maximum birth round in PcesFiles to be less than <%d> but was <%d>", expected, maxBirthRound);
+            }
+        } catch (IOException e) {
+            fail("Unexpected IOException while evaluating PcesFiles", e);
+        }
+        return this;
     }
 }
