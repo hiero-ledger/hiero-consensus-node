8d0a7ced88dee07583eb407e5f2623118fc5cb78
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2025-05-27 14:05:25 +0300

    fix: try with ordered in isolation
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/misc/BesuNativeLibVerificationTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/misc/BesuNativeLibVerificationTest.java
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.services.bdd.suites.misc;
 
-import static com.hedera.services.bdd.junit.TestTags.ISS;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.utilops.FakeNmt.restartNetwork;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.blockingOrder;
@@ -16,6 +15,7 @@ import static org.hiero.consensus.model.status.PlatformStatus.ACTIVE;
 import static org.hiero.consensus.model.status.PlatformStatus.STARTING_UP;
 
 import com.hedera.services.bdd.junit.HapiTest;
+import com.hedera.services.bdd.junit.OrderedInIsolation;
 import com.hedera.services.bdd.junit.hedera.NodeSelector;
 import com.hedera.services.bdd.suites.regression.system.LifecycleTest;
 import java.util.Map;
@@ -23,11 +23,10 @@ import java.util.stream.Stream;
 import org.hyperledger.besu.crypto.Blake2bfMessageDigest.Blake2bfDigest;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Tag;
 
-@Tag(ISS)
 // Order to be last as it will restart the network and halt if the lib is not present
 @Order(Integer.MAX_VALUE)
+@OrderedInIsolation
 public class BesuNativeLibVerificationTest implements LifecycleTest {
 
     @HapiTest
