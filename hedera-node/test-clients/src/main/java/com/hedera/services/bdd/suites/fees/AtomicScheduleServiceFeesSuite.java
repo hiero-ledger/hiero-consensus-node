783d5eca4563cf273f49c7e9ef7eebc646531a1e
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2025-06-03 11:36:22 +0300

    refactor: extracting, creating constants, and clean-up
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/AtomicScheduleServiceFeesSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/AtomicScheduleServiceFeesSuite.java
@@ -16,6 +16,7 @@ import static com.hedera.services.bdd.spec.transactions.crypto.HapiCryptoTransfe
 import static com.hedera.services.bdd.spec.utilops.EmbeddedVerbs.handleAnyRepeatableQueryPayment;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.validateInnerTxnChargedUsd;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HBAR;
+import static com.hedera.services.bdd.suites.HapiSuite.flattened;
 import static com.hedera.services.bdd.suites.schedule.ScheduleUtils.OTHER_PAYER;
 import static com.hedera.services.bdd.suites.schedule.ScheduleUtils.PAYING_SENDER;
 import static com.hedera.services.bdd.suites.schedule.ScheduleUtils.RECEIVER;
@@ -24,6 +25,7 @@ import static com.hedera.services.bdd.suites.schedule.ScheduleUtils.SIMPLE_UPDAT
 import com.hedera.services.bdd.junit.HapiTestLifecycle;
 import com.hedera.services.bdd.junit.LeakyRepeatableHapiTest;
 import com.hedera.services.bdd.junit.support.TestLifecycle;
+import com.hedera.services.bdd.spec.HapiSpecOperation;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.math.BigInteger;
 import java.util.Map;
@@ -39,6 +41,8 @@ public class AtomicScheduleServiceFeesSuite {
     private static final double BASE_FEE_SCHEDULE_SIGN = 0.001;
     private static final double BASE_FEE_SCHEDULE_DELETE = 0.001;
     private static final double BASE_FEE_CONTRACT_CALL = 0.1;
+    private static final String BATCH_OPERATOR = "batchOperator";
+    private static final String ATOMIC_BATCH = "atomicBatch";
 
     @BeforeAll
     static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
@@ -51,68 +55,77 @@ public class AtomicScheduleServiceFeesSuite {
                 "50"));
     }
 
+    private HapiSpecOperation[] scheduleSetup() {
+        return new HapiSpecOperation[] {
+            cryptoCreate(BATCH_OPERATOR),
+            uploadInitCode(SIMPLE_UPDATE),
+            cryptoCreate(OTHER_PAYER),
+            cryptoCreate(PAYING_SENDER),
+            cryptoCreate(RECEIVER).receiverSigRequired(true),
+            contractCreate(SIMPLE_UPDATE).gas(300_000L),
+        };
+    }
+
     @LeakyRepeatableHapiTest(value = NEEDS_SYNCHRONOUS_HANDLE_WORKFLOW, fees = "scheduled-contract-fees.json")
     @DisplayName("Schedule ops have expected USD fees")
     final Stream<DynamicTest> scheduleOpsBaseUSDFees() {
-        final var batchOperator = "batchOperator";
         final String SCHEDULE_NAME = "canonical";
         return customizedHapiTest(
                 Map.of("memo.useSpecName", "false"),
-                cryptoCreate(batchOperator),
-                uploadInitCode(SIMPLE_UPDATE),
-                cryptoCreate(OTHER_PAYER),
-                cryptoCreate(PAYING_SENDER),
-                cryptoCreate(RECEIVER).receiverSigRequired(true),
-                contractCreate(SIMPLE_UPDATE).gas(300_000L),
-                scheduleCreate(
-                                SCHEDULE_NAME,
-                                cryptoTransfer(tinyBarsFromTo(PAYING_SENDER, RECEIVER, 1L))
-                                        .memo("")
-                                        .fee(ONE_HBAR))
-                        .payingWith(OTHER_PAYER)
-                        .via("canonicalCreation")
-                        .alsoSigningWith(PAYING_SENDER)
-                        .adminKey(OTHER_PAYER),
-                atomicBatch(scheduleCreate(
-                                        "tbd",
+                flattened(
+                        scheduleSetup(),
+                        scheduleCreate(
+                                        SCHEDULE_NAME,
                                         cryptoTransfer(tinyBarsFromTo(PAYING_SENDER, RECEIVER, 1L))
                                                 .memo("")
                                                 .fee(ONE_HBAR))
-                                .payingWith(PAYING_SENDER)
-                                .adminKey(PAYING_SENDER)
-                                .batchKey(batchOperator))
-                        .via("atomicBatch")
-                        .signedByPayerAnd(batchOperator),
-                atomicBatch(
-                                scheduleSign(SCHEDULE_NAME)
-                                        .via("canonicalSigning")
-                                        .payingWith(PAYING_SENDER)
-                                        .alsoSigningWith(RECEIVER)
-                                        .batchKey(batchOperator),
-                                scheduleDelete("tbd")
-                                        .via("canonicalDeletion")
-                                        .payingWith(PAYING_SENDER)
-                                        .batchKey(batchOperator),
-                                scheduleCreate(
-                                                "contractCall",
-                                                contractCall(
-                                                                SIMPLE_UPDATE,
-                                                                "set",
-                                                                BigInteger.valueOf(5),
-                                                                BigInteger.valueOf(42))
-                                                        .gas(24_000)
+                                .payingWith(OTHER_PAYER)
+                                .via("canonicalCreation")
+                                .alsoSigningWith(PAYING_SENDER)
+                                .adminKey(OTHER_PAYER),
+                        atomicBatch(scheduleCreate(
+                                                "tbd",
+                                                cryptoTransfer(tinyBarsFromTo(PAYING_SENDER, RECEIVER, 1L))
                                                         .memo("")
                                                         .fee(ONE_HBAR))
-                                        .payingWith(OTHER_PAYER)
-                                        .via("canonicalContractCall")
-                                        .adminKey(OTHER_PAYER)
-                                        .batchKey(batchOperator))
-                        .via("atomicBatch")
-                        .signedByPayerAnd(batchOperator),
-                handleAnyRepeatableQueryPayment(),
-                validateInnerTxnChargedUsd("canonicalCreation", "atomicBatch", BASE_FEE_SCHEDULE_CREATE, 5.0),
-                validateInnerTxnChargedUsd("canonicalSigning", "atomicBatch", BASE_FEE_SCHEDULE_SIGN, 5.0),
-                validateInnerTxnChargedUsd("canonicalDeletion", "atomicBatch", BASE_FEE_SCHEDULE_DELETE, 5.0),
-                validateInnerTxnChargedUsd("canonicalContractCall", "atomicBatch", BASE_FEE_CONTRACT_CALL, 5.0));
+                                        .payingWith(PAYING_SENDER)
+                                        .adminKey(PAYING_SENDER)
+                                        .batchKey(BATCH_OPERATOR))
+                                .via(ATOMIC_BATCH)
+                                .signedByPayerAnd(BATCH_OPERATOR)
+                                .payingWith(BATCH_OPERATOR),
+                        atomicBatch(
+                                        scheduleSign(SCHEDULE_NAME)
+                                                .via("canonicalSigning")
+                                                .payingWith(PAYING_SENDER)
+                                                .alsoSigningWith(RECEIVER)
+                                                .batchKey(BATCH_OPERATOR),
+                                        scheduleDelete("tbd")
+                                                .via("canonicalDeletion")
+                                                .payingWith(PAYING_SENDER)
+                                                .batchKey(BATCH_OPERATOR),
+                                        scheduleCreate(
+                                                        "contractCall",
+                                                        contractCall(
+                                                                        SIMPLE_UPDATE,
+                                                                        "set",
+                                                                        BigInteger.valueOf(5),
+                                                                        BigInteger.valueOf(42))
+                                                                .gas(24_000)
+                                                                .memo("")
+                                                                .fee(ONE_HBAR))
+                                                .payingWith(OTHER_PAYER)
+                                                .via("canonicalContractCall")
+                                                .adminKey(OTHER_PAYER)
+                                                .batchKey(BATCH_OPERATOR))
+                                .via(ATOMIC_BATCH)
+                                .signedByPayerAnd(BATCH_OPERATOR)
+                                .payingWith(BATCH_OPERATOR),
+                        handleAnyRepeatableQueryPayment(),
+                        validateInnerTxnChargedUsd("canonicalCreation", ATOMIC_BATCH, BASE_FEE_SCHEDULE_CREATE, 5.0),
+                        validateInnerTxnChargedUsd("canonicalSigning", ATOMIC_BATCH, BASE_FEE_SCHEDULE_SIGN, 5.0),
+                        validateInnerTxnChargedUsd("canonicalDeletion", ATOMIC_BATCH, BASE_FEE_SCHEDULE_DELETE, 5.0),
+                        validateInnerTxnChargedUsd(
+                                "canonicalContractCall", ATOMIC_BATCH, BASE_FEE_CONTRACT_CALL, 5.0)));
     }
 }
