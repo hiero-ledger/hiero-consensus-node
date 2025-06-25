a1248ea568e9130ffef913d29a8cdcbc37365607
Timo Brandstätter <timo@swirldslabs.com>
2025-06-25 11:10:50 +0200

    minor changes
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/internal/result/SingleNodeConsensusResultImpl.java
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/internal/result/SingleNodeConsensusResultImpl.java
@@ -29,8 +29,8 @@ public class SingleNodeConsensusResultImpl implements SingleNodeConsensusResult 
     /**
      * {@inheritDoc}
      */
-    @NonNull
     @Override
+    @NonNull
     public NodeId nodeId() {
         return collector.nodeId();
     }
