1e44d45e927c6fb294df0cdd92ff017e07305889
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2025-07-07 16:34:54 +0300

    nit: spotless
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/records/batch/AtomicLogsSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/records/batch/AtomicLogsSuite.java
@@ -43,124 +43,124 @@ public class AtomicLogsSuite {
     @BeforeAll
     static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
         testLifecycle.overrideInClass(Map.of(
-            "atomicBatch.isEnabled",
-            "true",
-            "atomicBatch.maxNumberOfTransactions",
-            "50",
-            "contracts.throttle.throttleByGas",
-            "false"));
+                "atomicBatch.isEnabled",
+                "true",
+                "atomicBatch.maxNumberOfTransactions",
+                "50",
+                "contracts.throttle.throttleByGas",
+                "false"));
         testLifecycle.doAdhoc(cryptoCreate(BATCH_OPERATOR).balance(ONE_MILLION_HBARS));
     }
 
     @HapiTest
     final Stream<DynamicTest> log0Works() {
         return hapiTest(
-            uploadInitCode(CONTRACT),
-            contractCreate(CONTRACT),
-            atomicBatch(contractCall(CONTRACT, "log0", BigInteger.valueOf(15))
-                .via("log0")
-                .gas(GAS_TO_OFFER)
-                .batchKey(BATCH_OPERATOR))
-                .payingWith(BATCH_OPERATOR),
-            getTxnRecord("log0")
-                .hasPriority(recordWith()
-                    .contractCallResult(resultWith()
-                        .logs(inOrder(logWith().noTopics().longValue(15)))
-                        .gasUsed(22_489))));
+                uploadInitCode(CONTRACT),
+                contractCreate(CONTRACT),
+                atomicBatch(contractCall(CONTRACT, "log0", BigInteger.valueOf(15))
+                                .via("log0")
+                                .gas(GAS_TO_OFFER)
+                                .batchKey(BATCH_OPERATOR))
+                        .payingWith(BATCH_OPERATOR),
+                getTxnRecord("log0")
+                        .hasPriority(recordWith()
+                                .contractCallResult(resultWith()
+                                        .logs(inOrder(logWith().noTopics().longValue(15)))
+                                        .gasUsed(22_489))));
     }
 
     @HapiTest
     final Stream<DynamicTest> log1Works() {
         return hapiTest(
-            uploadInitCode(CONTRACT),
-            contractCreate(CONTRACT),
-            atomicBatch(contractCall(CONTRACT, "log1", BigInteger.valueOf(15))
-                .via("log1")
-                .gas(GAS_TO_OFFER)
-                .batchKey(BATCH_OPERATOR))
-                .payingWith(BATCH_OPERATOR),
-            getTxnRecord("log1")
-                .hasPriority(recordWith()
-                    .contractCallResult(resultWith()
-                        .logs(inOrder(logWith()
-                            .noData()
-                            .withTopicsInOrder(List.of(
-                                eventSignatureOf("Log1(uint256)"),
-                                parsedToByteString(0, 0, 15)))))
-                        .gasUsed(22_787))));
+                uploadInitCode(CONTRACT),
+                contractCreate(CONTRACT),
+                atomicBatch(contractCall(CONTRACT, "log1", BigInteger.valueOf(15))
+                                .via("log1")
+                                .gas(GAS_TO_OFFER)
+                                .batchKey(BATCH_OPERATOR))
+                        .payingWith(BATCH_OPERATOR),
+                getTxnRecord("log1")
+                        .hasPriority(recordWith()
+                                .contractCallResult(resultWith()
+                                        .logs(inOrder(logWith()
+                                                .noData()
+                                                .withTopicsInOrder(List.of(
+                                                        eventSignatureOf("Log1(uint256)"),
+                                                        parsedToByteString(0, 0, 15)))))
+                                        .gasUsed(22_787))));
     }
 
     @HapiTest
     final Stream<DynamicTest> log2Works() {
         return hapiTest(
-            uploadInitCode(CONTRACT),
-            contractCreate(CONTRACT),
-            atomicBatch(contractCall(CONTRACT, "log2", BigInteger.ONE, BigInteger.TWO)
-                .gas(GAS_TO_OFFER)
-                .via("log2")
-                .batchKey(BATCH_OPERATOR))
-                .payingWith(BATCH_OPERATOR),
-            getTxnRecord("log2")
-                .hasPriority(recordWith()
-                    .contractCallResult(resultWith()
-                        .logs(inOrder(logWith()
-                            .noData()
-                            .withTopicsInOrder(List.of(
-                                eventSignatureOf("Log2(uint256,uint256)"),
-                                parsedToByteString(0, 0, 1),
-                                parsedToByteString(0, 0, 2)))))
-                        .gasUsed(23_456))));
+                uploadInitCode(CONTRACT),
+                contractCreate(CONTRACT),
+                atomicBatch(contractCall(CONTRACT, "log2", BigInteger.ONE, BigInteger.TWO)
+                                .gas(GAS_TO_OFFER)
+                                .via("log2")
+                                .batchKey(BATCH_OPERATOR))
+                        .payingWith(BATCH_OPERATOR),
+                getTxnRecord("log2")
+                        .hasPriority(recordWith()
+                                .contractCallResult(resultWith()
+                                        .logs(inOrder(logWith()
+                                                .noData()
+                                                .withTopicsInOrder(List.of(
+                                                        eventSignatureOf("Log2(uint256,uint256)"),
+                                                        parsedToByteString(0, 0, 1),
+                                                        parsedToByteString(0, 0, 2)))))
+                                        .gasUsed(23_456))));
     }
 
     @HapiTest
     final Stream<DynamicTest> log3Works() {
         return hapiTest(
-            uploadInitCode(CONTRACT),
-            contractCreate(CONTRACT),
-            atomicBatch(contractCall(CONTRACT, "log3", BigInteger.ONE, BigInteger.TWO, BigInteger.valueOf(3))
-                .gas(GAS_TO_OFFER)
-                .via("log3")
-                .batchKey(BATCH_OPERATOR))
-                .payingWith(BATCH_OPERATOR),
-            getTxnRecord("log3")
-                .hasPriority(recordWith()
-                    .contractCallResult(resultWith()
-                        .logs(inOrder(logWith()
-                            .noData()
-                            .withTopicsInOrder(List.of(
-                                eventSignatureOf("Log3(uint256,uint256,uint256)"),
-                                parsedToByteString(0, 0, 1),
-                                parsedToByteString(0, 0, 2),
-                                parsedToByteString(0, 0, 3)))))
-                        .gasUsed(24_122))));
+                uploadInitCode(CONTRACT),
+                contractCreate(CONTRACT),
+                atomicBatch(contractCall(CONTRACT, "log3", BigInteger.ONE, BigInteger.TWO, BigInteger.valueOf(3))
+                                .gas(GAS_TO_OFFER)
+                                .via("log3")
+                                .batchKey(BATCH_OPERATOR))
+                        .payingWith(BATCH_OPERATOR),
+                getTxnRecord("log3")
+                        .hasPriority(recordWith()
+                                .contractCallResult(resultWith()
+                                        .logs(inOrder(logWith()
+                                                .noData()
+                                                .withTopicsInOrder(List.of(
+                                                        eventSignatureOf("Log3(uint256,uint256,uint256)"),
+                                                        parsedToByteString(0, 0, 1),
+                                                        parsedToByteString(0, 0, 2),
+                                                        parsedToByteString(0, 0, 3)))))
+                                        .gasUsed(24_122))));
     }
 
     @HapiTest
     final Stream<DynamicTest> log4Works() {
         return hapiTest(
-            uploadInitCode(CONTRACT),
-            contractCreate(CONTRACT),
-            atomicBatch(contractCall(
-                CONTRACT,
-                "log4",
-                BigInteger.ONE,
-                BigInteger.TWO,
-                BigInteger.valueOf(3),
-                BigInteger.valueOf(4))
-                .gas(GAS_TO_OFFER)
-                .via("log4")
-                .batchKey(BATCH_OPERATOR))
-                .payingWith(BATCH_OPERATOR),
-            getTxnRecord("log4")
-                .hasPriority(recordWith()
-                    .contractCallResult(resultWith()
-                        .logs(inOrder(logWith()
-                            .longValue(4)
-                            .withTopicsInOrder(List.of(
-                                eventSignatureOf("Log4(uint256,uint256,uint256," + "uint256)"),
-                                parsedToByteString(0, 0, 1),
-                                parsedToByteString(0, 0, 2),
-                                parsedToByteString(0, 0, 3)))))
-                        .gasUsed(24_918))));
+                uploadInitCode(CONTRACT),
+                contractCreate(CONTRACT),
+                atomicBatch(contractCall(
+                                        CONTRACT,
+                                        "log4",
+                                        BigInteger.ONE,
+                                        BigInteger.TWO,
+                                        BigInteger.valueOf(3),
+                                        BigInteger.valueOf(4))
+                                .gas(GAS_TO_OFFER)
+                                .via("log4")
+                                .batchKey(BATCH_OPERATOR))
+                        .payingWith(BATCH_OPERATOR),
+                getTxnRecord("log4")
+                        .hasPriority(recordWith()
+                                .contractCallResult(resultWith()
+                                        .logs(inOrder(logWith()
+                                                .longValue(4)
+                                                .withTopicsInOrder(List.of(
+                                                        eventSignatureOf("Log4(uint256,uint256,uint256," + "uint256)"),
+                                                        parsedToByteString(0, 0, 1),
+                                                        parsedToByteString(0, 0, 2),
+                                                        parsedToByteString(0, 0, 3)))))
+                                        .gasUsed(24_918))));
     }
 }
