cc23ef7cc5149cfc5f0c3e7a993dc3b17be2943d
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2025-07-14 20:11:08 +0300

    chore: tests rerun
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/AssertMessages.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/AssertMessages.java
@@ -3,6 +3,6 @@ package com.hedera.node.app.service.contract.impl.test;
 
 public class AssertMessages {
 
-    public static final String STATUS = "Status does not match";
-    public static final String OUTPUT = "Output does not match";
+    public static final String STATUS = "Status does not match.";
+    public static final String OUTPUT = "Output does not match.";
 }
