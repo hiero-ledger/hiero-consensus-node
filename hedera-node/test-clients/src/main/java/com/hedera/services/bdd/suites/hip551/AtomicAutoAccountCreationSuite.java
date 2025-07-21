2229b5b69b5a9b75827687f496480e065be73416
ibankov <ivan.bankov@limechain.tech>
2025-07-21 21:45:17 +0300

    init
    Signed-off-by: ibankov <ivan.bankov@limechain.tech>

---
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip551/AtomicAutoAccountCreationSuite.java
@@ -0,0 +1,66 @@
+package com.hedera.services.bdd.suites.hip551;
+
+import com.hedera.services.bdd.junit.HapiTest;
+import com.hedera.services.bdd.junit.HapiTestLifecycle;
+import com.hedera.services.bdd.junit.support.TestLifecycle;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Tag;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.hedera.services.bdd.junit.TestTags.CRYPTO;
+import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
+import static com.hedera.services.bdd.spec.assertions.AccountInfoAsserts.accountWith;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAliasedAccountInfo;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.atomicBatch;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
+import static com.hedera.services.bdd.spec.transactions.crypto.HapiCryptoTransfer.tinyBarsFromToWithAlias;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
+import static com.hedera.services.bdd.suites.HapiSuite.ONE_HBAR;
+import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
+import static com.hedera.services.bdd.suites.crypto.AutoAccountUpdateSuite.ALIAS;
+import static com.hedera.services.bdd.suites.crypto.AutoAccountUpdateSuite.INITIAL_BALANCE;
+import static com.hedera.services.bdd.suites.crypto.AutoCreateUtils.updateSpecFor;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.PAYER_ACCOUNT_NOT_FOUND;
+
+@Tag(CRYPTO)
+@HapiTestLifecycle
+public class AtomicAutoAccountCreationSuite {
+
+    @BeforeAll
+    static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
+        testLifecycle.overrideInClass(
+                Map.of("atomicBatch.isEnabled", "true", "atomicBatch.maxNumberOfTransactions", "50"));
+    }
+
+    @HapiTest
+    final Stream<DynamicTest> aliasedPayerDoesntWork() {
+        return hapiTest(
+                newKeyNamed(ALIAS),
+                newKeyNamed("alias2"),
+                cryptoCreate("batchOperator"),
+                cryptoCreate("payer").balance(INITIAL_BALANCE * ONE_HBAR),
+                cryptoTransfer(tinyBarsFromToWithAlias("payer", ALIAS, 2 * ONE_HUNDRED_HBARS)),
+                withOpContext((spec, opLog) -> updateSpecFor(spec, ALIAS)),
+                getAliasedAccountInfo(ALIAS)
+                        .has(accountWith().expectedBalanceWithChargedUsd((2 * ONE_HUNDRED_HBARS), 0, 0)),
+                // pay with aliased id
+                atomicBatch(
+                cryptoTransfer(tinyBarsFromToWithAlias(ALIAS, "alias2", ONE_HUNDRED_HBARS))
+                        .payingWithAliased(ALIAS)
+                        .batchKey("batchOperator"))
+                        .payingWith("batchOperator")
+                        .hasPrecheck(PAYER_ACCOUNT_NOT_FOUND),
+                // pay with regular accountID
+                atomicBatch(
+                cryptoTransfer(tinyBarsFromToWithAlias(ALIAS, "alias2", ONE_HUNDRED_HBARS))
+                        .payingWith(ALIAS)
+                        .batchKey("batchOperator"))
+                        .payingWith("batchOperator"));
+    }
+}
