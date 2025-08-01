20c76d19f8eb33a2fd25143b26c6c1c8d4fdf934
Lazar Petrovic <lpetrovic05@gmail.com>
2025-08-01 11:27:50 +0200

    s
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
@@ -521,9 +521,7 @@ public final class Hedera implements SwirldMain<MerkleNodeState>, AppContext.Gos
         contractServiceImpl = new ContractServiceImpl(appContext, metrics);
         scheduleServiceImpl = new ScheduleServiceImpl(appContext);
         blockStreamService = new BlockStreamService();
-        transactionLimits = new TransactionLimits(
-                2621440,
-                2621440);
+        transactionLimits = new TransactionLimits(2621440, 2621440);
         transactionPool = new TransactionPoolNexus(
                 transactionLimits,
                 bootstrapConfig.getConfigData(HederaConfig.class).maxTransactionBytesPerEvent(),
