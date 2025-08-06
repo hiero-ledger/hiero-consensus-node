6eb028180bf307188753106d210bf40ce77b15c0
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-08-06 16:34:58 +0300

    Move address167 batch tests
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/token/address_167/TokenAndTypeCheckSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/token/address_167/TokenAndTypeCheckSuite.java
@@ -9,7 +9,6 @@ import static com.hedera.services.bdd.spec.assertions.ContractFnResultAsserts.re
 import static com.hedera.services.bdd.spec.assertions.TransactionRecordAsserts.recordWith;
 import static com.hedera.services.bdd.spec.dsl.entities.SpecContract.VARIANT_167;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.contractCallLocal;
-import static com.hedera.services.bdd.spec.transactions.TxnVerbs.atomicBatch;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
@@ -20,34 +19,26 @@ import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.childRecordsCheck;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
-import static com.hedera.services.bdd.suites.HapiSuite.ONE_MILLION_HBARS;
 import static com.hedera.services.bdd.suites.HapiSuite.TOKEN_TREASURY;
 import static com.hedera.services.bdd.suites.contract.Utils.asAddress;
 import static com.hedera.services.bdd.suites.contract.Utils.asToken;
 import static com.hedera.services.bdd.suites.token.TokenAssociationSpecs.VANILLA_TOKEN;
 import static com.hedera.services.bdd.suites.utils.contracts.precompile.HTSPrecompileResult.htsPrecompileResult;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_REVERT_EXECUTED;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INNER_TRANSACTION_FAILED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_TOKEN_ID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
 import static com.hederahashgraph.api.proto.java.TokenType.FUNGIBLE_COMMON;
 
 import com.hedera.services.bdd.junit.HapiTest;
-import com.hedera.services.bdd.junit.HapiTestLifecycle;
-import com.hedera.services.bdd.junit.support.TestLifecycle;
 import com.hedera.services.bdd.spec.transactions.contract.HapiParserUtil;
 import com.hederahashgraph.api.proto.java.TokenID;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import java.math.BigInteger;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
 
-@HapiTestLifecycle
 @Tag(SMART_CONTRACT)
 public class TokenAndTypeCheckSuite {
     private static final String TOKEN_AND_TYPE_CHECK_CONTRACT = "TokenAndTypeCheck";
@@ -55,14 +46,6 @@ public class TokenAndTypeCheckSuite {
     private static final int GAS_TO_OFFER = 1_000_000;
     private static final String GET_TOKEN_TYPE = "getType";
     private static final String IS_TOKEN = "isAToken";
-    private static final String BATCH_OPERATOR = "batchOperator";
-
-    @BeforeAll
-    static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
-        testLifecycle.overrideInClass(
-                Map.of("atomicBatch.isEnabled", "true", "atomicBatch.maxNumberOfTransactions", "50"));
-        testLifecycle.doAdhoc(cryptoCreate(BATCH_OPERATOR).balance(ONE_MILLION_HBARS));
-    }
 
     @HapiTest
     final Stream<DynamicTest> checkTokenAndTypeStandardCases() {
@@ -144,71 +127,6 @@ public class TokenAndTypeCheckSuite {
                                 .payingWith(ACCOUNT)
                                 .gas(GAS_TO_OFFER)
                                 .logged())),
-                childRecordsCheck(
-                        "FakeAddressTokenCheckTx",
-                        SUCCESS,
-                        recordWith()
-                                .status(SUCCESS)
-                                .contractCallResult(resultWith()
-                                        .contractCallResult(htsPrecompileResult()
-                                                .forFunction(HAPI_IS_TOKEN)
-                                                .withStatus(SUCCESS)
-                                                .withIsToken(false)))),
-                childRecordsCheck(
-                        "FakeAddressTokenTypeCheckTx",
-                        CONTRACT_REVERT_EXECUTED,
-                        recordWith().status(INVALID_TOKEN_ID)
-                        //                                        .contractCallResult(resultWith()
-                        //
-                        // .contractCallResult(htsPrecompileResult()
-                        //                                                        .forFunction(HAPI_IS_TOKEN)
-                        //                                                        .withStatus(INVALID_TOKEN_ID)
-                        //                                                        .withIsToken(false)))
-                        ));
-    }
-
-    // Should just return false on isToken() check for missing token type
-    @HapiTest
-    final Stream<DynamicTest> atomicCheckTokenAndTypeNegativeCases() {
-        final AtomicReference<TokenID> vanillaTokenID = new AtomicReference<>();
-        final var notAnAddress = new byte[20];
-
-        return hapiTest(
-                cryptoCreate(ACCOUNT).balance(100 * ONE_HUNDRED_HBARS),
-                cryptoCreate(TOKEN_TREASURY),
-                tokenCreate(VANILLA_TOKEN)
-                        .tokenType(FUNGIBLE_COMMON)
-                        .treasury(TOKEN_TREASURY)
-                        .initialSupply(1_000)
-                        .exposingCreatedIdTo(id -> vanillaTokenID.set(asToken(id))),
-                tokenAssociate(ACCOUNT, VANILLA_TOKEN),
-                uploadInitCode(Optional.empty(), VARIANT_167, TOKEN_AND_TYPE_CHECK_CONTRACT),
-                contractCreate(TOKEN_AND_TYPE_CHECK_CONTRACT),
-                withOpContext((spec, opLog) -> allRunFor(
-                        spec,
-                        atomicBatch(contractCall(
-                                                Optional.of(VARIANT_167),
-                                                TOKEN_AND_TYPE_CHECK_CONTRACT,
-                                                IS_TOKEN,
-                                                HapiParserUtil.asHeadlongAddress(notAnAddress))
-                                        .via("FakeAddressTokenCheckTx")
-                                        .payingWith(ACCOUNT)
-                                        .gas(GAS_TO_OFFER)
-                                        .batchKey(BATCH_OPERATOR))
-                                .payingWith(BATCH_OPERATOR),
-                        atomicBatch(contractCall(
-                                                Optional.of(VARIANT_167),
-                                                TOKEN_AND_TYPE_CHECK_CONTRACT,
-                                                GET_TOKEN_TYPE,
-                                                HapiParserUtil.asHeadlongAddress(notAnAddress))
-                                        .hasKnownStatus(CONTRACT_REVERT_EXECUTED)
-                                        .via("FakeAddressTokenTypeCheckTx")
-                                        .payingWith(ACCOUNT)
-                                        .gas(GAS_TO_OFFER)
-                                        .logged()
-                                        .batchKey(BATCH_OPERATOR))
-                                .payingWith(BATCH_OPERATOR)
-                                .hasKnownStatus(INNER_TRANSACTION_FAILED))),
                 childRecordsCheck(
                         "FakeAddressTokenCheckTx",
                         SUCCESS,
