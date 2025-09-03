42a3585860b281624236099d14f8227d656c4d78
Derek Riley <derek.riley@swirldslabs.com>
2025-09-03 09:51:11 -0400

    fix blocks pruned increment
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/blocks/impl/streaming/BlockBufferService.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/blocks/impl/streaming/BlockBufferService.java
@@ -608,7 +608,9 @@ public class BlockBufferService {
 
         // update the earliest block number after pruning
         earliestBlockNumber.set(newEarliestBlock == Long.MAX_VALUE ? -1 : newEarliestBlock);
-        blockStreamMetrics.recordNumberOfBlocksPruned(numPruned);
+        if (numPruned > 0) {
+            blockStreamMetrics.recordNumberOfBlocksPruned(numPruned);
+        }
 
         return new PruneResult(
                 idealMaxBufferSize, numChecked, numPendingAck, numPruned, newEarliestBlock, newLatestBlock);
