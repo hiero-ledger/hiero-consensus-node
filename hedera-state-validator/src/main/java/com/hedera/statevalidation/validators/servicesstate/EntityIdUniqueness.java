33076a061a6e09878cf921ca1528f04148474cc4
Ivan Malygin <ivan@swirldslabs.com>
2025-09-05 12:42:04 -0400

    Addressed review comments
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- hedera-state-validator/src/main/java/com/hedera/statevalidation/validators/servicesstate/EntityIdUniqueness.java
+++ hedera-state-validator/src/main/java/com/hedera/statevalidation/validators/servicesstate/EntityIdUniqueness.java
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.statevalidation.validators.servicesstate;
 
+import static com.hedera.node.app.service.consensus.impl.ConsensusServiceImpl.TOPICS_KEY;
+import static com.hedera.node.app.service.contract.impl.schemas.V0490ContractSchema.BYTECODE_KEY;
+import static com.hedera.node.app.service.file.impl.schemas.V0490FileSchema.BLOBS_KEY;
+import static com.hedera.node.app.service.schedule.impl.schemas.V0490ScheduleSchema.SCHEDULES_BY_ID_KEY;
+import static com.hedera.node.app.service.token.impl.schemas.V0490TokenSchema.ACCOUNTS_KEY;
+import static com.hedera.node.app.service.token.impl.schemas.V0490TokenSchema.TOKENS_KEY;
+
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.ContractID;
 import com.hedera.hapi.node.base.FileID;
@@ -14,6 +21,11 @@ import com.hedera.hapi.node.state.file.File;
 import com.hedera.hapi.node.state.schedule.Schedule;
 import com.hedera.hapi.node.state.token.Account;
 import com.hedera.hapi.node.state.token.Token;
+import com.hedera.node.app.service.consensus.ConsensusService;
+import com.hedera.node.app.service.contract.ContractService;
+import com.hedera.node.app.service.file.FileService;
+import com.hedera.node.app.service.schedule.ScheduleService;
+import com.hedera.node.app.service.token.TokenService;
 import com.hedera.statevalidation.parameterresolver.ReportResolver;
 import com.hedera.statevalidation.parameterresolver.StateResolver;
 import com.hedera.statevalidation.reporting.Report;
@@ -26,6 +38,8 @@ import com.swirlds.state.spi.ReadableSingletonState;
 import com.swirlds.virtualmap.VirtualMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,6 +47,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith({StateResolver.class, ReportResolver.class, SlackReportGenerator.class})
 @Tag("entityIdUniqueness")
 public class EntityIdUniqueness {
+
+    private static final Logger log = LogManager.getLogger(EntityIdUniqueness.class);
 
     @Test
     void validate(DeserializedSignedState deserializedState, Report report)
@@ -48,17 +64,17 @@ public class EntityIdUniqueness {
         AtomicInteger issuesFound = new AtomicInteger(0);
 
         final ReadableKVState<TokenID, Token> tokensState =
-                servicesState.getReadableStates("TokenService").get("TOKENS");
+                servicesState.getReadableStates(TokenService.NAME).get(TOKENS_KEY);
         final ReadableKVState<AccountID, Account> accountState =
-                servicesState.getReadableStates("TokenService").get("ACCOUNTS");
+                servicesState.getReadableStates(TokenService.NAME).get(ACCOUNTS_KEY);
         final ReadableKVState<ContractID, Bytecode> smartContractState =
-                servicesState.getReadableStates("ContractService").get("BYTECODE");
+                servicesState.getReadableStates(ContractService.NAME).get(BYTECODE_KEY);
         final ReadableKVState<TopicID, Topic> topicState =
-                servicesState.getReadableStates("ConsensusService").get("TOPICS");
+                servicesState.getReadableStates(ConsensusService.NAME).get(TOPICS_KEY);
         final ReadableKVState<FileID, File> fileState =
-                servicesState.getReadableStates("FileService").get("FILES");
+                servicesState.getReadableStates(FileService.NAME).get(BLOBS_KEY);
         final ReadableKVState<ScheduleID, Schedule> scheduleState =
-                servicesState.getReadableStates("ScheduleService").get("SCHEDULES_BY_ID");
+                servicesState.getReadableStates(ScheduleService.NAME).get(SCHEDULES_BY_ID_KEY);
 
         ParallelProcessingUtil.processRange(0, lastEntityIdNumber, number -> {
                     int counter = 0;
@@ -118,12 +134,12 @@ public class EntityIdUniqueness {
                                  Schedule = %s
                                 """,
                                 number, counter, token, account, contract, topic, file, schedule);
-                        System.out.println(errorMessage);
+                        log.info(errorMessage);
                         issuesFound.incrementAndGet();
                     }
                 })
                 .get();
 
-        System.out.println("Entity ID uniqueness validation completed. Issues found: " + issuesFound.get());
+        log.info("Entity ID uniqueness validation completed. Issues found: " + issuesFound.get());
     }
 }
