54ee28134026b263be9fe0583a9ff0ffa802f7a6
Nikita Lebedev <nikita.lebedev@limechain.tech>
2025-09-08 15:43:19 +0300

    Added state label to V0490RecordCacheSchema
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/recordcache/schemas/V0490RecordCacheSchema.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/recordcache/schemas/V0490RecordCacheSchema.java
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.node.app.state.recordcache.schemas;
 
+import static com.swirlds.state.lifecycle.StateMetadata.computeLabel;
+
 import com.hedera.hapi.node.base.SemanticVersion;
 import com.hedera.hapi.node.state.recordcache.TransactionReceiptEntries;
 import com.hedera.hapi.platform.state.StateKey;
+import com.hedera.node.app.state.recordcache.RecordCacheService;
 import com.swirlds.state.lifecycle.Schema;
 import com.swirlds.state.lifecycle.StateDefinition;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -14,6 +17,8 @@ public class V0490RecordCacheSchema extends Schema {
     public static final String TRANSACTION_RECEIPTS_KEY = "TRANSACTION_RECEIPTS";
     public static final int TRANSACTION_RECEIPTS_STATE_ID =
             StateKey.KeyOneOfType.RECORDCACHE_I_TRANSACTION_RECEIPTS.protoOrdinal();
+    public static final String TRANSACTION_RECEIPTS_STATE_LABEL =
+            computeLabel(RecordCacheService.NAME, TRANSACTION_RECEIPTS_KEY);
 
     /**
      * The version of the schema.
