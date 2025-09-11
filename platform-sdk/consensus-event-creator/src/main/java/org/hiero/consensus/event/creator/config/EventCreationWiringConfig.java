931c66764dd46a87e652b34b50d2fa6d18373a41
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-09-11 10:15:53 -0500

    Removed wiring config that has become obsolete
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-event-creator/src/main/java/org/hiero/consensus/event/creator/config/EventCreationWiringConfig.java
+++ platform-sdk/consensus-event-creator/src/main/java/org/hiero/consensus/event/creator/config/EventCreationWiringConfig.java
@@ -10,14 +10,8 @@ import org.hiero.consensus.event.creator.ConsensusEventCreator;
  * Contains configuration values for the {@link ConsensusEventCreator}'s internal wiring.
  *
  * @param eventCreationManager configuration for the event creation manager scheduler
- * @param staleEventDetector   configuration for the stale event detector scheduler
- * @param transactionPool      configuration for the transaction pool scheduler
  */
 @ConfigData("event.creation.wiring")
 public record EventCreationWiringConfig(
         @ConfigProperty(defaultValue = "SEQUENTIAL CAPACITY(500) FLUSHABLE SQUELCHABLE UNHANDLED_TASK_METRIC")
-                TaskSchedulerConfiguration eventCreationManager,
-        @ConfigProperty(defaultValue = "DIRECT") TaskSchedulerConfiguration selfEventSigner,
-        @ConfigProperty(defaultValue = "SEQUENTIAL CAPACITY(500) FLUSHABLE SQUELCHABLE UNHANDLED_TASK_METRIC")
-                TaskSchedulerConfiguration staleEventDetector,
-        @ConfigProperty(defaultValue = "DIRECT_THREADSAFE") TaskSchedulerConfiguration transactionPool) {}
+                TaskSchedulerConfiguration eventCreationManager) {}
