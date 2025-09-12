051e8514efb1ef0110cdbb569e53cd6bec3c17e4
Lazar Petrovic <lpetrovic05@gmail.com>
2025-09-12 16:24:10 +0200

    yeah
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/quiescence/QuiescenceController.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/quiescence/QuiescenceController.java
@@ -10,13 +10,14 @@ import com.hedera.pbj.runtime.io.buffer.Bytes;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.Iterator;
 import java.util.Objects;
-import java.util.Spliterator;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import org.hiero.consensus.model.event.Event;
 import org.hiero.consensus.model.transaction.Transaction;
 
 public class QuiescenceController {
+    private static final Function<Bytes, TransactionBody> TRANSACTION_BODY_PARSER = uncheckedParse(TransactionBody.PROTOBUF);
+    private static final Function<Bytes, SignedTransaction> SIGNED_TRANSACTION_PARSER = uncheckedParse(SignedTransaction.PROTOBUF);
     private final AtomicLong pipelineTransactionCount = new AtomicLong(0);
 
     public void fullySignedBlock(@NonNull final Block block) {
@@ -24,9 +25,9 @@ public class QuiescenceController {
                 .filter(BlockItem::hasSignedTransaction)
                 .map(BlockItem::signedTransaction)
                 .filter(Objects::nonNull)
-                .map(uncheckedParse(SignedTransaction.PROTOBUF))
+                .map(SIGNED_TRANSACTION_PARSER)
                 .map(SignedTransaction::bodyBytes)
-                .map(uncheckedParse(TransactionBody.PROTOBUF))
+                .map(TRANSACTION_BODY_PARSER)
                 .filter(QuiescenceController::nonSignatureTransactions)
                 .count();
         final long updatedValue = pipelineTransactionCount.addAndGet(transactionCount);
@@ -41,7 +42,7 @@ public class QuiescenceController {
         while (iterator.hasNext()){
 
             if (nonSignatureTransactions(
-                    uncheckedParse(TransactionBody.PROTOBUF).apply(iterator.next().getApplicationTransaction()))) {
+                    TRANSACTION_BODY_PARSER.apply(iterator.next().getApplicationTransaction()))) {
                 transactionCount++;
             }
         }
@@ -54,9 +55,9 @@ public class QuiescenceController {
     }
 
     private static <T> Function<Bytes, T> uncheckedParse(@NonNull final Codec<T> codec) {
-        return function -> {
+        return bytes -> {
             try {
-                return codec.parse(function);
+                return codec.parse(bytes);
             } catch (final ParseException e) {
                 throw new RuntimeException(e);
             }
