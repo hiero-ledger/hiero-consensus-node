de9272947aebc05427493c4e413873313052b4f7
Lazar Petrovic <lpetrovic05@gmail.com>
2025-09-12 16:17:53 +0200

    got started with the controller
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

---
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/quiescence/QuiescenceConfig.java
@@ -0,0 +1,19 @@
+package com.hedera.node.app.quiescence;
+
+import com.swirlds.config.api.ConfigData;
+import com.swirlds.config.api.ConfigProperty;
+import java.time.Duration;
+
+/**
+ * Configuration for quiescence.
+ * @param enabled       indicates if quiescence is enabled
+ * @param tctDuration   the amount of time before the target consensus timestamp (TCT) when quiescence should not be
+ *                      active
+ */
+@ConfigData("quiescence")
+public record QuiescenceConfig(
+        @ConfigProperty(value = "enabled", defaultValue = "true")
+        boolean enabled,
+        @ConfigProperty(value = "tctDuration", defaultValue = "5s")
+        Duration tctDuration) {
+}
