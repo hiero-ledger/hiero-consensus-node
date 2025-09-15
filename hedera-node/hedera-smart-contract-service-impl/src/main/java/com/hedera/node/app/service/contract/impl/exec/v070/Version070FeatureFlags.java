4c48d76a01489308b97af59ee1147025f33457a4
Luke Lee <luke.lee@hashgraph.com>
2025-09-15 15:39:06 -0700

    first pass
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

---
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/v070/Version070FeatureFlags.java
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.node.app.service.contract.impl.exec.v066.v065;
+
+import com.hedera.node.app.service.contract.impl.exec.v051.Version051FeatureFlags;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class Version066FeatureFlags extends Version051FeatureFlags {
+    @Inject
+    public Version066FeatureFlags() {
+        // Dagger2
+    }
+}
