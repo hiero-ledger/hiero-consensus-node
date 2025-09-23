206672808a3c27c876fc6bd13bae3041909df1d7
Matt Hess <matt.hess@swirldslabs.com>
2025-09-23 09:52:23 -0600

    Module deps changes
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-roster-service-impl/build.gradle.kts
+++ hedera-node/hedera-roster-service-impl/build.gradle.kts
@@ -5,10 +5,10 @@ description = "Hedera Roster Service Implementation"
 
 testModuleInfo {
     requires("com.hedera.node.app.test.fixtures")
-    requires("com.hedera.node.app.spi.test.fixtures")
     requires("com.swirlds.platform.core.test.fixtures")
     requires("org.assertj.core")
     requires("org.junit.jupiter.api")
     requires("org.mockito")
     requires("org.mockito.junit.jupiter")
+    requires("com.hedera.node.hapi")
 }
