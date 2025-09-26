c98d4d26c69419f8e7334e6bb8067d80572db100
Matt Hess <matt.hess@swirldslabs.com>
2025-09-26 13:51:36 -0600

    Update BlockItem with new definition
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/blocks/impl/BlockStreamManagerImpl.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/blocks/impl/BlockStreamManagerImpl.java
@@ -91,6 +91,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hiero.base.concurrent.AbstractTask;
 import org.hiero.base.crypto.Hash;
+import org.hiero.base.exceptions.NotImplementedException;
 import org.hiero.consensus.model.hashgraph.Round;
 
 @Singleton
@@ -759,7 +760,10 @@ public class BlockStreamManagerImpl implements BlockStreamManager {
                             STATE_CHANGES,
                             ROUND_HEADER,
                             BLOCK_HEADER,
-                            TRACE_DATA -> {
+						 	BLOCK_FOOTER,
+						    BLOCK_PROOF
+							// Also EndBlock?
+							-> {
                         MessageDigest digest = sha384DigestOrThrow();
                         bytes.writeTo(digest);
                         hash = ByteBuffer.wrap(digest.digest());
@@ -798,7 +802,7 @@ public class BlockStreamManagerImpl implements BlockStreamManager {
                 }
                 case TRANSACTION_OUTPUT, BLOCK_HEADER -> outputTreeHasher.addLeaf(hash);
                 case STATE_CHANGES -> stateChangesHasher.addLeaf(hash);
-                case TRACE_DATA -> traceDataHasher.addLeaf(hash);
+				case BLOCK_FOOTER, BLOCK_PROOF -> throw new NotImplementedException();
             }
 
             final BlockHeader header = item.blockHeader();
--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/impl/BlockStreamManagerImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/impl/BlockStreamManagerImplTest.java
@@ -114,8 +114,8 @@ class BlockStreamManagerImplTest {
     private static final BlockItem FAKE_STATE_CHANGES = BlockItem.newBuilder()
             .stateChanges(StateChanges.newBuilder().consensusTimestamp(CONSENSUS_THEN))
             .build();
-    private static final BlockItem FAKE_RECORD_FILE_ITEM =
-            BlockItem.newBuilder().recordFile(RecordFileItem.DEFAULT).build();
+	// TODO: remove, or replace with wrapped record file item
+    private static final BlockItem FAKE_RECORD_FILE_ITEM = null;
     private final InitialStateHash hashInfo = new InitialStateHash(completedFuture(ZERO_BLOCK_HASH), 0);
 
     @Mock
