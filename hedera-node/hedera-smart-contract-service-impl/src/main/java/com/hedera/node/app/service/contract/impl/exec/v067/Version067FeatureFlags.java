ad9a854da957656c87dcbaba648aef3af660c0bc
Lukasz Gasior <lukasz.gasior@swirldslabs.com>
2025-10-02 19:15:37 +0200

    Cleanup V065 and V066 FeatureFlags
    Signed-off-by: Lukasz Gasior <lukasz.gasior@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/v067/Version067FeatureFlags.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/v067/Version067FeatureFlags.java
@@ -1,25 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.node.app.service.contract.impl.exec.v067;
 
-import static java.util.Objects.requireNonNull;
-
-import com.hedera.node.app.service.contract.impl.exec.v065.Version065FeatureFlags;
-import com.hedera.node.config.data.ContractsConfig;
-import com.swirlds.config.api.Configuration;
-import edu.umd.cs.findbugs.annotations.NonNull;
+import com.hedera.node.app.service.contract.impl.exec.v066.Version066FeatureFlags;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class Version067FeatureFlags extends Version065FeatureFlags {
+public class Version067FeatureFlags extends Version066FeatureFlags {
 
     @Inject
     public Version067FeatureFlags() {
         // Dagger2
-    }
-
-    public boolean isNativeLibVerificationEnabled(@NonNull Configuration config) {
-        requireNonNull(config);
-        return config.getConfigData(ContractsConfig.class).nativeLibVerificationHaltEnabled();
     }
 }
