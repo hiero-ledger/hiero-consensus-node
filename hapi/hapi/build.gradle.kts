9999380ef5664eb9e83f775f479017239aa8b552
Anthony Petrov <anthony@swirldslabs.com>
2025-10-03 16:08:33 -0700

    dependencies
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- hapi/hapi/build.gradle.kts
+++ hapi/hapi/build.gradle.kts
@@ -45,7 +45,7 @@ testModuleInfo {
     requires("com.hedera.node.hapi")
     // we depend on the protoc compiled hapi during test as we test our pbj generated code
     // against it to make sure it is compatible
-    requires("com.google.protobuf.util")
+    runtimeOnly("com.google.protobuf.util")
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
     requires("org.assertj.core")
