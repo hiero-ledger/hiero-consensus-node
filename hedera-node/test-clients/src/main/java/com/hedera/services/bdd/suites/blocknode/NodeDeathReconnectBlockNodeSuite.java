d2a2f389e597eb669673c5caa5de3804dce5be89
Derek Riley <derek.riley@swirldslabs.com>
2025-10-07 09:45:04 -0400

    update restartWithConfigVersion
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/NodeDeathReconnectBlockNodeSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/NodeDeathReconnectBlockNodeSuite.java
@@ -9,11 +9,7 @@ import static com.hedera.services.bdd.spec.utilops.UtilVerbs.assertHgcaaLogDoesN
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.logIt;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.sleepFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.waitForActive;
-import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.MIXED_OPS_BURST_DURATION;
-import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.MIXED_OPS_BURST_TPS;
-import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.PORT_UNBINDING_WAIT_PERIOD;
-import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.RESTART_TO_ACTIVE_TIMEOUT;
-import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.SHUTDOWN_TIMEOUT;
+import static com.hedera.services.bdd.suites.regression.system.LifecycleTest.*;
 import static com.hedera.services.bdd.suites.regression.system.MixedOperations.burstOfTps;
 
 import com.hedera.services.bdd.HapiBlockNode;
@@ -133,7 +129,7 @@ public class NodeDeathReconnectBlockNodeSuite {
                 // Submit operations when node 2 is down
                 burstOfTps(MIXED_OPS_BURST_TPS, MIXED_OPS_BURST_DURATION),
                 // Restart node2
-                FakeNmt.restartNode(NodeSelector.byNodeId(2) /*, CURRENT_CONFIG_VERSION.get()*/),
+                FakeNmt.restartWithConfigVersion(NodeSelector.byNodeId(2), CURRENT_CONFIG_VERSION.get()),
                 // Wait for node2 ACTIVE (BUSY and RECONNECT_COMPLETE are too transient to reliably poll for)
                 waitForActive(NodeSelector.byNodeId(2), RESTART_TO_ACTIVE_TIMEOUT),
                 // Run some more transactions
