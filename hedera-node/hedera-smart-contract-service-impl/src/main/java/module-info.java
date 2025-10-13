819687efac118fb2b4b25c8ed2a0c5854c0b9b48
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-10-13 15:47:43 +0300

    spotless
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
@@ -5,7 +5,6 @@
 module com.hedera.node.app.service.contract.impl {
     requires transitive com.hedera.node.app.hapi.fees;
     requires transitive com.hedera.node.app.hapi.utils;
-    requires com.hedera.node.app.service.addressbook;
     requires transitive com.hedera.node.app.service.contract;
     requires transitive com.hedera.node.app.service.file;
     requires transitive com.hedera.node.app.service.schedule;
@@ -26,6 +25,7 @@ module com.hedera.node.app.service.contract.impl {
     requires transitive org.hyperledger.besu.evm;
     requires transitive tuweni.bytes;
     requires transitive tuweni.units;
+    requires com.hedera.node.app.service.addressbook;
     requires com.swirlds.base;
     requires org.hiero.base.utility;
     requires com.github.benmanes.caffeine;
