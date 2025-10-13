c41f011982d7f71b9c2b861fb5be36a91b097cc2
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-10-13 13:09:10 +0300

    fix module-info
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
@@ -5,6 +5,7 @@
 module com.hedera.node.app.service.contract.impl {
     requires transitive com.hedera.node.app.hapi.fees;
     requires transitive com.hedera.node.app.hapi.utils;
+    requires transitive com.hedera.node.app.service.addressbook;
     requires transitive com.hedera.node.app.service.contract;
     requires transitive com.hedera.node.app.service.file;
     requires transitive com.hedera.node.app.service.schedule;
@@ -25,7 +26,6 @@ module com.hedera.node.app.service.contract.impl {
     requires transitive org.hyperledger.besu.evm;
     requires transitive tuweni.bytes;
     requires transitive tuweni.units;
-    requires com.hedera.node.app.service.addressbook;
     requires com.swirlds.base;
     requires org.hiero.base.utility;
     requires com.github.benmanes.caffeine;
--- hedera-node/hedera-token-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-token-service-impl/src/main/java/module-info.java
@@ -5,6 +5,7 @@
 module com.hedera.node.app.service.token.impl {
     requires transitive com.hedera.node.app.hapi.fees;
     requires transitive com.hedera.node.app.hapi.utils;
+    requires transitive com.hedera.node.app.service.addressbook;
     requires transitive com.hedera.node.app.service.token;
     requires transitive com.hedera.node.app.spi;
     requires transitive com.hedera.node.config;
@@ -17,7 +18,6 @@ module com.hedera.node.app.service.token.impl {
     requires transitive dagger;
     requires transitive javax.inject;
     requires transitive org.apache.commons.lang3;
-    requires com.hedera.node.app.service.addressbook;
     requires com.swirlds.base;
     requires com.swirlds.common;
     requires com.github.spotbugs.annotations;
