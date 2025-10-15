94d57b215a3835bdc1c44fd944ece52b8cf469d2
Derek Riley <derek.riley@swirldslabs.com>
2025-10-15 10:53:38 -0400

    Updates to unit test
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/impl/streaming/BlockBufferServiceTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/impl/streaming/BlockBufferServiceTest.java
@@ -872,7 +872,6 @@ class BlockBufferServiceTest extends BlockNodeCommunicationTestBase {
     void testOpenBlock_streamingDisabled() {
         when(configProvider.getConfiguration()).thenReturn(versionedConfiguration);
         when(blockStreamConfig.writerMode()).thenReturn(BlockStreamWriterMode.FILE);
-        when(blockStreamConfig.streamMode()).thenReturn(StreamMode.BOTH);
         when(blockBufferConfig.bufferDirectory()).thenReturn(testDir);
         when(versionedConfiguration.getConfigData(BlockStreamConfig.class)).thenReturn(blockStreamConfig);
         when(versionedConfiguration.getConfigData(BlockBufferConfig.class)).thenReturn(blockBufferConfig);
@@ -892,7 +891,6 @@ class BlockBufferServiceTest extends BlockNodeCommunicationTestBase {
         when(configProvider.getConfiguration()).thenReturn(versionedConfiguration);
         when(versionedConfiguration.getConfigData(BlockStreamConfig.class)).thenReturn(blockStreamConfig);
         when(blockStreamConfig.writerMode()).thenReturn(BlockStreamWriterMode.FILE);
-        when(blockStreamConfig.streamMode()).thenReturn(StreamMode.BOTH);
         when(blockBufferConfig.bufferDirectory()).thenReturn(testDir);
         when(versionedConfiguration.getConfigData(BlockBufferConfig.class)).thenReturn(blockBufferConfig);
         blockBufferService = initBufferService(configProvider);
