eb80e53217f3a98b5cda7ef5356b152aad9a25cf
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-10-20 20:08:39 +0200

    Disable flaky test
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-otter-tests/src/testIntegration/java/org/hiero/otter/zzz/NetworkPartitionTest.java
+++ platform-sdk/consensus-otter-tests/src/testIntegration/java/org/hiero/otter/zzz/NetworkPartitionTest.java
@@ -21,6 +21,7 @@ import org.hiero.otter.fixtures.container.ContainerTestEnvironment;
 import org.hiero.otter.fixtures.network.Partition;
 import org.hiero.otter.fixtures.result.MultipleNodeLogResults;
 import org.hiero.otter.fixtures.turtle.TurtleTestEnvironment;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -51,6 +52,7 @@ class NetworkPartitionTest {
      */
     @ParameterizedTest
     @MethodSource("environments")
+    @Disabled
     void testCreateAndRemovePartition(@NonNull final TestEnvironment env) {
         try {
             final Network network = env.network();
