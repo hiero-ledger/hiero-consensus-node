774bf3155b5528c585b8410f0f63785f19cd7204
mxtartaglia <maxi@swirldslabs.com>
2025-10-23 14:00:24 -0300

    feat(20422): Wrong Rename
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-merkle/src/test/java/com/swirlds/merkle/test/map/ValueTests.java
+++ platform-sdk/swirlds-merkle/src/test/java/com/swirlds/merkle/test/map/ValueTests.java
@@ -11,7 +11,7 @@ import org.hiero.base.constructable.ConstructableRegistryException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class ReservedSignedStateResultTests {
+class ValueTests {
 
     @BeforeAll
     public static void setUp() throws ConstructableRegistryException {
