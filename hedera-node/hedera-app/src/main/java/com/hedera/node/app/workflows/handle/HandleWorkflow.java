ec3fb55b83e40a9e32be611840dded9afacca9f6
Derek Riley <derek.riley@swirldslabs.com>
2025-10-24 12:58:04 -0400

    chore: Cleanup legacy system account cleanup code (#21849)
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/HandleWorkflow.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/HandleWorkflow.java
@@ -162,8 +162,6 @@ public class HandleWorkflow {
     private final PlatformStateFacade platformStateFacade;
     // Flag to indicate whether we have checked for transplant updates after JVM started
     private boolean checkedForTransplant;
-    // Flag whether the 0.65 system account cleanup has been done; can be removed after that release
-    private boolean systemAccountCleanupDone;
 
     @Inject
     public HandleWorkflow(
@@ -518,11 +516,6 @@ public class HandleWorkflow {
             if (streamMode == RECORDS) {
                 // Only update this if we are relying on RecordManager state for post-upgrade processing
                 blockRecordManager.markMigrationRecordsStreamed();
-            }
-        } else {
-            if (!systemAccountCleanupDone) {
-                // Ensure the system account cleanup is finished post-upgrade
-                systemAccountCleanupDone = systemTransactions.do066SystemAccountCleanup(consensusNow, state);
             }
         }
 
