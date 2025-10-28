0698a0dc2cc0072b65f6b2620c4042f89ec28da8
Artur Kugal <artur.kugal@swirldslabs.com>
2025-10-28 15:24:50 -0400

    Add MetricsExportManagerConfig
    Signed-off-by: Artur Kugal <artur.kugal@swirldslabs.com>

---
+++ hiero-observability/hiero-metrics-core/src/main/java/org/hiero/metrics/internal/export/config/MetricsExportManagerConfigurationExtension.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+package org.hiero.metrics.internal.export.config;
+
+import com.swirlds.config.api.ConfigurationExtension;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.Set;
+
+public class MetricsExportManagerConfigurationExtension implements ConfigurationExtension {
+
+    @NonNull
+    @Override
+    public Set<Class<? extends Record>> getConfigDataTypes() {
+        return Set.of(MetricsExportManagerConfig.class);
+    }
+}
