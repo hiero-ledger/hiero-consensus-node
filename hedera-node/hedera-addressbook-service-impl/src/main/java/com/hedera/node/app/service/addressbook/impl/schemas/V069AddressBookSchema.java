89d0f8d73631378f03641bed27d5335e2866034d
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-11-05 17:23:02 +0200

    Update migration logic
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/schemas/V069AddressBookSchema.java
+++ hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/schemas/V069AddressBookSchema.java
@@ -4,10 +4,6 @@ package com.hedera.node.app.service.addressbook.impl.schemas;
 import static com.hedera.hapi.util.HapiUtils.SEMANTIC_VERSION_COMPARATOR;
 
 import com.hedera.hapi.node.base.SemanticVersion;
-import com.hedera.hapi.node.state.addressbook.Node;
-import com.hedera.hapi.node.state.common.EntityNumber;
-import com.hedera.hapi.platform.state.NodeId;
-import com.hedera.node.app.service.entityid.impl.schemas.V0690EntityIdSchema;
 import com.swirlds.state.lifecycle.MigrationContext;
 import com.swirlds.state.lifecycle.Schema;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -31,30 +27,22 @@ public class V069AddressBookSchema extends Schema<SemanticVersion> {
     @Override
     public void migrate(@NonNull final MigrationContext ctx) {
         if (!ctx.isGenesis()) {
+            // TODO: Delete this schema. We will not remove the deleted nodes from the state.
             // 1) Initialize the highest node id from previous nodes in [0,100)
-            final var highestNodeIdState = ctx.newStates().getSingleton(V0690EntityIdSchema.HIGHEST_NODE_ID_STATE_ID);
-            long highest = -1;
-            if (highestNodeIdState.get() == null) {
-                final var prevNodes = ctx.previousStates().get(V053AddressBookSchema.NODES_STATE_ID);
-                for (long i = 0; i < 100; i++) {
-                    final var key = EntityNumber.newBuilder().number(i).build();
-                    final var node = prevNodes.get(key);
-                    if (node != null) {
-                        highest = Math.max(highest, i);
-                    }
-                }
-                highestNodeIdState.put(NodeId.newBuilder().id(highest).build());
-            }
-
-            // 2) Remove any nodes with deleted=true, scanning ids in [0, 100)
-            final var nodes = ctx.newStates().get(V053AddressBookSchema.NODES_STATE_ID);
-            for (long i = 0; i < 100; i++) {
-                final var key = EntityNumber.newBuilder().number(i).build();
-                final var node = (Node) nodes.get(key);
-                if (node != null && node.deleted()) {
-                    nodes.remove(key);
-                }
-            }
+            //            final var highestNodeIdState =
+            // ctx.newStates().getSingleton(V0690EntityIdSchema.HIGHEST_NODE_ID_STATE_ID);
+            //            long highest = -1;
+            //            if (highestNodeIdState.get() == null) {
+            //                final var prevNodes = ctx.previousStates().get(V053AddressBookSchema.NODES_STATE_ID);
+            //                for (long i = 0; i < 100; i++) {
+            //                    final var key = EntityNumber.newBuilder().number(i).build();
+            //                    final var node = prevNodes.get(key);
+            //                    if (node != null) {
+            //                        highest = Math.max(highest, i);
+            //                    }
+            //                }
+            //                highestNodeIdState.put(NodeId.newBuilder().id(highest).build());
+            //            }
         }
     }
 }
--- hedera-node/hedera-entity-id-service-impl/src/main/java/com/hedera/node/app/service/entityid/impl/schemas/V0690EntityIdSchema.java
+++ hedera-node/hedera-entity-id-service-impl/src/main/java/com/hedera/node/app/service/entityid/impl/schemas/V0690EntityIdSchema.java
@@ -2,11 +2,14 @@
 package com.hedera.node.app.service.entityid.impl.schemas;
 
 import static com.hedera.hapi.util.HapiUtils.SEMANTIC_VERSION_COMPARATOR;
+import static com.hedera.node.app.service.entityid.impl.schemas.V0590EntityIdSchema.ENTITY_COUNTS_STATE_ID;
 import static com.swirlds.state.lifecycle.StateMetadata.computeLabel;
 
 import com.hedera.hapi.node.base.SemanticVersion;
+import com.hedera.hapi.node.state.entity.EntityCounts;
 import com.hedera.hapi.platform.state.NodeId;
 import com.hedera.hapi.platform.state.SingletonType;
+import com.hedera.hapi.platform.state.StateKey;
 import com.hedera.node.app.service.entityid.EntityIdService;
 import com.swirlds.state.lifecycle.MigrationContext;
 import com.swirlds.state.lifecycle.Schema;
@@ -21,6 +24,8 @@ public class V0690EntityIdSchema extends Schema<SemanticVersion> {
 
     private static final SemanticVersion VERSION =
             SemanticVersion.newBuilder().major(0).minor(69).patch(0).build();
+
+    private static final int NODES_STATE_ID = StateKey.KeyOneOfType.ADDRESSBOOKSERVICE_I_NODES.protoOrdinal();
 
     public static final String HIGHEST_NODE_ID_KEY = "HIGHEST_NODE_ID";
     public static final int HIGHEST_NODE_ID_STATE_ID = SingletonType.ENTITYIDSERVICE_I_HIGHEST_NODE_ID.protoOrdinal();
@@ -38,8 +43,10 @@ public class V0690EntityIdSchema extends Schema<SemanticVersion> {
 
     @Override
     public void migrate(@NonNull final MigrationContext ctx) {
-        // No-op; initialization of the highest node id is handled in the AddressBook v0.69 schema migration.
         final var highestNodeIdState = ctx.newStates().getSingleton(HIGHEST_NODE_ID_STATE_ID);
-        highestNodeIdState.put(NodeId.newBuilder().id(-1).build());
+        final var entityCountsState = (EntityCounts)
+                ctx.previousStates().getSingleton(ENTITY_COUNTS_STATE_ID).get();
+        highestNodeIdState.put(
+                NodeId.newBuilder().id(entityCountsState.numNodes() - 1).build());
     }
 }
