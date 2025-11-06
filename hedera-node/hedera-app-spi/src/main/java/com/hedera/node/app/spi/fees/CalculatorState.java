92153ed5529b356ba4b9c21d9b7ed9dc94ddc35f
artemderevets <artem.derevets@limechain.tech>
2025-11-06 18:44:28 +0100

    WIP: refactor implementation
    Signed-off-by: artemderevets <artem.derevets@limechain.tech>

---
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/fees/CalculatorState.java
@@ -0,0 +1,11 @@
+package com.hedera.node.app.spi.fees;
+
+public interface CalculatorState {
+
+    /**
+     * Returns the number of signatures provided for the transaction.
+     * This is typically the size of the signature map ({@code txInfo.signatureMap().sigPair().size()}).
+     * @return the number of signatures
+     */
+    int numTxnSignatures();
+}
