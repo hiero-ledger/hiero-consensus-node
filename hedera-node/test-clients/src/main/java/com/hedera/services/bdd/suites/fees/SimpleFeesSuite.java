c86e0d01ca592a87eb3bc121c8979823da0d004e
Josh Marinacci <joshua@marinacci.org>
2025-11-06 12:59:18 -0800

    add more mint cases move token services simple fees tests to separate file
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/SimpleFeesSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/SimpleFeesSuite.java
@@ -70,7 +70,6 @@ public class SimpleFeesSuite {
     private static final String PAYER = "payer";
     private static final String ADMIN = "admin";
     private static final String NEW_ADMIN = "newAdmin";
-    private static final String SUPPLY_KEY = "supplyKey";
 
     @BeforeAll
     static void beforeAll(@NonNull final TestLifecycle testLifecycle) {
@@ -436,112 +435,6 @@ public class SimpleFeesSuite {
         }
     }
 
-    @Nested
-    class TokenServiceFees {
-        private static final String FUNGIBLE_TOKEN = "fungibleToken";
-        private static final String NFT_TOKEN = "nonFungibleToken";
-        private static final String METADATA_KEY = "metadata-key";
-
-        @HapiTest
-        @DisplayName("create a fungible token")
-        final Stream<DynamicTest> createFungibleToken() {
-            return hapiTest(
-                    cryptoCreate(ADMIN).balance(ONE_BILLION_HBARS),
-                    cryptoCreate(PAYER).balance(ONE_BILLION_HBARS),
-                    tokenCreate(FUNGIBLE_TOKEN)
-                            .blankMemo()
-                            .payingWith(PAYER)
-                            .fee(ONE_HUNDRED_HBARS)
-                            .treasury(ADMIN)
-                            .tokenType(FUNGIBLE_COMMON)
-                            .autoRenewAccount(ADMIN)
-                            .autoRenewPeriod(THREE_MONTHS_IN_SECONDS)
-                            .logged()
-                            .hasKnownStatus(SUCCESS)
-                            .via("create-token-txn")
-                    ,
-                    validateChargedUsd("create-token-txn", ucents_to_USD(100000))
-            );
-        }
-
-        @HapiTest
-        @DisplayName("create a non-fungible token")
-        final Stream<DynamicTest> createNonFungibleToken() {
-            return hapiTest(
-                    newKeyNamed(SUPPLY_KEY),
-                    cryptoCreate(ADMIN).balance(ONE_BILLION_HBARS),
-                    cryptoCreate(PAYER).balance(ONE_BILLION_HBARS),
-                    tokenCreate("uniqueNoFees")
-                            .blankMemo()
-                            .payingWith(PAYER)
-                            .fee(ONE_HUNDRED_HBARS)
-                            .treasury(ADMIN)
-                            .tokenType(NON_FUNGIBLE_UNIQUE)
-                            .initialSupply(0L)
-                            .supplyKey(SUPPLY_KEY)
-                            .autoRenewAccount(ADMIN)
-                            .autoRenewPeriod(THREE_MONTHS_IN_SECONDS)
-                            .logged()
-                            .hasKnownStatus(SUCCESS)
-                            .via("create-token-txn")
-                    ,
-                    validateChargedUsd("create-token-txn", ucents_to_USD(100000))
-            );
-        }
-
-        @HapiTest
-        @DisplayName("mint a common token")
-        final Stream<DynamicTest> mintCommonToken() {
-            return hapiTest(
-                    newKeyNamed(SUPPLY_KEY),
-                    cryptoCreate(ADMIN).balance(ONE_BILLION_HBARS),
-                    cryptoCreate(PAYER).balance(ONE_BILLION_HBARS).key(SUPPLY_KEY),
-                    tokenCreate(FUNGIBLE_TOKEN)
-                            .tokenType(FUNGIBLE_COMMON)
-                            .initialSupply(0L)
-                            .payingWith(PAYER)
-                            .supplyKey(SUPPLY_KEY)
-                            .fee(ONE_HUNDRED_HBARS)
-                            .hasKnownStatus(SUCCESS)
-                            .via("create-token-txn"),
-                    mintToken(FUNGIBLE_TOKEN, 1)
-                            .payingWith(PAYER)
-                            .signedBy(SUPPLY_KEY)
-                            .blankMemo()
-                            .fee(ONE_HUNDRED_HBARS)
-                            .hasKnownStatus(SUCCESS)
-                            .via("fungible-mint-txn"),
-                    validateChargedUsd("fungible-mint-txn", 0.001)
-            );
-        }
-
-        @HapiTest
-        @DisplayName("mint a unique token")
-        final Stream<DynamicTest> mintUniqueToken() {
-            return hapiTest(
-                    newKeyNamed(SUPPLY_KEY),
-                    newKeyNamed(METADATA_KEY),
-                    cryptoCreate(ADMIN).balance(ONE_BILLION_HBARS),
-                    cryptoCreate(PAYER).balance(ONE_BILLION_HBARS).key(SUPPLY_KEY),
-                    tokenCreate(NFT_TOKEN)
-                            .tokenType(NON_FUNGIBLE_UNIQUE)
-                            .initialSupply(0L)
-                            .payingWith(PAYER)
-                            .supplyKey(SUPPLY_KEY)
-                            .fee(ONE_HUNDRED_HBARS)
-                            .hasKnownStatus(SUCCESS)
-                            .via("create-token-txn"),
-                    mintToken(NFT_TOKEN, List.of(ByteString.copyFromUtf8("Bart Simpson")))
-                            .payingWith(PAYER)
-                            .signedBy(SUPPLY_KEY)
-                            .blankMemo()
-                            .fee(ONE_HUNDRED_HBARS)
-                            .hasKnownStatus(SUCCESS)
-                            .via("non-fungible-mint-txn"),
-                    validateChargedUsd("non-fungible-mint-txn", 0.001)
-            );
-        }
-    }
 
     @Nested
     class TopicFeesComparisonNegativeCases {
