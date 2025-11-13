7f1740f86fc9c7e45c00b33438450f297c8c439f
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2025-11-12 21:42:05 -0600

    fix test
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/SimpleFeesSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/SimpleFeesSuite.java
@@ -303,6 +303,8 @@ public class SimpleFeesSuite {
                     cryptoDelete("accountToDelete")
                             .transfer(PAYER)
                             .payingWith("accountToDelete")
+                            .signedBy("accountToDelete")
+                            .blankMemo()
                             .fee(ONE_HBAR)
                             .via("deleteAccountTxn"),
                     validateChargedUsdWithin("deleteAccountTxn", 0.005, 1.0));
