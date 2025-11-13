b61960e266bdc7df6207548a4809b14c4018ca4b
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-11-13 10:00:26 +0100

    Make state initialized check flexible
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/app/OtterApp.java
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/app/OtterApp.java
@@ -10,6 +10,7 @@ import com.swirlds.config.api.Configuration;
 import com.swirlds.platform.state.ConsensusStateEventHandler;
 import com.swirlds.platform.system.InitTrigger;
 import com.swirlds.platform.system.Platform;
+import com.swirlds.state.spi.ReadableStates;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import java.io.IOException;
@@ -227,8 +228,14 @@ public class OtterApp implements ConsensusStateEventHandler<OtterAppState> {
             @NonNull final InitTrigger trigger,
             @Nullable final SemanticVersion previousVersion) {
         final Configuration configuration = platform.getContext().getConfiguration();
-        if (state.getReadableStates(ConsistencyService.NAME).isEmpty()) {
-            OtterStateInitializer.initOtterAppState(state, version, appServices);
+        if (! appServices.isEmpty()) {
+            final boolean stateNotInitialized = appServices.stream()
+                    .map(OtterService::name)
+                    .map(state::getReadableStates)
+                    .allMatch(ReadableStates::isEmpty);
+            if (stateNotInitialized) {
+                OtterStateInitializer.initOtterAppState(state, version, appServices);
+            }
         }
 
         for (final OtterService service : allServices) {
