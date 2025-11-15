e0836827a558c522d52567654c2e6b4dc8394a9a
mxtartaglia <maxi@swirldslabs.com>
2025-11-15 00:33:35 -0300

    chore(22069): create otter test
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/src/testOtter/java/org/hiero/otter/test/SingleNodeNetworkTest.java
+++ platform-sdk/consensus-otter-tests/src/testOtter/java/org/hiero/otter/test/SingleNodeNetworkTest.java
@@ -12,24 +12,36 @@ import static org.hiero.otter.fixtures.OtterAssertions.assertContinuouslyThat;
 import static org.hiero.otter.fixtures.OtterAssertions.assertThat;
 import static org.hiero.otter.fixtures.assertions.StatusProgressionStep.target;
 
+import com.swirlds.common.config.StateCommonConfig;
+import com.swirlds.common.io.utility.FileUtils;
+import com.swirlds.config.api.Configuration;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import org.hiero.otter.fixtures.Network;
 import org.hiero.otter.fixtures.Node;
 import org.hiero.otter.fixtures.OtterTest;
 import org.hiero.otter.fixtures.TestEnvironment;
 import org.hiero.otter.fixtures.TimeManager;
+import org.hiero.otter.fixtures.result.MultipleNodePlatformStatusResults;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * Tests basic functionality of a single node network. Single node networks would probably never be used in production,
  * but they can be useful for testing and are officially supported.
  */
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class SingleNodeNetworkTest {
 
     /**
      * A basic test that a single node network can reach consensus and freeze correctly.
      */
     @OtterTest
+    @Order(0)
     void testSingleNodeNetwork(@NonNull final TestEnvironment env) {
         final Network network = env.network();
         final TimeManager timeManager = env.timeManager();
@@ -57,5 +69,68 @@ public class SingleNodeNetworkTest {
         assertThat(freezeRound)
                 .withFailMessage("10 seconds should be enough time for a single node to reach at least round 20")
                 .isGreaterThan(20);
+    }
+
+    private static Path tempDir;
+
+    static {
+        try {
+            tempDir = Path.of("tmp");
+            if (Files.exists(tempDir)) FileUtils.deleteDirectory(tempDir);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    FileUtils.deleteDirectory(tempDir);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }));
+        } catch (IOException e) {
+            System.out.println(tempDir);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OtterTest
+    @Order(2)
+    void testSingleNodeNetwork2(@NonNull final TestEnvironment environment) throws IOException {
+        final Network network = environment.network();
+        final TimeManager timeManager = environment.timeManager();
+
+        network.addNodes(1);
+        network.start();
+        timeManager.waitFor(Duration.ofSeconds(10L));
+        network.freeze();
+
+        network.shutdown();
+        final Configuration configuration =
+                network.nodes().getLast().configuration().current();
+        final Path outputDirectory =
+                configuration.getConfigData(StateCommonConfig.class).savedStateDirectory();
+        System.out.println(outputDirectory);
+        FileUtils.copyDirectory(outputDirectory, tempDir);
+    }
+
+    @OtterTest
+    @Order(3)
+    void testSingleNodeNetwork3(@NonNull final TestEnvironment env) throws IOException {
+        final Network network = env.network();
+        network.savedStateDirectory(tempDir);
+        final TimeManager timeManager = env.timeManager();
+        final Node node0 = network.addNode();
+        final Node node1 = network.addNode();
+
+        // Setup continuous assertions
+        assertContinuouslyThat(node0.newLogResult()).hasNoErrorLevelMessages();
+        assertContinuouslyThat(node1.newLogResult()).hasNoErrorLevelMessages();
+
+        network.start();
+        // Let the single node run for a short time
+        timeManager.waitFor(Duration.ofSeconds(10));
+        // Verify that the node was healthy prior to being killed
+        final MultipleNodePlatformStatusResults networkStatusResults = network.newPlatformStatusResults();
+        assertThat(networkStatusResults)
+                .haveSteps(target(ACTIVE).requiringInterim(REPLAYING_EVENTS, OBSERVING, CHECKING));
+        networkStatusResults.clear();
     }
 }
