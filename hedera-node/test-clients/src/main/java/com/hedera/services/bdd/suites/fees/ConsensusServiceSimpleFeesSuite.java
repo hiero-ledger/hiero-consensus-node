bdb30b260ca29029f6526a8f55c48dcb133a851c
Josh Marinacci <joshua@marinacci.org>
2025-11-17 12:47:34 -0800

    move comapreSimpleToOld to UtilVerbs. Add second value for comparing the old fees to.
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ConsensusServiceSimpleFeesSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ConsensusServiceSimpleFeesSuite.java
@@ -3,32 +3,19 @@ package com.hedera.services.bdd.suites.fees;
 
 import static com.hedera.services.bdd.junit.TestTags.MATS;
 import static com.hedera.services.bdd.junit.TestTags.SIMPLE_FEES;
-import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
-import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.createTopic;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.deleteTopic;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.submitMessageTo;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.updateTopic;
-import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.assertionsHold;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.compareSimpleToOld;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.overriding;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HBAR;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
-import static com.hedera.services.bdd.suites.crypto.CryptoTransferSuite.sdec;
-import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.hedera.services.bdd.junit.HapiTestLifecycle;
 import com.hedera.services.bdd.junit.LeakyHapiTest;
-import com.hedera.services.bdd.spec.HapiSpec;
-import com.hedera.services.bdd.spec.SpecOperation;
-import com.hedera.services.bdd.spec.utilops.CustomSpecAssert;
-import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
@@ -39,53 +26,6 @@ import org.junit.jupiter.api.Tag;
 @Tag(MATS)
 @HapiTestLifecycle
 public class ConsensusServiceSimpleFeesSuite {
-
-    @FunctionalInterface
-    public interface OpsProvider {
-        List<SpecOperation> provide();
-    }
-
-    private static CustomSpecAssert validateChargedSimpleFees(
-            String name, String txn, double expectedUsd, double allowedPercentDiff) {
-        return assertionsHold((spec, assertLog) -> {
-            final var actualUsdCharged = getChargedUsed(spec, txn);
-            assertEquals(
-                    expectedUsd,
-                    actualUsdCharged,
-                    (allowedPercentDiff / 100.0) * expectedUsd,
-                    String.format(
-                            "%s: %s fee (%s) more than %.2f percent different than expected!",
-                            name, sdec(actualUsdCharged, 4), txn, allowedPercentDiff));
-        });
-    }
-
-    private static double getChargedUsed(@NonNull final HapiSpec spec, @NonNull final String txn) {
-        requireNonNull(spec);
-        requireNonNull(txn);
-        var subOp = getTxnRecord(txn).logged();
-        allRunFor(spec, subOp);
-        final var rcd = subOp.getResponseRecord();
-        return (1.0 * rcd.getTransactionFee())
-                / ONE_HBAR
-                / rcd.getReceipt().getExchangeRate().getCurrentRate().getHbarEquiv()
-                * rcd.getReceipt().getExchangeRate().getCurrentRate().getCentEquiv()
-                / 100;
-    }
-
-    private Stream<DynamicTest> compareSimpleToOld(
-            OpsProvider provider, String txName, double fee, double simpleDiff, double oldDiff) {
-        List<SpecOperation> opsList = new ArrayList<>();
-
-        opsList.add(overriding("fees.simpleFeesEnabled", "true"));
-        opsList.addAll(provider.provide());
-        opsList.add(validateChargedSimpleFees("Simple Fees", txName, fee, simpleDiff));
-
-        opsList.add(overriding("fees.simpleFeesEnabled", "false"));
-        opsList.addAll(provider.provide());
-        opsList.add(validateChargedSimpleFees("Old Fees", txName, fee, oldDiff));
-
-        return hapiTest(opsList.toArray(new SpecOperation[opsList.size()]));
-    }
 
     @Nested
     class TopicFeesComparison {
@@ -106,6 +46,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "create-topic-txn",
                     0.01009,
                     1,
+                    0.01009,
                     1);
         }
 
@@ -125,6 +66,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "create-topic-admin-txn",
                     0.02109,
                     1,
+                    0.02109,
                     30);
         }
 
@@ -144,6 +86,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "create-topic-admin-txn",
                     0.02009,
                     1,
+                    0.02009,
                     100);
         }
 
@@ -166,6 +109,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "update-topic-txn",
                     0.000310,
                     1,
+                    0.000310,
                     1000);
         }
 
@@ -191,6 +135,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "update-topic-txn",
                     0.00131,
                     1,
+                    0.00131,
                     100);
         }
 
@@ -221,6 +166,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "submit-message-txn",
                     0.0001900,
                     1,
+                    0.0001900,
                     90);
         }
 
@@ -251,6 +197,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "submit-message-txn",
                     0.000200,
                     1,
+                    0.000200,
                     40);
         }
 
@@ -299,6 +246,7 @@ public class ConsensusServiceSimpleFeesSuite {
                     "delete-topic-txn",
                     0.005,
                     1,
+                    0.005,
                     10);
         }
     }
