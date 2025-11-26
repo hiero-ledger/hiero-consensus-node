997c4bcb3abeb7b6864d1d10872af428afed3d2a
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2025-11-25 23:33:18 -0600

    update submit message fees
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ConsensusServiceFeesSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ConsensusServiceFeesSuite.java
@@ -33,7 +33,7 @@ public class ConsensusServiceFeesSuite {
     private static final double TOPIC_CREATE_WITH_FIVE_CUSTOM_FEES = 2.10;
     private static final double BASE_FEE_TOPIC_UPDATE = 0.00022;
     private static final double BASE_FEE_TOPIC_DELETE = 0.005;
-    private static final double BASE_FEE_TOPIC_SUBMIT_MESSAGE = 0.0001;
+    private static final double BASE_FEE_TOPIC_SUBMIT_MESSAGE = 0.0008;
     private static final double BASE_FEE_TOPIC_GET_INFO = 0.0001;
 
     private static final String PAYER = "payer";
