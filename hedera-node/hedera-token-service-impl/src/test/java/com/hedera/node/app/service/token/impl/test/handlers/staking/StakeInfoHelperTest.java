c62071c4eccc03c873eb1346aeb701c5b8b8d468
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-12-02 16:15:00 +0200

    Fix unit test
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/staking/StakeInfoHelperTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/staking/StakeInfoHelperTest.java
@@ -128,7 +128,10 @@ class StakeInfoHelperTest {
         final var newStates = newStatesInstance(stakingInfosState);
         infoStore = new WritableStakingInfoStore(newStates, entityIdStore);
         entityIdStore.adjustEntityCount(EntityType.STAKING_INFO, 4L);
-        entityIdStore.adjustEntityCount(EntityType.NODE, 4L);
+        // since FakeNetworkInfo has node 8, set the highest node id to 8
+        for (int i = 0; i < 8; i++) {
+            entityIdStore.incrementHighestNodeIdAndGet();
+        }
         // Platform address book has node Ids 2, 4, 8
         final var networkInfo = new FakeNetworkInfo();
 
