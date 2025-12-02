654fdb26d43856a190b0c60a73044b3493b70025
Tim Farber-Newman <tim.farber-newman@swirldslabs.com>
2025-12-02 10:48:23 -0600

    fix modules
    Signed-off-by: Tim Farber-Newman <tim.farber-newman@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/module-info.java
+++ hedera-node/hedera-app/src/main/java/module-info.java
@@ -35,6 +35,8 @@ module com.hedera.node.app {
     requires transitive io.grpc.stub;
     requires transitive io.grpc;
     requires transitive io.helidon.grpc.core;
+    requires transitive io.helidon.webclient.grpc;
+    requires transitive io.helidon.webclient.http2;
     requires transitive javax.inject;
     requires transitive org.apache.logging.log4j;
     requires transitive org.hyperledger.besu.datatypes;
@@ -58,7 +60,6 @@ module com.hedera.node.app {
     requires io.grpc.netty;
     requires io.helidon.common.tls;
     requires io.helidon.webclient.api;
-    requires io.helidon.webclient.grpc;
     requires io.netty.handler;
     requires io.netty.transport.classes.epoll;
     requires io.netty.transport;
