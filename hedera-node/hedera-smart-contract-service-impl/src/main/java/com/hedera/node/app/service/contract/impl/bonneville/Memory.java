fe8ca7888677735ceb22a755f13192b611440cb1
Cliff Click <cliffc@acm.org>
2025-12-03 10:38:34 -0800

    Cleanup memory math

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/bonneville/Memory.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/bonneville/Memory.java
@@ -16,6 +16,12 @@ public class Memory {
     int _len = 0;               // Active, or in-use bytes.  Rounded to 32bytes
     byte[] _mem=new byte[32];   // Raw bytes
 
+    long read( int adr ) {
+        assert (adr&7)==0;      // 8b aligned, caller already checked
+        return adr >= _mem.length ? 0 : read8(_mem,adr);
+    }
+
+
     // Grow to handle len bytes
     private void growMem( int len ) {
         len = (len+31) & -32;   // Round up to nearest 32
@@ -31,8 +37,18 @@ public class Memory {
         _mem = Arrays.copyOf(_mem, newlen);
     }
 
-    // Big-endian write long to any array
-    static void write( byte[] dst, int off, long x ) {
+    // Big-endian read long from any array
+    static long read8( byte[] src, int off ) {
+        long x = 0;
+        for( int i=0; i<8; i++ ) {
+            x <<= 8;
+            x |= src[off + i] & 0xFF;
+        }
+        return x;
+    }
+
+    // Big-endian write long to any array, any offset
+    static void write8( byte[] dst, int off, long x ) {
         for( int i=0; i<8; i++ ) {
             dst[off + 7-i] = (byte)x;
             x >>= 8;
@@ -41,13 +57,12 @@ public class Memory {
 
     // Big-endian write 4xlongs
     void write( int adr, long x0, long x1, long x2, long x3 ) {
-        assert (adr&31)==0;     // 32b aligned, caller already checked
         growMem( adr+32 );
-        /// Big-endian write
-        write(_mem, adr   , x3);
-        write(_mem, adr+ 8, x2);
-        write(_mem, adr+16, x1);
-        write(_mem, adr+24, x0);
+        // Big-endian write
+        write8(_mem, adr   , x3);
+        write8(_mem, adr+ 8, x2);
+        write8(_mem, adr+16, x1);
+        write8(_mem, adr+24, x0);
     }
 
     // Array write.  Caller checks args are positive.
@@ -65,29 +80,14 @@ public class Memory {
         return Bytes.wrap(_mem,off,len);
     }
 
-    // Pretty print
-    static StringBuilder hex( int s, StringBuilder sb ) {
-        int digit = (s>>4) & 0xf;
-        sb.append((char)((digit <= 9 ? '0' : ('A'-10))+digit));
-        digit = s & 0xf;
-        sb.append((char)((digit <= 9 ? '0' : ('A'-10))+digit));
-        return sb;
-    }
-
     @Override public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[[\n");
+        SB sb = new SB().p("[[\n");
         for( int i=0; i<_len; i+=32 ) {
-            sb.append(" ");
-            hex(i>>24,sb);
-            hex(i>>16,sb);
-            hex(i>> 8,sb);
-            hex(i    ,sb);
-            sb.append(": 0x");
+            sb.p(" ").hex4(i).p(": 0x");
             for( int j=i; j<i+32; j++ )
-                hex(_mem[j],sb);
-            sb.append("\n");
+                sb.hex1(_mem[j]);
+            sb.p("\n");
         }
-        return sb.append("]\n").toString();
+        return sb.p("]]\n").toString();
     }
 }
