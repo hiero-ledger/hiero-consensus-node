bf91e33c9800c438865177d7a9140ea1490ad66c
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-12-10 15:39:19 +0200

    feat: Keep self node account id in memory (#22487)
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>
    Co-authored-by: Derek Riley <derek.riley@swirldslabs.com>
    (cherry picked from commit 91953bdfa014948cf3260d5c538a3356626a8c33)

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
@@ -63,6 +63,7 @@ import com.hedera.node.app.info.CurrentPlatformStatusImpl;
 import com.hedera.node.app.info.StateNetworkInfo;
 import com.hedera.node.app.metrics.StoreMetricsServiceImpl;
 import com.hedera.node.app.records.BlockRecordService;
+import com.hedera.node.app.records.impl.producers.formats.SelfNodeAccountIdManagerImpl;
 import com.hedera.node.app.service.addressbook.impl.AddressBookServiceImpl;
 import com.hedera.node.app.service.consensus.impl.ConsensusServiceImpl;
 import com.hedera.node.app.service.contract.impl.ContractServiceImpl;
@@ -1229,6 +1230,7 @@ public final class Hedera implements SwirldMain<MerkleNodeState>, AppContext.Gos
                 platform.getSelfId().id(), state, currentRoster, configProvider, () -> requireNonNull(
                                 genesisNetworkSupplier)
                         .get());
+        final var selfNodeAccountIdManager = new SelfNodeAccountIdManagerImpl(configProvider, networkInfo, state);
         hintsService.initCurrentRoster(currentRoster);
         final var blockHashSigner = blockHashSignerFactory.apply(hintsService, historyService, configProvider);
         // Fully qualified so as to not confuse javadoc
@@ -1256,6 +1258,7 @@ public final class Hedera implements SwirldMain<MerkleNodeState>, AppContext.Gos
                 .migrationStateChanges(migrationStateChanges != null ? migrationStateChanges : new ArrayList<>())
                 .initialStateHash(initialStateHash)
                 .networkInfo(networkInfo)
+                .selfNodeAccountIdManager(selfNodeAccountIdManager)
                 .startupNetworks(startupNetworks)
                 .hintsService(hintsService)
                 .historyService(historyService)
