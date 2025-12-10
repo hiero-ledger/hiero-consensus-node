2be2fe8926a042e29d114c167d8246ffb5d266c2
Josh Marinacci <joshua@marinacci.org>
2025-12-09 17:05:20 -0800

    initial commit
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

---
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/examples/SimpleFeesMirrorNodeExample.java
@@ -0,0 +1,109 @@
+package com.hedera.node.app.examples;
+
+import com.hedera.node.app.hints.impl.HintsServiceImpl;
+import com.hedera.node.app.history.impl.HistoryServiceImpl;
+import com.hedera.node.app.service.consensus.impl.ConsensusServiceImpl;
+import com.hedera.node.app.service.schedule.impl.ScheduleServiceImpl;
+import com.hedera.node.app.service.token.impl.TokenServiceImpl;
+import com.hedera.node.app.service.util.impl.UtilServiceImpl;
+import org.hiero.hapi.support.fees.FeeSchedule;
+import com.hedera.hapi.node.consensus.ConsensusCreateTopicTransactionBody;
+import com.hedera.hapi.node.transaction.TransactionBody;
+import com.hedera.node.app.spi.fees.FeeContext;
+import com.hedera.node.app.spi.fees.QueryFeeCalculator;
+import com.hedera.node.app.spi.fees.ServiceFeeCalculator;
+import com.hedera.node.app.spi.fees.SimpleFeeCalculator;
+import com.hedera.node.app.spi.fees.SimpleFeeCalculatorImpl;
+import com.hedera.node.app.service.file.impl.FileServiceImpl;
+import com.hedera.node.app.service.contract.impl.ContractServiceImpl;
+
+import com.hedera.pbj.runtime.ParseException;
+import com.hedera.pbj.runtime.io.buffer.Bytes;
+import org.hiero.hapi.fees.FeeResult;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hiero.hapi.fees.FeeScheduleUtils.isValid;
+
+/*
+
+Here's how calculating fees works.  Every service has a
+fee calculator class which implements the ServiceFeeCalculator interface.
+
+
+First get an instance of the SimpleFeeCalculator and call the calculateTxFee or calculateQueryFee method.
+This will call the accumulateServiceFee method of the approprate ServiceFeeCalculator, as well as calculating the base fee and
+node and network fees. You pass in the transaction body and the fee context, which contains the simple fee's schedule
+and current exchange rate.
+
+This will return a FeeResult object in USD, which you can convert to hbar using the FeeUtils.feeResultToFees() method
+and the current exchange rate.
+
+
+Questions:
+
+Where is the best place to put this?
+
+How do we properly import the service impls? ConsensusServiceImpl seems to be exported already but
+others like CryptoServiceImpl are not. And some impls need a bunch of parameters to init.
+
+How do I call this main method from within gradle?
+
+
+ */
+public class SimpleFeesMirrorNodeExample {
+
+    static class SimpleFeesCalculatorProvider {
+        SimpleFeeCalculator make(FeeSchedule feeSchedule) {
+            var services = Arrays.asList(
+                    new ConsensusServiceImpl()
+                    ,new FileServiceImpl()
+//                    ,new UtilServiceImpl()
+//                    ,new ContractServiceImpl()
+//                    ,new ScheduleServiceImpl()
+//                    ,new TokenServiceImpl()
+//                    ,new HintsServiceImpl()
+//                    ,new HistoryServiceImpl()
+            );
+
+            Set<ServiceFeeCalculator> serviceFeeCalculators = new HashSet<>();
+            Set<QueryFeeCalculator> queryFeeCalculators = new HashSet<>();
+            for(var service : services) {
+                serviceFeeCalculators.addAll(service.serviceFeeCalculators());
+                queryFeeCalculators.addAll(service.queryFeeCalculators());
+            }
+            return new SimpleFeeCalculatorImpl(feeSchedule, serviceFeeCalculators, queryFeeCalculators);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        System.out.println("calculating a fee");
+
+        // load up the fee schedule from JSON file
+        var input = SimpleFeesMirrorNodeExample.class.getClassLoader().getResourceAsStream("test-schedule.json");
+        var bytes = input.readAllBytes();
+        final FeeSchedule feeSchedule = FeeSchedule.JSON.parse(Bytes.wrap(bytes));
+
+        // check the schedule is valid
+        if (!isValid(feeSchedule)) {
+            throw new Error("invalid fee schedule");
+        }
+
+
+        // create a simple fee calculator
+        SimpleFeeCalculator calc = new SimpleFeesCalculatorProvider().make(feeSchedule);
+
+        // build an example transaction
+        final var op = ConsensusCreateTopicTransactionBody.newBuilder().build();
+        final var txnBody = TransactionBody.newBuilder().consensusCreateTopic(op).build();
+
+        // create a fee context
+        FeeContext feeContext = null;
+
+        FeeResult result = calc.calculateTxFee(txnBody, feeContext);
+        System.out.println("the fees are " + result);
+    }
+}
