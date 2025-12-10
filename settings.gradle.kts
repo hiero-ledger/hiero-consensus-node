d9605a800a5e66bfcb7a2f775677d8eaed529edd
Petar Tonev <petar.tonev@limechain.tech>
2025-12-10 19:14:18 +0200

    revert changes for local development and testing
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement { repositories { mavenLocal() } }
-
 plugins {
     id("org.hiero.gradle.build") version "0.6.2"
     id("com.hedera.pbj.pbj-compiler") version "0.12.8" apply false
