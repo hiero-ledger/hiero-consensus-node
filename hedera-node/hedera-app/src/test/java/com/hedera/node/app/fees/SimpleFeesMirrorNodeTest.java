779493bdc878b7eb81dd00bc73eaa1e844e0c753
Josh Marinacci <joshua@marinacci.org>
2025-12-10 11:31:04 -0800

    move to be a unit test
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/SimpleFeesMirrorNodeTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/SimpleFeesMirrorNodeTest.java
@@ -1,10 +1,14 @@
-package com.hedera.node.app.examples;
+package com.hedera.node.app.fees;
 
+import com.hedera.hapi.node.consensus.ConsensusCreateTopicTransactionBody;
+import org.junit.jupiter.api.Test;
+
+import com.hedera.hapi.node.base.TopicID;
+import com.hedera.hapi.node.consensus.ConsensusSubmitMessageTransactionBody;
 import com.hedera.node.app.service.consensus.impl.ConsensusServiceImpl;
 import com.hedera.node.app.service.schedule.impl.ScheduleServiceImpl;
 import com.hedera.node.app.service.token.impl.TokenServiceImpl;
 import org.hiero.hapi.support.fees.FeeSchedule;
-import com.hedera.hapi.node.consensus.ConsensusCreateTopicTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.spi.fees.FeeContext;
 import com.hedera.node.app.spi.fees.QueryFeeCalculator;
@@ -16,6 +20,8 @@ import com.hedera.node.app.service.file.impl.FileServiceImpl;
 import com.hedera.pbj.runtime.ParseException;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import org.hiero.hapi.fees.FeeResult;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -42,11 +48,18 @@ How do we properly import the service impls? ConsensusServiceImpl seems to be ex
 others like CryptoServiceImpl are not. And some impls need a bunch of parameters to init. Instead
 let's have static lists of fee calculators on each service impl.
 
-How do I call this main method from within gradle?
+How do I call this main method from within gradle?  For now I'll make it be a unit test.
+
+
+The FeeContext is going to be a challenge. Some calculators need the fee context, not just for the
+number of sigs but also for the readable store so they can get, for example, the topic a message
+is being submitted to so it can determine if there are custom fees applied.
 
 
  */
-public class SimpleFeesMirrorNodeExample {
+
+@ExtendWith(MockitoExtension.class)
+public class SimpleFeesMirrorNodeTest {
 
     static class SimpleFeesCalculatorProvider {
         SimpleFeeCalculator make(FeeSchedule feeSchedule) {
@@ -67,11 +80,12 @@ public class SimpleFeesMirrorNodeExample {
         }
     }
 
-    public static void main(String[] args) throws IOException, ParseException {
+    @Test
+    public void doTest() throws IOException, ParseException {
         System.out.println("calculating a fee for a create topic transaction");
 
         // load up the fee schedule from JSON file
-        var input = SimpleFeesMirrorNodeExample.class.getClassLoader().getResourceAsStream("test-schedule.json");
+        var input = SimpleFeesMirrorNodeTest.class.getClassLoader().getResourceAsStream("test-schedule.json");
         var bytes = input.readAllBytes();
         final FeeSchedule feeSchedule = FeeSchedule.JSON.parse(Bytes.wrap(bytes));
 
@@ -85,11 +99,23 @@ public class SimpleFeesMirrorNodeExample {
         SimpleFeeCalculator calc = new SimpleFeesCalculatorProvider().make(feeSchedule);
 
         // build an example transaction
-        final var op = ConsensusCreateTopicTransactionBody.newBuilder().build();
-        final var txnBody = TransactionBody.newBuilder().consensusCreateTopic(op).build();
+//        final var op = ConsensusCreateTopicTransactionBody.newBuilder().build();
+//        final var txnBody = TransactionBody.newBuilder().consensusCreateTopic(op).build();
+//        final var txn = Transaction.newBuilder().body(txnBody).build();
+//        final var numSigs =  txn.sigMap().sigPair().size();
+        final long topicEntityNum = 1L;
+        final TopicID topicId =
+                TopicID.newBuilder().topicNum(topicEntityNum).build();
+
+        final var op = ConsensusSubmitMessageTransactionBody.newBuilder()
+                .topicID(topicId)
+                .message(Bytes.wrap("foo"))
+                .build();
+        final var txnBody =
+                TransactionBody.newBuilder().consensusSubmitMessage(op).build();
 
         // create a fee context
-        FeeContext feeContext = null;
+        FeeContext feeContext = null;//new FeeContextImpl()
 
         FeeResult result = calc.calculateTxFee(txnBody, feeContext);
         System.out.println("the fees are " + result);
