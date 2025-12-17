a038a50eee51dcdfbaccaaa251642df65271045b
Eva <evdokia.georgieva@limechain.tech>
2025-12-17 11:29:36 +0200

    initial negative crypto transfer tests
    Signed-off-by: Eva <evdokia.georgieva@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip1261/CryptoTransferSimpleFeesTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip1261/CryptoTransferSimpleFeesTest.java
@@ -6,8 +6,12 @@ import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.junit.HapiTestLifecycle;
 import com.hedera.services.bdd.junit.support.TestLifecycle;
 import com.hedera.services.bdd.spec.SpecOperation;
+import com.hedera.services.bdd.spec.keys.ControlForKey;
+import com.hedera.services.bdd.spec.keys.KeyShape;
+import com.hedera.services.bdd.spec.keys.SigControl;
 import com.hedera.services.bdd.spec.transactions.token.HapiTokenCreate;
 import com.hedera.services.bdd.spec.transactions.token.HapiTokenMint;
+import com.hedera.services.bdd.spec.transactions.token.TokenMovement;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -17,13 +21,21 @@ import org.junit.jupiter.api.Tag;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.hedera.services.bdd.junit.TestTags.MATS;
 import static com.hedera.services.bdd.junit.TestTags.SIMPLE_FEES;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
+import static com.hedera.services.bdd.spec.keys.ControlForKey.forKey;
+import static com.hedera.services.bdd.spec.keys.KeyShape.SIMPLE;
+import static com.hedera.services.bdd.spec.keys.KeyShape.sigs;
+import static com.hedera.services.bdd.spec.keys.KeyShape.threshOf;
+import static com.hedera.services.bdd.spec.keys.SigControl.OFF;
+import static com.hedera.services.bdd.spec.keys.SigControl.ON;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.mintToken;
@@ -34,6 +46,7 @@ import static com.hedera.services.bdd.spec.transactions.token.TokenMovement.movi
 import static com.hedera.services.bdd.spec.transactions.token.TokenMovement.movingUnique;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.validateChargedUsdWithin;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_BILLION_HBARS;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HBAR;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
@@ -43,17 +56,24 @@ import static com.hedera.services.bdd.suites.hip1261.utils.FeesChargingUtils.exp
 import static com.hedera.services.bdd.suites.hip1261.utils.FeesChargingUtils.expectedCryptoTransferHBARAndFTAndNFTFullFeeUsd;
 import static com.hedera.services.bdd.suites.hip1261.utils.FeesChargingUtils.expectedCryptoTransferHbarFullFeeUsd;
 import static com.hedera.services.bdd.suites.hip1261.utils.FeesChargingUtils.expectedCryptoTransferNFTFullFeeUsd;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INSUFFICIENT_TX_FEE;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_SIGNATURE;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.RECORD_NOT_FOUND;
 import static com.hederahashgraph.api.proto.java.TokenType.FUNGIBLE_COMMON;
 import static com.hederahashgraph.api.proto.java.TokenType.NON_FUNGIBLE_UNIQUE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag(MATS)
 @Tag(SIMPLE_FEES)
 @HapiTestLifecycle
 public class CryptoTransferSimpleFeesTest {
     private static final String PAYER = "payer";
+    private static final String THRESHOLD_PAYER = "thresholdPayer";
     private static final String RECEIVER_ASSOCIATED_FIRST = "receiverAssociatedFirst";
     private static final String RECEIVER_ASSOCIATED_SECOND = "receiverAssociatedSecond";
     private static final String RECEIVER_ASSOCIATED_THIRD = "receiverAssociatedThird";
+    private static final String RECEIVER_UNLIMITED_AUTO_ASSOCIATIONS = "receiverUnlimitedAutoAssociations";
+    private static final String RECEIVER_FREE_AUTO_ASSOCIATIONS = "receiverFreeAutoAssociations";
     private static final String OWNER = "owner";
     private static final String FUNGIBLE_TOKEN = "fungibleToken";
     private static final String FUNGIBLE_TOKEN_2 = "fungibleToken2";
@@ -62,6 +82,8 @@ public class CryptoTransferSimpleFeesTest {
     private static final String NON_FUNGIBLE_TOKEN_3 = "nonFungibleToken3";
     private static final String adminKey = "adminKey";
     private static final String supplyKey = "supplyKey";
+    private static final String PAYER_KEY = "payerKey";
+    private final double tokenAssociateFee = 0.05;
 
 
     @BeforeAll
@@ -1032,6 +1054,65 @@ public class CryptoTransferSimpleFeesTest {
         @Nested
         @DisplayName("Crypto Transfer Unassociated Accounts and Auto-Account Creation Positive Tests")
         class CryptoTransferUnassociatedAccountsAndAutoAccountCreationPositiveTests {
+            @HapiTest
+            @DisplayName("Crypto Transfer FT to Unassociated Account with unlimited Auto-associations - " +
+                    "base fees full charging")
+            final Stream<DynamicTest> cryptoTransferUnassociatedReceiverUnlimitedAutoAssociations_BaseFeesFullCharging() {
+                return hapiTest(flattened(
+                        // create keys, tokens and accounts
+                        createAccountsAndKeys(),
+                        createFungibleTokenWithoutCustomFees(
+                                FUNGIBLE_TOKEN, 100L, OWNER, adminKey),
+
+                        // transfer tokens
+                        cryptoTransfer(
+                                moving(1L, FUNGIBLE_TOKEN).between(OWNER, RECEIVER_UNLIMITED_AUTO_ASSOCIATIONS))
+                                .payingWith(OWNER)
+                                .signedBy(OWNER)
+                                .fee(ONE_HBAR)
+                                .via("ftTransferTxn"),
+                        validateChargedUsdWithin("ftTransferTxn",
+                                (expectedCryptoTransferFTFullFeeUsd(
+                                        1,
+                                        0,
+                                        2,
+                                        1,
+                                        0) + tokenAssociateFee), 0.001)));
+            }
+
+            @HapiTest
+            @DisplayName("Crypto Transfer FT to Unassociated Accounts with unlimited and free Auto-associations - " +
+                    "base fees full charging")
+            final Stream<DynamicTest>
+            cryptoTransferUnassociatedReceiverUnlimitedAndFreeAutoAssociations_ExtrasCharging() {
+                return hapiTest(flattened(
+                        // create keys, tokens and accounts
+                        createAccountsAndKeys(),
+                        createFungibleTokenWithoutCustomFees(
+                                FUNGIBLE_TOKEN, 100L, OWNER, adminKey),
+                        createNonFungibleTokenWithoutCustomFees(
+                                NON_FUNGIBLE_TOKEN, OWNER, supplyKey, adminKey),
+                        mintNFT(NON_FUNGIBLE_TOKEN, 1, 5),
+
+                        // transfer tokens
+                        cryptoTransfer(
+                                moving(20L, FUNGIBLE_TOKEN).between(OWNER, RECEIVER_UNLIMITED_AUTO_ASSOCIATIONS),
+                                movingUnique(NON_FUNGIBLE_TOKEN, 1L, 2L)
+                                        .between(OWNER, RECEIVER_FREE_AUTO_ASSOCIATIONS),
+                                moving(10L, FUNGIBLE_TOKEN)
+                                        .between(RECEIVER_UNLIMITED_AUTO_ASSOCIATIONS, RECEIVER_FREE_AUTO_ASSOCIATIONS))
+                                .payingWith(PAYER)
+                                .signedBy(PAYER, OWNER)
+                                .fee(ONE_HBAR)
+                                .via("ftTransferTxn"),
+                        validateChargedUsdWithin("ftTransferTxn",
+                                (expectedCryptoTransferFTAndNFTFullFeeUsd(
+                                        2,
+                                        0,
+                                        3,
+                                        1,
+                                        2) + tokenAssociateFee * 3), 0.001)));
+            }
 
         }
     }
@@ -1042,7 +1123,92 @@ public class CryptoTransferSimpleFeesTest {
         @Nested
         @DisplayName("Crypto Transfer Simple Fees Failures on Ingest")
         class CryptoTransferSimpleFeesFailuresOnIngest {
+            @HapiTest
+            @DisplayName("Crypto Transfer HBAR, FT and NFT - with invalid signature - fails on ingest")
+            final Stream<DynamicTest> cryptoTransferHBARAndFTAndNFTWithInvalidSignatureFailsOnIngest() {
+                final AtomicLong initialBalance = new AtomicLong();
+                final AtomicLong afterBalance = new AtomicLong();
 
+                // Define a threshold submit key that requires two simple keys signatures
+                KeyShape keyShape = threshOf(2, SIMPLE, SIMPLE);
+
+                // Create invalid signature with both simple keys signing
+                SigControl invalidSig = keyShape.signedWith(sigs(ON, OFF));
+                return hapiTest(flattened(
+                        // create keys, tokens and accounts
+                        createAccountsAndKeys(),
+                        newKeyNamed(PAYER_KEY).shape(keyShape),
+                        cryptoCreate(THRESHOLD_PAYER)
+                                .key(PAYER_KEY)
+                                .sigControl(forKey(PAYER_KEY, invalidSig))
+                                .balance(ONE_HUNDRED_HBARS),
+                        getAccountBalance(THRESHOLD_PAYER).exposingBalanceTo(initialBalance::set),
+                        createFungibleTokenWithoutCustomFees(
+                                FUNGIBLE_TOKEN, 100L, OWNER, adminKey),
+                        tokenAssociate(RECEIVER_ASSOCIATED_FIRST, FUNGIBLE_TOKEN),
+                        createNonFungibleTokenWithoutCustomFees(
+                                NON_FUNGIBLE_TOKEN, OWNER, supplyKey, adminKey),
+                        tokenAssociate(RECEIVER_ASSOCIATED_FIRST, NON_FUNGIBLE_TOKEN),
+                        mintNFT(NON_FUNGIBLE_TOKEN, 1, 5),
+
+                        // transfer tokens
+                        cryptoTransfer(
+                                movingHbar(1L).between(OWNER, RECEIVER_ASSOCIATED_FIRST),
+                                moving(10L, FUNGIBLE_TOKEN).between(OWNER, RECEIVER_ASSOCIATED_FIRST),
+                                movingUnique(NON_FUNGIBLE_TOKEN, 1L).between(OWNER, RECEIVER_ASSOCIATED_FIRST))
+                                .payingWith(PAYER)
+                                .signedBy(OWNER, THRESHOLD_PAYER)
+                                .fee(ONE_HBAR)
+                                .via("ftTransferTxn")
+                                .hasPrecheck(INVALID_SIGNATURE),
+
+                        // assert no txn record is created
+                        getTxnRecord("ftTransferTxn").logged().hasAnswerOnlyPrecheckFrom(RECORD_NOT_FOUND),
+
+                        // Save balances and assert changes
+                        getAccountBalance(THRESHOLD_PAYER).exposingBalanceTo(afterBalance::set),
+                        withOpContext((spec, log) -> {
+                            assertEquals(initialBalance.get(), afterBalance.get());
+                        })));
+            }
+
+            @HapiTest
+            @DisplayName("Crypto Transfer HBAR, FT and NFT - with insufficient txn fee - fails on ingest")
+            final Stream<DynamicTest> cryptoTransferHBARAndFTAndNFTWithInsufficientTxnFailsOnIngest() {
+                final AtomicLong initialBalance = new AtomicLong();
+                final AtomicLong afterBalance = new AtomicLong();
+                return hapiTest(flattened(
+                        // create keys, tokens and accounts
+                        createAccountsAndKeys(),
+                        getAccountBalance(PAYER).exposingBalanceTo(initialBalance::set),
+                        createFungibleTokenWithoutCustomFees(
+                                FUNGIBLE_TOKEN, 100L, OWNER, adminKey),
+                        tokenAssociate(RECEIVER_ASSOCIATED_FIRST, FUNGIBLE_TOKEN),
+                        createNonFungibleTokenWithoutCustomFees(
+                                NON_FUNGIBLE_TOKEN, OWNER, supplyKey, adminKey),
+                        tokenAssociate(RECEIVER_ASSOCIATED_FIRST, NON_FUNGIBLE_TOKEN),
+                        mintNFT(NON_FUNGIBLE_TOKEN, 1, 5),
+
+                        // transfer tokens
+                        cryptoTransfer(
+                                movingHbar(1L).between(OWNER, RECEIVER_ASSOCIATED_FIRST),
+                                moving(10L, FUNGIBLE_TOKEN).between(OWNER, RECEIVER_ASSOCIATED_FIRST),
+                                movingUnique(NON_FUNGIBLE_TOKEN, 1L).between(OWNER, RECEIVER_ASSOCIATED_FIRST))
+                                .payingWith(PAYER)
+                                .signedBy(OWNER, PAYER)
+                                .fee(ONE_HBAR / 1000) // insufficient fee
+                                .via("ftTransferTxn")
+                                .hasPrecheck(INSUFFICIENT_TX_FEE),
+
+                        // assert no txn record is created
+                        getTxnRecord("ftTransferTxn").logged().hasAnswerOnlyPrecheckFrom(RECORD_NOT_FOUND),
+
+                        // Save balances and assert changes
+                        getAccountBalance(PAYER).exposingBalanceTo(afterBalance::set),
+                        withOpContext((spec, log) -> {
+                            assertEquals(initialBalance.get(), afterBalance.get());
+                        })));
+            }
         }
 
         @Nested
@@ -1099,6 +1265,12 @@ public class CryptoTransferSimpleFeesTest {
                 cryptoCreate(RECEIVER_ASSOCIATED_FIRST).balance(ONE_HBAR),
                 cryptoCreate(RECEIVER_ASSOCIATED_SECOND).balance(ONE_HBAR),
                 cryptoCreate(RECEIVER_ASSOCIATED_THIRD).balance(ONE_HBAR),
+                cryptoCreate(RECEIVER_UNLIMITED_AUTO_ASSOCIATIONS)
+                        .maxAutomaticTokenAssociations(-1)
+                        .balance(ONE_HBAR),
+                cryptoCreate(RECEIVER_FREE_AUTO_ASSOCIATIONS)
+                        .maxAutomaticTokenAssociations(2)
+                        .balance(ONE_HBAR),
                 newKeyNamed(adminKey),
                 newKeyNamed(supplyKey)
         );
