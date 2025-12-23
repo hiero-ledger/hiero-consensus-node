747e14a56fcaa87930cd76272e48e2a77d0fbd59
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-12-23 07:37:25 +0100

    chore: Remove PlatformContext (#22711)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/preconsensus/DefaultInlinePcesWriterTest.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/preconsensus/DefaultInlinePcesWriterTest.java
@@ -2,10 +2,12 @@
 package com.swirlds.platform.event.preconsensus;
 
 import com.swirlds.base.test.fixtures.time.FakeTime;
+import com.swirlds.base.time.Time;
 import com.swirlds.common.context.PlatformContext;
 import com.swirlds.common.test.fixtures.platform.TestPlatformContextBuilder;
 import com.swirlds.config.api.Configuration;
 import com.swirlds.config.extensions.test.fixtures.TestConfigBuilder;
+import com.swirlds.metrics.api.Metrics;
 import com.swirlds.platform.test.fixtures.event.PcesWriterTestUtils;
 import com.swirlds.platform.test.fixtures.event.generator.StandardGraphGenerator;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -61,8 +63,12 @@ class DefaultInlinePcesWriterTest {
 
         final PcesFileTracker pcesFiles = new PcesFileTracker();
 
-        final PcesFileManager fileManager = new PcesFileManager(platformContext, pcesFiles, tempDir, 0);
-        final DefaultInlinePcesWriter writer = new DefaultInlinePcesWriter(platformContext, fileManager, selfId);
+        final Configuration configuration = platformContext.getConfiguration();
+        final Metrics metrics = platformContext.getMetrics();
+        final Time time = platformContext.getTime();
+        final PcesFileManager fileManager = new PcesFileManager(configuration, metrics, time, pcesFiles, tempDir, 0);
+        final DefaultInlinePcesWriter writer =
+                new DefaultInlinePcesWriter(configuration, metrics, time, fileManager, selfId);
 
         writer.beginStreamingNewEvents();
         for (final PlatformEvent event : events) {
@@ -85,8 +91,12 @@ class DefaultInlinePcesWriterTest {
         final int stepsUntilAncient = random.nextInt(50, 100);
         final PcesFileTracker pcesFiles = new PcesFileTracker();
 
-        final PcesFileManager fileManager = new PcesFileManager(platformContext, pcesFiles, tempDir, 0);
-        final DefaultInlinePcesWriter writer = new DefaultInlinePcesWriter(platformContext, fileManager, selfId);
+        final Configuration configuration = platformContext.getConfiguration();
+        final Metrics metrics = platformContext.getMetrics();
+        final Time time = platformContext.getTime();
+        final PcesFileManager fileManager = new PcesFileManager(configuration, metrics, time, pcesFiles, tempDir, 0);
+        final DefaultInlinePcesWriter writer =
+                new DefaultInlinePcesWriter(configuration, metrics, time, fileManager, selfId);
 
         // We will add this event at the very end, it should be ancient by then
         final PlatformEvent ancientEvent = generator.generateEventWithoutIndex().getBaseEvent();
