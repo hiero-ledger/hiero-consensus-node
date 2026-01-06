1425b2f1c4d3de8b27180d181b84ef8c49ca27ea
mxtartaglia <maxi@swirldslabs.com>
2026-01-06 10:34:15 -0300

    chore(20927): Code review comments
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-cli/build.gradle.kts
+++ platform-sdk/swirlds-cli/build.gradle.kts
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins { id("org.hiero.gradle.module.application") }
 
-mainModuleInfo {}
-
-testModuleInfo {}
-
 tasks.jar {
     // Gradle fails to track 'configurations.runtimeClasspath' as an input to the task if it is
     // only used in the 'manifest.attributes'. Hence, we explicitly add it as input.
@@ -30,10 +26,10 @@ val sdkDir = layout.projectDirectory.dir("../sdk")
 tasks.register<Copy>("copyApps") {
     destinationDir = layout.projectDirectory.dir("../sdk").asFile
     from(tasks.jar)
-    into("data/lib") { from(project.configurations.runtimeClasspath) }
+    into("data/lib") { from(configurations.runtimeClasspath) }
 }
 
-application.mainClass.set("org.hiero.consensus.pcli.Pcli")
+application.mainClass = "org.hiero.consensus.pcli.Pcli"
 
 val cleanRun =
     tasks.register<Delete>("cleanRun") {
