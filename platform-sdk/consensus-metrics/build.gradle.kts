0f38b417df1b7616b8dbbe6d1d53733270b89859
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-01-08 19:48:40 +0100

    Move unit tests of the metrics classes to consensus-metrics
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-metrics/build.gradle.kts
+++ platform-sdk/consensus-metrics/build.gradle.kts
@@ -10,3 +10,19 @@ mainModuleInfo {
     annotationProcessor("com.swirlds.config.processor")
     annotationProcessor("com.google.auto.service.processor")
 }
+
+testModuleInfo {
+    requires("com.swirlds.base.test.fixtures")
+    requires("com.swirlds.common.test.fixtures")
+    requires("com.swirlds.config.extensions")
+    requires("com.swirlds.config.extensions.test.fixtures")
+    requires("com.swirlds.metrics.impl")
+    requires("simpleclient")
+    requires("org.hiero.base.crypto.test.fixtures")
+    requires("org.hiero.base.utility.test.fixtures")
+    requires("org.assertj.core")
+    requires("org.junit.jupiter.api")
+    requires("org.junit.jupiter.params")
+    requires("org.mockito")
+    requires("org.mockito.junit.jupiter")
+}
