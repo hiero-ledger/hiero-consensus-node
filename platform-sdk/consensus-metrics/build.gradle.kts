5594855b67392dd4978ea7cd83eb18eddab5b6b8
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-01-09 08:18:29 +0100

    Some fixes
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-metrics/build.gradle.kts
+++ platform-sdk/consensus-metrics/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.publish-artifactregistry")
+    id("org.hiero.gradle.feature.test-timing-sensitive")
 }
 
 description = "Consensus Metrics"
@@ -13,16 +14,25 @@ mainModuleInfo {
 
 testModuleInfo {
     requires("com.swirlds.base.test.fixtures")
-    requires("com.swirlds.common.test.fixtures")
     requires("com.swirlds.config.extensions")
     requires("com.swirlds.config.extensions.test.fixtures")
-    requires("com.swirlds.metrics.impl")
-    requires("simpleclient")
-    requires("org.hiero.base.crypto.test.fixtures")
-    requires("org.hiero.base.utility.test.fixtures")
     requires("org.assertj.core")
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
+    requires("org.mockito")
+    requires("org.mockito.junit.jupiter")
+}
+
+timingSensitiveModuleInfo {
+    requires("com.swirlds.base")
+    requires("com.swirlds.base.test.fixtures")
+    requires("com.swirlds.config.api")
+    requires("com.swirlds.config.extensions.test.fixtures")
+    requires("com.swirlds.metrics.api")
+    requires("com.swirlds.metrics.impl")
+    requires("org.hiero.consensus.metrics")
+    requires("org.assertj.core")
+    requires("org.junit.jupiter.api")
     requires("org.mockito")
     requires("org.mockito.junit.jupiter")
 }
