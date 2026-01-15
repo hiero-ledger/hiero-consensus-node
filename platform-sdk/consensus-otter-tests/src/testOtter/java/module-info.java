d55a1ddf838d4d72e63d3143ca8bff3ab3f5cbf8
Lazar Petrovic <lpetrovic05@gmail.com>
2026-01-15 14:09:24 +0100

    qg
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/consensus-otter-tests/src/testOtter/java/module-info.java
+++ platform-sdk/consensus-otter-tests/src/testOtter/java/module-info.java
@@ -3,6 +3,7 @@ open module org.hiero.otter.test {
     requires com.swirlds.common;
     requires com.swirlds.platform.core;
     requires org.hiero.base.crypto;
+    requires org.hiero.consensus.event.creator;
     requires org.hiero.consensus.hashgraph;
     requires org.hiero.otter.fixtures;
     requires org.junit.jupiter.params;
