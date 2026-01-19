243b44e44514b2c7233311f229ff133330a54715
Cliff Click <cliffc@acm.org>
2026-01-19 12:49:04 -0800

    gradle assemble without most of the extras

---
+++ cliff.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./gradlew -x :test-clients:processResources -x :test-clients:javadoc -x :test-clients:compileJava -x :yahcli:yahCliJar -x :yahcli:compileJava -x :yahcli:compileTestJava -x :yahcli:javadoc -x :hapi:javadoc -x :hapi:generatePbjSource -x :hapi:generateProto -x :hapi:javadocJar -x :hapi:compileJava -x :app-service-contract-impl:javadoc -x :app:javadoc -x :PlatformTestingTool:extractIncludeTimingSensitiveProto -x :hedera-state-validator:javadoc $@
