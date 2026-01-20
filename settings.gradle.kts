d17ba32fb080f8afd03eabec86aa8a64123f1781
Jendrik Johannes <jendrik@onepiece.software>
2026-01-20 10:52:38 +0100

    build: update org.hiero.gradle.build to 0.7.0-SNAPSHOT
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://central.sonatype.com/repository/maven-snapshots")
+    }
+}
+
+buildscript {
+    configurations.classpath { resolutionStrategy.cacheChangingModulesFor(0, "seconds") }
+}
+
 plugins {
-    id("org.hiero.gradle.build") version "0.6.4"
+    id("org.hiero.gradle.build") version "0.7.0-SNAPSHOT"
     id("com.hedera.pbj.pbj-compiler") version "0.12.10" apply false
 }
 
