5a4f88e4513e5574c79952afab8ab49c6b4987cb
aderevets <artem.derevets@limechain.tech>
2026-01-20 17:33:31 +0200

    feat: Add simpleFeeSchedules.json to environment folders in hedera-node/configuration
    Signed-off-by: aderevets <artem.derevets@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip993/SystemFileExportsTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip993/SystemFileExportsTest.java
@@ -105,6 +105,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.hiero.base.utility.CommonUtils;
 import org.hiero.consensus.model.node.NodeId;
+import org.hiero.hapi.support.fees.FeeSchedule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
@@ -251,7 +252,7 @@ public class SystemFileExportsTest {
     @GenesisHapiTest
     final Stream<DynamicTest> syntheticSimpleFeesSchedulesUpdateHappensAtUpgradeBoundary() throws ParseException {
         final var simpleFeesJson = resourceAsString("testSystemFiles/test-simple-fees.json");
-        final var upgradeSimpleFees = org.hiero.hapi.support.fees.FeeSchedule.PROTOBUF.parse(
+        final var upgradeSimpleFees = FeeSchedule.PROTOBUF.parse(
                 Bytes.wrap(SYS_FILE_SERDES.get(113L).toRawFile(simpleFeesJson, null)));
         return hapiTest(
                 recordStreamMustIncludePassFrom(selectedItems(
@@ -459,9 +460,9 @@ public class SystemFileExportsTest {
         return CurrentAndNextFeeSchedule.parseFrom(bytes);
     }
 
-    private static org.hiero.hapi.support.fees.FeeSchedule parseSimpleFeeSchedule(final byte[] bytes)
+    private static FeeSchedule parseSimpleFeeSchedule(final byte[] bytes)
             throws ParseException {
-        return org.hiero.hapi.support.fees.FeeSchedule.PROTOBUF.parse(Bytes.wrap(bytes));
+        return FeeSchedule.PROTOBUF.parse(Bytes.wrap(bytes));
     }
 
     private static ThrottleDefinitions parseThrottleDefs(final byte[] bytes) throws InvalidProtocolBufferException {
@@ -473,7 +474,7 @@ public class SystemFileExportsTest {
     }
 
     private interface ParseFunction<T> {
-        T parse(@NonNull byte[] bytes) throws Exception;
+        T parse(@NonNull byte[] bytes) throws InvalidProtocolBufferException, ParseException;
     }
 
     private static VisibleItemsValidator nodeUpdatesValidator() {
@@ -511,7 +512,7 @@ public class SystemFileExportsTest {
             final T actual;
             try {
                 actual = parser.parse(synthOp.getContents().toByteArray());
-            } catch (Exception e) {
+            } catch (ParseException | InvalidProtocolBufferException e) {
                 throw new IllegalStateException(fileNumProperty + " update was not parseable", e);
             }
             assertEquals(expectedValue, actual);
