feb8a9b484660c7700df5c6b8f42f18803b52017
Michael Tinker <michael.tinker@swirldslabs.com>
2026-01-21 13:13:07 -0600

    spotless
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/record/BlockRecordServiceTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/record/BlockRecordServiceTest.java
@@ -4,24 +4,12 @@ package com.hedera.node.app.workflows.handle.record;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.hedera.node.app.records.BlockRecordService;
-import com.swirlds.state.lifecycle.MigrationContext;
-import com.swirlds.state.lifecycle.SchemaRegistry;
-import com.swirlds.state.spi.WritableSingletonState;
-import com.swirlds.state.spi.WritableStates;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
 @ExtendWith(MockitoExtension.class)
 final class BlockRecordServiceTest {
-    private @Mock SchemaRegistry schemaRegistry;
-    private @Mock MigrationContext migrationContext;
-    private @Mock WritableSingletonState runningHashesState;
-    private @Mock WritableSingletonState blockInfoState;
-    private @Mock WritableStates writableStates;
-
     @Test
     void testGetServiceName() {
         BlockRecordService blockRecordService = new BlockRecordService();
