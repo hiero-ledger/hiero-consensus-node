55c0009531cc0859f18860d24a4788e5f9a91e38
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-01-28 10:24:47 +0100

    Initial implementation
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

---
+++ platform-sdk/consensus-otter-tests/src/testFixtures/java/org/hiero/otter/fixtures/result/ReadablePcesFile.java
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: Apache-2.0
+package org.hiero.otter.fixtures.result;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Instant;
+import org.hiero.consensus.io.IOIterator;
+import org.hiero.consensus.model.event.PlatformEvent;
+import org.hiero.consensus.pces.impl.common.PcesFile;
+
+/**
+ * A ReadablePcesFile provides read-only access to the metadata and events stored in a PCES file.
+ */
+public interface ReadablePcesFile {
+
+    /**
+     * The file extension for standard files. Stands for "PreConsensus Event Stream".
+     */
+    String EVENT_FILE_EXTENSION = PcesFile.EVENT_FILE_EXTENSION;
+
+    /**
+     * The character used to separate fields in the file name.
+     */
+    String EVENT_FILE_SEPARATOR = PcesFile.EVENT_FILE_SEPARATOR;
+
+    /**
+     * Written before the sequence number in the file name. Improves readability for humans.
+     */
+    String SEQUENCE_NUMBER_PREFIX = PcesFile.SEQUENCE_NUMBER_PREFIX;
+
+    /**
+     * Written before the minimum birth round in the file name. Improves readability for humans.
+     */
+    String MINIMUM_BIRTH_ROUND_PREFIX = PcesFile.MINIMUM_BIRTH_ROUND_PREFIX;
+
+    /**
+     * Written before the maximum birth round in the file name. Improves readability for humans.
+     */
+    String MAXIMUM_BIRTH_ROUND_PREFIX = PcesFile.MAXIMUM_BIRTH_ROUND_PREFIX;
+
+    /**
+     * Written before the origin round. Improves readability for humans.
+     */
+    String ORIGIN_PREFIX = PcesFile.ORIGIN_PREFIX;
+
+    /**
+     * @return the timestamp when this file was created (wall clock time)
+     */
+    @NonNull
+    Instant timestamp();
+
+    /**
+     * @return the sequence number of the file. All file sequence numbers are unique. Sequence numbers are allocated in
+     * monotonically increasing order.
+     */
+    long sequenceNumber();
+
+    /**
+     * @return the minimum event bound permitted to be in this file (inclusive), based on the birth round of events.
+     */
+    long lowerBound();
+
+    /**
+     * @return the maximum event generation permitted to be in this file (inclusive), based on the birth round of events.
+     */
+    long upperBound();
+
+    /**
+     * Get the origin of the stream containing this file. A stream's origin is defined as the round number after which
+     * the stream is unbroken. When the origin of two sequential files is different, this signals a discontinuity in the
+     * stream (i.e. the end of one stream and the beginning of another). When replaying events, it is never ok to stream
+     * events from files with different origins.
+     *
+     * @return the origin round number
+     */
+    long origin();
+
+    /**
+     * @return the path to this file
+     */
+    @NonNull
+    Path path();
+
+    /**
+     * Get the file name of this file.
+     *
+     * @return this file's name
+     */
+    @NonNull
+    String fileName();
+
+    /**
+     * Check if it is legal for the file described by this object to contain a particular event.
+     *
+     * @param eventSequenceNumber a sequence number that describes which file an event should be in, based on the birth round of events.
+     * @return true if it is legal for this event to be in the file described by this object
+     */
+    boolean canContain(long eventSequenceNumber);
+
+    /**
+     * Get an iterator that walks over the events in this file. The iterator will only return events that have an
+     * ancient indicator that is greater than or equal to the lower bound.
+     *
+     * @param lowerBound lower bound of the events to return, based on the birth round of events.
+     * @return an iterator over the events in this file
+     * @throws IOException if an I/O error occurs
+     */
+    @NonNull
+    IOIterator<PlatformEvent> iterator(long lowerBound) throws IOException;
+}
