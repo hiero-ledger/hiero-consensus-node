25dd4037917e2ebb058d7235f2cdcf5bfc61f7fa
Alex Kuzmin <alex.kuzmin@swirldslabs.com>
2026-02-03 10:58:03 -0800

    Preparing for PR review
    Signed-off-by: Alex Kuzmin <alex.kuzmin@swirldslabs.com>

--- .github/workflows/support/citr/ansible-playbook-templates/copyKeys.sh
+++ .github/workflows/support/citr/ansible-playbook-templates/copyKeys.sh
@@ -1,4 +1,5 @@
 namespace=$1
+TOOLDIR=`dirname $0`
 
 rm -rf roles/hedera-docker/files/keys-signing/latitude roles/hedera-docker/files/keys-tls/latitude >/dev/null 2>&1
 
@@ -6,7 +7,7 @@ mkdir -p roles/hedera-docker/files/keys-signing/latitude
 mkdir -p roles/hedera-docker/files/keys-tls/latitude
 mkdir inventory
 
-NofNodes=`kubectl -n $namespace get pods | grep 'network-node' | wc -l`
+NofNodes=$(sh ${TOOLDIR}/kubectlt -n ${namespace} get pods | grep 'network-node' | wc -l)
 
 cat << EOF > inventory/latitude.yml
 network:
@@ -17,16 +18,15 @@ network:
   hosts:
 EOF
 
-for i in `seq 1 1 $NofNodes`
+for i in $(seq 1 1 ${NofNodes})
 do
-  node_id=`expr $i - 1`
-  acc=`expr $node_id + 3`
+  node_id=$(expr $i - 1)
+  acc=$(expr $node_id + 3)
 
-  kubectl -n $namespace cp network-node${i}-0:/opt/hgcapp/services-hedera/HapiApp2.0/data/keys/ roles/hedera-docker/files/keys-signing/latitude/
-  kubectl -n $namespace cp network-node${i}-0:/opt/hgcapp/services-hedera/HapiApp2.0/hedera.crt roles/hedera-docker/files/keys-tls/latitude/node${node_id}.crt
+  sh ${TOOLDIR}/kubectlt -n ${namespace} cp network-node${i}-0:/opt/hgcapp/services-hedera/HapiApp2.0/data/keys/ roles/hedera-docker/files/keys-signing/latitude/
+  sh ${TOOLDIR}/kubectlt -n ${namespace} cp network-node${i}-0:/opt/hgcapp/services-hedera/HapiApp2.0/hedera.crt roles/hedera-docker/files/keys-tls/latitude/node${node_id}.crt
 
-#  ip=`kubectl -n $namespace describe pod network-node${i}-0 | grep 'IP:' | head -n 1 | awk '{print $NF}'`
-  ip=`kubectl -n $namespace get svc | grep NodePort | grep "network-node${i}" | awk '{print $3}'`
+  ip=$(sh ${TOOLDIR}/kubectlt -n ${namespace} get svc | grep NodePort | grep "network-node${i}" | awk '{print $3}')
 cat << EOF >> inventory/latitude.yml
     node0$node_id:
       ansible_host: $ip
@@ -34,9 +34,6 @@ cat << EOF >> inventory/latitude.yml
       NODE_NUM: $node_id
 EOF
 done
-
-#      internal_ip: $ip
-
 
 ls -l roles/hedera-docker/files/keys-signing/latitude/
 
@@ -47,12 +44,11 @@ proxy:
       hosts:
 EOF
 
-for i in `seq 1 1 $NofNodes`
+for i in $(seq 1 1 ${NofNodes})
 do
-  node_id=`expr $i - 1`
-  acc=`expr $node_id + 3`
-#  ip=`kubectl -n $namespace describe pod network-node${i}-0 | grep 'IP:' | head -n 1 | awk '{print $NF}'`
-  ip=`kubectl -n $namespace get svc | grep NodePort | grep "network-node${i}" | awk '{print $3}'`
+  node_id=$(expr $i - 1)
+  acc=$(expr $node_id + 3)
+  ip=$(sh ${TOOLDIR}/kubectlt -n ${namespace} get svc | grep NodePort | grep "network-node${i}" | awk '{print $3}')
 
 cat << EOF >> inventory/latitude.yml
         proxy0${node_id}_gcp:
