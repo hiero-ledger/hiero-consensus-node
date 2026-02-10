fe4c487992beba4ae69793b77f208ee8cde222d8
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-02-10 16:21:40 +0100

    Fix automerge
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-gossip-impl/build.gradle.kts
+++ platform-sdk/consensus-gossip-impl/build.gradle.kts
@@ -11,6 +11,7 @@ testModuleInfo {
     requires("com.swirlds.base.test.fixtures")
     requires("com.swirlds.common.test.fixtures")
     requires("com.swirlds.platform.core")
+    requires("com.swirlds.platform.core.test.fixtures")
     requires("com.swirlds.config.extensions")
     requires("com.swirlds.config.extensions.test.fixtures")
     requires("org.hiero.base.crypto.test.fixtures")
