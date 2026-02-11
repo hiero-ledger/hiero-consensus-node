36d36de81a6dd28bb069d941b698ec5cb4558610
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2026-02-11 09:21:29 -0600

    add unit tests

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/ChildFeeContextImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/ChildFeeContextImplTest.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.node.app.fees;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -203,5 +204,12 @@ class ChildFeeContextImplTest {
     @Test
     void delegatesAuthorizer() {
         assertSame(authorizer, subject.authorizer());
+    }
+
+    @Test
+    void delegatesHighVolumeThrottleUtilization() {
+        given(context.getHighVolumeThrottleUtilization(HederaFunctionality.CRYPTO_CREATE)).willReturn(4_321);
+
+        assertEquals(4_321, subject.getHighVolumeThrottleUtilization(HederaFunctionality.CRYPTO_CREATE));
     }
 }
