c42fe22d9ce32c77859c6edead0cbb4e9d109fe7
Kelly Greco <kelly@swirldslabs.com>
2026-02-12 08:29:47 -0500

    add config mappings for moved config values
    Signed-off-by: Kelly Greco <kelly@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/internal/ConfigMappings.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/internal/ConfigMappings.java
@@ -18,6 +18,8 @@ public final class ConfigMappings {
     private ConfigMappings() {}
 
     static final List<ConfigMapping> MAPPINGS = List.of(
+            new ConfigMapping("gossip.connectionServerThreadPriority", "thread.threadPrioritySync"),
+            new ConfigMapping("sync.hangingThreadDuration", "hangingThreadDuration"),
             new ConfigMapping("consensus.roundsNonAncient", "state.roundsNonAncient"),
             new ConfigMapping("consensus.roundsExpired", "state.roundsExpired"),
             new ConfigMapping("consensus.coinFreq", "coinFreq"),
