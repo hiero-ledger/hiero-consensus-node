c0d8682d861df699c78ab01489b57df52a51fd31
Jendrik Johannes <jendrik.johannes@gmail.com>
2026-02-13 07:47:06 +0100

    build: remove redundant shadow plugin configuration (#23499)
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- hedera-node/yahcli/build.gradle.kts
+++ hedera-node/yahcli/build.gradle.kts
@@ -29,22 +29,13 @@ tasks.compileJava { dependsOn(":test-clients:assemble") }
 
 val yahCliJar =
     tasks.register<ShadowJar>("yahCliJar") {
-        archiveClassifier.set("shadow")
-        configurations = listOf(project.configurations.getByName("runtimeClasspath"))
+        archiveClassifier = "shadow"
+        configurations = listOf(project.configurations["runtimeClasspath"])
 
         manifest { attributes("Main-Class" to "com.hedera.services.yahcli.Yahcli") }
 
         // Include all classes and resources from the main source set
         from(sourceSets["main"].output)
-
-        // Also include all service files (except signature-related) in META-INF
-        mergeServiceFiles()
-        exclude(listOf("META-INF/*.DSA", "META-INF/*.RSA", "META-INF/*.SF", "META-INF/INDEX.LIST"))
-
-        // Allow shadow Jar files to have more than 64k entries
-        isZip64 = true
-
-        dependsOn(tasks.compileJava, tasks.classes, tasks.processResources)
     }
 
 tasks.register<Copy>("copyYahCli") {
