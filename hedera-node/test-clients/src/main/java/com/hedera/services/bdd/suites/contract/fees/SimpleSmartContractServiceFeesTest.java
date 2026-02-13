b1211a764da63ad7cd18c95e4c4e8f9f66a6d398
ibankov <ivan.bankov@limechain.tech>
2026-02-13 11:49:03 +0200

    wip
    Signed-off-by: ibankov <ivan.bankov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/fees/SimpleSmartContractServiceFeesTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/fees/SimpleSmartContractServiceFeesTest.java
@@ -137,9 +137,9 @@ public class SimpleSmartContractServiceFeesTest {
                         .nonce(0)
                         .via("ethCall"),
                 // Estimated base fee for EthereumCall is 0.0001 USD and is paid by the relayer account
-                validateChargedUsdWithin("ethCall", EXPECTED_GAS_USED + ETHEREUM_CALL_BASE_FEE, 1),
-                validateChargedUsdForGasOnly("ethCall", EXPECTED_GAS_USED, 1),
-                validateChargedUsdWithoutGas("ethCall", ETHEREUM_CALL_BASE_FEE, 1));
+                validateChargedUsdWithin("ethCall", EXPECTED_GAS_USED + ETHEREUM_CALL_BASE_FEE, 0.1),
+                validateChargedUsdForGasOnly("ethCall", EXPECTED_GAS_USED, 0.1),
+                validateChargedUsdWithoutGas("ethCall", ETHEREUM_CALL_BASE_FEE, 0.1));
     }
 
     @LeakyHapiTest(overrides = "contracts.evm.ethTransaction.zeroHapiFees.enabled")
