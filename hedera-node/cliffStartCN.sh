68e0e181c0141f8f20dbb8adbf1046379525ebb8
Cliff Click <cliffc@acm.org>
2026-02-18 09:55:16 -0800

    Update cliffStartCN.sh

--- hedera-node/cliffStartCN.sh
+++ hedera-node/cliffStartCN.sh
@@ -1,2 +1,6 @@
 # One-line launch & go & debug
-cp configuration/dev/config.txt .; rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*; UseBonnevilleHEVM=1 java -cp '.:data/lib/*:data/apps/*' -Dflag=1 '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006' com.hedera.node.app.ServicesMain -local 0
+#cp configuration/dev/config.txt .; rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*; UseBonnevilleEVM=1 java -cp '.:data/lib/*:data/apps/*' -Dflag=1 '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006' com.hedera.node.app.ServicesMain -local 0
+
+cp configuration/dev/config.txt .
+rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*;
+UseBonnevilleEVM=1 java -cp '.:data/lib/*:data/apps/*' -Dflag=1 com.hedera.node.app.ServicesMain -local 0
