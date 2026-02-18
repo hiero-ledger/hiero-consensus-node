41eeace33fc0511c9ff1529e0683afd99cfa7b80
mxtartaglia <maxi@swirldslabs.com>
2026-02-18 00:10:26 -0300

    feat(23618): Create experiments architecture
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/src/testPerformance/run-benchmark.sh
+++ platform-sdk/consensus-otter-tests/src/testPerformance/run-benchmark.sh
@@ -14,6 +14,7 @@ antiselfishness:AntiSelfishnessExperiment
 maxcreationrate:MaxCreationRateExperiment
 signature:SignatureSchemeExperiment
 combined:CombinedOptimizationsExperiment
+benchmark:ConsensusLayerBenchmark
 "
 
 # Parse arguments
