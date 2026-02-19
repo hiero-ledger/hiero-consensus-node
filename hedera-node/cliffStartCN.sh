796ee6e8e61e375ef179ccbe76f7dff448acbd5e
Cliff Click <cliffc@acm.org>
2026-02-19 09:53:31 -0800

    minor CN test script changes

--- hedera-node/cliffStartCN.sh
+++ hedera-node/cliffStartCN.sh
@@ -1,6 +1,7 @@
 # One-line launch & go & debug
-#cp configuration/dev/config.txt .; rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*; UseBonnevilleEVM=1 java -cp '.:data/lib/*:data/apps/*' -Dflag=1 '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006' com.hedera.node.app.ServicesMain -local 0
-
-cp configuration/dev/config.txt .
-rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*;
+# Nuked when run, so must be copied every time
+cp configuration/dev/genesis-network.json data/config
+# Cleanup from prior run
+rm -rf data/saved data/stats data/tmp data/*treams data/accountBalances output .archive /opt/hgcapp/*
+# Launch and go
 UseBonnevilleEVM=1 java -cp '.:data/lib/*:data/apps/*' -Dflag=1 com.hedera.node.app.ServicesMain -local 0
