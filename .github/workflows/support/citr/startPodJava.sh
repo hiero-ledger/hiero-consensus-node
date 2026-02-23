b355e762209982e6d93abc0c2435a9411fcc075d
Alex Kuzmin <alex.kuzmin@swirldslabs.com>
2026-02-23 13:09:44 -0800

    Addressed comments of code-review
    Signed-off-by: Alex Kuzmin <alex.kuzmin@swirldslabs.com>

--- .github/workflows/support/citr/startPodJava.sh
+++ .github/workflows/support/citr/startPodJava.sh
@@ -1,5 +1,5 @@
-node_id=$1
-isToClean=$2
+node_id=${1}
+isToClean=${2}
 export MALLOC_ARENA_MAX=4
 
 APP_HOME=/opt/hgcapp/services-hedera/HapiApp2.0
@@ -8,7 +8,7 @@ if [[ "${isToClean}" == "clean" ]]
 then
   echo "Cleaning old data ..."
 
-  cd $APP_HOME
+  cd ${APP_HOME}
   rm -rf data/saved/saved/*
   rm -rf data/saved/swirlds-tmp/*
   rm -rf data/saved/preconsensus-events/*/*
@@ -36,10 +36,10 @@ then
 
   for package in `cat packs.txt | awk '{print $1}'`
   do
-   file=`grep $package packs.txt | awk '{print $2}'| sed -e 's@file://@@g'`
-   packagename=`echo $package | awk -F @ '{print $1}'`
+   file=`grep ${package} packs.txt | awk '{print $2}'| sed -e 's@file://@@g'`
+   packagename=`echo ${package} | awk -F @ '{print $1}'`
    unzip -l ${file} | awk '{print $NF}' |  grep '.class' | grep -v 'module-info.class' | sed -e 's/^\(.*\)[\/][^\/][^\/]*.class/\1/g' | sort -u | sed -e 's/[\/]/./g' |\
-   perl -ne "~s/\n//g;print \"--add-reads $packagename=cobertura --add-opens $packagename/\$_=cobertura --add-exports $packagename/\$_=cobertura \"" >>module_reads.txt
+   perl -ne "~s/\n//g;print \"--add-reads ${packagename}=cobertura --add-opens ${packagename}/\$_=cobertura --add-exports $packagename/\$_=cobertura \"" >>module_reads.txt
   done
   export DISABLE_JDK_SERIAL_FILTER=true
   export EXTRA_COBERTURA_OPTS="--add-modules cobertura --add-reads cobertura=ALL-UNNAMED --add-opens cobertura/net.sourceforge.cobertura=ALL-UNNAMED  --add-modules org.slf4j --add-reads cobertura=org.slf4j --add-reads org.slf4j=cobertura $(cat module_reads.txt)"
@@ -71,5 +71,5 @@ JAVA_MAIN_CLASS=com.hedera.node.app.ServicesMain
 LOGNAME=hedera
 PATH=/usr/local/java/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-cd $APP_HOME
+cd ${APP_HOME}
 nohup /usr/bin/env java ${JAVA_HEAP_OPTS} ${JAVA_OPTS} ${EXTRA_COBERTURA_OPTS} -cp "${JAVA_CLASS_PATH}" "${JAVA_MAIN_CLASS}" -local ${node_id} > node.log 2>&1 &
