7024dcbaae155fcbffda486f590a743a9ff7098b
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-02-25 18:39:19 +0100

    Added flaky test
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

---
+++ platform-sdk/consensus-otter-tests/src/test/java/org/hiero/otter/fixtures/FlakyTest.java
@@ -0,0 +1,15 @@
+package org.hiero.otter.fixtures;
+
+import org.junit.jupiter.api.Test;
+
+class FlakyTest {
+
+    @Test
+    void flakyTest() {
+        // This test is intentionally flaky to demonstrate.
+        // It will fail approximately 50% of the time.
+        if (Math.random() < 0.5) {
+            throw new RuntimeException("Flaky test failed!");
+        }
+    }
+}
