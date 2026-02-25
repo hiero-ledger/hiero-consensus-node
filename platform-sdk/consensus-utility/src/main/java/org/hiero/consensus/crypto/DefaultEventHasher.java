a73a044dda366c1a2ebe821b372ea9d08ac42b83
mxtartaglia <maxi@swirldslabs.com>
2026-02-25 09:44:49 -0300

    fix(23741): Event hasher reuses pbj hasher
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/crypto/DefaultEventHasher.java
+++ platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/crypto/DefaultEventHasher.java
@@ -9,11 +9,14 @@ import org.hiero.consensus.model.event.PlatformEvent;
  * Default implementation of the {@link EventHasher}.
  */
 public class DefaultEventHasher implements EventHasher {
+
+    private static final ThreadLocal<PbjStreamHasher> HASHER = ThreadLocal.withInitial(PbjStreamHasher::new);
+
     @Override
     @NonNull
     public PlatformEvent hashEvent(@NonNull final PlatformEvent event) {
         Objects.requireNonNull(event);
-        new PbjStreamHasher().hashEvent(event);
+        HASHER.get().hashEvent(event);
         return event;
     }
 }
