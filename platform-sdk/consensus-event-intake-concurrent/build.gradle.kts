8025e2b748ebd36950603d17f62d2b684aa3abd9
mxtartaglia <maxi@swirldslabs.com>
2026-03-04 18:53:47 -0300

    feat(23903): Concurrent EventIntake
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/consensus-event-intake-concurrent/build.gradle.kts
+++ platform-sdk/consensus-event-intake-concurrent/build.gradle.kts
@@ -2,14 +2,13 @@
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.publish-artifactregistry")
-    id("org.hiero.gradle.feature.benchmark")
 }
 
-description = "Default Consensus Event Intake Implementation"
+description = "Concurrent Consensus Event Intake Implementation"
 
 testModuleInfo {
     requires("com.swirlds.base.test.fixtures")
-    requires("org.hiero.base.crypto.test.fixtures")
+    requires("org.hiero.base.concurrent.test.fixtures")
     requires("org.hiero.base.utility.test.fixtures")
     requires("org.hiero.consensus.model.test.fixtures")
     requires("org.hiero.consensus.utility.test.fixtures")
