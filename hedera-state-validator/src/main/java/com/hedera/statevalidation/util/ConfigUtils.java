2bd52c6cff4cf3d97b32cf8afc58eacd758cb4ec
Ivan Malygin <ivan@swirldslabs.com>
2026-03-06 14:29:29 -0500

    Spotless
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- hedera-state-validator/src/main/java/com/hedera/statevalidation/util/ConfigUtils.java
+++ hedera-state-validator/src/main/java/com/hedera/statevalidation/util/ConfigUtils.java
@@ -119,7 +119,8 @@ public final class ConfigUtils {
                 .withSource(new SimpleConfigSource().withValue("merkleDb.minNumberOfFilesInCompaction", 2))
                 .withSource(new SimpleConfigSource().withValue("merkleDb.maxFileChannelsPerFileReader", FILE_CHANNELS))
                 .withSource(new SimpleConfigSource().withValue("merkleDb.maxThreadsPerFileChannel", 1))
-                .withSource(new SimpleConfigSource().withValue("virtualMap.fullRehashTimeoutMs", FULL_REHASH_TIMEOUT_MS))
+                .withSource(
+                        new SimpleConfigSource().withValue("virtualMap.fullRehashTimeoutMs", FULL_REHASH_TIMEOUT_MS))
                 .withConverter(CongestionMultipliers.class, new CongestionMultipliersConverter())
                 .withConverter(EntityScaleFactors.class, new EntityScaleFactorsConverter())
                 .withConverter(KnownBlockValues.class, new KnownBlockValuesConverter())
