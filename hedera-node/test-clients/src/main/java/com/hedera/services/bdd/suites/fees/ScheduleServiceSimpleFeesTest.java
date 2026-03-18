d28a0f27a5a2737d2c88048d2ab4a3620f916fd8
aderevets <artem.derevets@limechain.tech>
2026-03-18 16:17:27 +0100

    spotless
    Signed-off-by: aderevets <artem.derevets@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ScheduleServiceSimpleFeesTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/fees/ScheduleServiceSimpleFeesTest.java
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.services.bdd.suites.fees;
 
-import static com.hedera.services.bdd.junit.TestTags.SERIAL;
 import static com.hedera.services.bdd.junit.TestTags.SIMPLE_FEES;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.assertions.AccountInfoAsserts.accountWith;
@@ -49,7 +48,6 @@ import com.hedera.services.bdd.junit.HapiTest;
 import java.math.BigInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
