edcc0d12710f369580da8ea575f8e8626f99aaf2
Petar Tonev <petar.tonev@limechain.tech>
2026-03-18 14:25:19 +0200

    remove the change for testing
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/BlockNodeSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/BlockNodeSuite.java
@@ -96,8 +96,7 @@ public class BlockNodeSuite {
                         connectionDropTime::get,
                         Duration.ofMinutes(1),
                         Duration.ofSeconds(45),
-                        String.format(
-                                "Selected sagagsa  block node localhost:%s for connection attempt", portNumbers.get(1)),
+                        String.format("Selected block node localhost:%s for connection attempt", portNumbers.get(1)),
                         String.format(
                                 "/localhost:%s/READY] Connection state transitioned from UNINITIALIZED to READY",
                                 portNumbers.get(1)),
