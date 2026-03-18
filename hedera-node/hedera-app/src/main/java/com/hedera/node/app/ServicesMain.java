0754e56ee4b1537f3d18a8befeedef58fcd6572a
Michael Tinker <michael.tinker@swirldslabs.com>
2026-03-18 17:45:39 -0500

    intermezzo
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/ServicesMain.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/ServicesMain.java
@@ -318,7 +318,7 @@ public class ServicesMain {
                         appContext,
                         new HintsLibraryImpl(),
                         bootstrapConfig.getConfigData(BlockStreamConfig.class).blockPeriod()),
-                (appContext, _) -> new HistoryServiceImpl(
+                (appContext, bootstrapConfig) -> new HistoryServiceImpl(
                         metrics, ForkJoinPool.commonPool(), appContext, new HistoryLibraryImpl()),
                 TssBlockHashSigner::new,
                 configuration,
