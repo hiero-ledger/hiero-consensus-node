96f9fa6315e9d72295e571e399062befadd2bb1f
Josh Marinacci <joshua@marinacci.org>
2026-03-24 15:32:35 -0700

    update formatter
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/JSONFormatter.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/JSONFormatter.java
@@ -3,6 +3,8 @@ package com.hedera.node.app.fees;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 class JSONFormatter {
 
@@ -27,6 +29,19 @@ class JSONFormatter {
         this.start = false;
     }
 
+    public void startObject(String name) throws IOException {
+        if (!this.start) {
+            writer.append(", ");
+        }
+        writer.append(String.format("\"%s\": {", name));
+        this.start = true;
+    }
+
+    public void endObject() throws IOException {
+        writer.append(" }");
+        this.start = false;
+    }
+
     public void endRecord() throws IOException {
         writer.write("}\n");
     }
@@ -43,6 +58,31 @@ class JSONFormatter {
             writer.append(", ");
         }
         writer.append(String.format("\"%s\" : %.5f", name, value));
+    }
+
+    public void key(String name, List<Map<String, Object>> value) throws IOException {
+        if (!this.start) {
+            writer.append(", ");
+        }
+        writer.append(String.format("\"%s\" : [", name));
+        for (int i = 0; i < value.size(); i++) {
+            if (i > 0) writer.append(", ");
+            writer.append("{ ");
+            boolean first = true;
+            for (var entry : value.get(i).entrySet()) {
+                if (!first) writer.append(", ");
+                Object v = entry.getValue();
+                if (v instanceof String s) {
+                    writer.append(String.format("\"%s\":\"%s\"", entry.getKey(), s.replaceAll("\n", " ")));
+                } else {
+                    writer.append(String.format("\"%s\":%s", entry.getKey(), v));
+                }
+                first = false;
+            }
+            writer.append(" }");
+        }
+        writer.append("]");
+        this.start = false;
     }
 
     public void close() throws IOException {
