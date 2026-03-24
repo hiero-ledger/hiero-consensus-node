fe6e17c97383c0c0d27dd740d67167ad4f0a019f
Josh Marinacci <joshua@marinacci.org>
2026-03-24 15:04:33 -0700

    add standalone fee calculator cli
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

---
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/StandaloneFeeCalculatorCLI.java
@@ -0,0 +1,9 @@
+package com.hedera.node.app.fees;
+
+import java.util.Arrays;
+
+public class StandaloneFeeCalculatorCLI {
+    public static void main(String[] args) {
+        System.out.println("args are " + Arrays.toString(args));
+    }
+}
