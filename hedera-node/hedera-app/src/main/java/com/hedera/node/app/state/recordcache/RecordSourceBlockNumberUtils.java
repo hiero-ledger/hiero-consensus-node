5f5613ee59fc7606c86fbf86f32cb396882894a8
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2026-03-25 11:05:51 -0500

    review comments
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/recordcache/RecordSourceBlockNumberUtils.java
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-package com.hedera.node.app.state.recordcache;
-
-final class RecordSourceBlockNumberUtils {
-    private RecordSourceBlockNumberUtils() {}
-}
