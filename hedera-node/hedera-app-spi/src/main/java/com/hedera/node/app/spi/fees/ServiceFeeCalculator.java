e26ff43ccb268645927307b646379113cfa67e86
Josh Marinacci <joshua@marinacci.org>
2026-03-27 11:54:03 -0700

    undo the change
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/fees/ServiceFeeCalculator.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/fees/ServiceFeeCalculator.java
@@ -69,9 +69,9 @@ public interface ServiceFeeCalculator {
                 int included = ref.includedCount();
                 long extraFee = lookupExtraFee(feeSchedule, ref.name()).fee();
                 result.addServiceExtraFeeTinycents(ref.name().name(), extraFee, amount, included);
-                return;
+//                return;
             }
         }
-        throw new IllegalArgumentException("Extra not defined " + extra.name());
+//        throw new IllegalArgumentException("Extra not defined " + extra.name());
     }
 }
