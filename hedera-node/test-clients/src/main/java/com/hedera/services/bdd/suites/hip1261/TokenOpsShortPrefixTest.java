76fe6db217743e1494ed4f3ed63fe9feb5fa8cc0
Alex Kehayov <45170963+AlexKehayov@users.noreply.github.com>
2026-03-31 13:49:52 +0300

    chore: refactoring after XTS speedup changes (#24622)
    Signed-off-by: Alex Kehayov <aleks.kehayov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip1261/TokenOpsShortPrefixTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip1261/TokenOpsShortPrefixTest.java
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.services.bdd.suites.hip1261;
 
-import static com.hedera.services.bdd.junit.TestTags.MATS;
 import static com.hedera.services.bdd.junit.TestTags.SIMPLE_FEES;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.keys.SigMapGenerator.Nature.UNIQUE_PREFIXES;
@@ -67,7 +66,6 @@ import org.junit.jupiter.api.Tag;
  * depending on actual key material, making the exact serialized transaction size
  * non-deterministic at authoring time.
  */
-@Tag(MATS)
 @Tag(SIMPLE_FEES)
 @HapiTestLifecycle
 public class TokenOpsShortPrefixTest {
