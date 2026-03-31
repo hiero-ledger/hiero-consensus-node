9c7e2796752b42ebb8c49f7bff642ee57b9cd8cb
Suvrat Acharya <suvrat1629@gmail.com>
2026-04-01 03:13:15 +0530

    enable staticcall
    Signed-off-by: Suvrat Acharya <suvrat1629@gmail.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hss/getscheduledinfo/GetScheduledFungibleTokenCreateCall.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hss/getscheduledinfo/GetScheduledFungibleTokenCreateCall.java
@@ -37,6 +37,11 @@ public class GetScheduledFungibleTokenCreateCall extends AbstractCall {
         this.scheduleID = scheduleID;
     }
 
+    @Override
+    public boolean allowsStaticFrame() {
+        return true;
+    }
+
     @NonNull
     @Override
     public PricedResult execute(MessageFrame frame) {
