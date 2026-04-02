a8c7110ee2da1babe473af81f3ea28d9af00972a
mxtartaglia <maxi@swirldslabs.com>
2026-04-02 01:28:00 -0300

    ffi
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/consensus-sloth/src/testPerformance/java/org/hiero/sloth/test/performance/experiments/combined/CombinedOptimizationsExperiment.java
+++ platform-sdk/consensus-sloth/src/testPerformance/java/org/hiero/sloth/test/performance/experiments/combined/CombinedOptimizationsExperiment.java
@@ -29,7 +29,7 @@ import org.hiero.sloth.fixtures.specs.SlothSpecs;
             "-XX:+UseZGC",
             "-XX:+ZGenerational",
             //"-XX:+AlwaysPreTouch",
-            "-XX:ConcGCThreads=4",
+            "-XX:ConcGCThreads=1",
             "-Xms4g",
             "-Xmx4g"
         })
