f7a7946e8aa7dafc85b8a69621ffb115bce32c15
Michael Tinker <michael.tinker@swirldslabs.com>
2026-04-29 11:27:36 -0500

    Add BlockHashSigning
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

---
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/hints/impl/BlockHashSigning.java
@@ -0,0 +1,9 @@
+package com.hedera.node.app.hints.impl;
+
+/**
+ * TODO - sealed interface first permitting just HintsContext.Signing and exposing
+ * public methods exactly as used by any current client of HintsContext.Signing
+ * <b>other than</b> {@link com.hedera.node.app.tss.TssBlockHashSigner}
+ */
+public interface BlockHashSigning {
+}
