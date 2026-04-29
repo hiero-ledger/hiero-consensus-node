05613d7f60676d6df6a001713f23a41ac075b4d5
Michael Tinker <michael.tinker@swirldslabs.com>
2026-04-29 11:31:08 -0500

    intermezzo
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/hints/impl/BlockHashSigning.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/hints/impl/BlockHashSigning.java
@@ -4,6 +4,9 @@ package com.hedera.node.app.hints.impl;
  * TODO - sealed interface first permitting just HintsContext.Signing and exposing
  * public methods exactly as used by any current client of HintsContext.Signing
  * <b>other than</b> {@link com.hedera.node.app.tss.TssBlockHashSigner}
+ *
+ * Then eliminate direct use of HintsContext.Signing everywhere except TssBlockHashSigner,
+ * replacing those usage sites with BlockHashSigning
  */
 public interface BlockHashSigning {
 }
