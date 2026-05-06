be669247f59c0b131dcdd518f8c2346e6c50bced
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-05-06 11:57:41 +0200

    Clean up remaining swirlds-common dependencies
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/swirlds-common/build.gradle.kts
+++ platform-sdk/swirlds-common/build.gradle.kts
@@ -36,6 +36,7 @@ timingSensitiveModuleInfo {
     requires("com.swirlds.metrics.api")
     requires("org.hiero.base.concurrent")
     requires("org.hiero.base.utility.test.fixtures")
+    requires("org.hiero.consensus.concurrent.test.fixtures")
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
 }
