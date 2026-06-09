6d89b2d9399c42268ec3a81ddd77df048011bef6
Jendrik Johannes <jendrik@onepiece.software>
2026-06-09 17:32:55 +0200

    build(deps): org.hiero.gradle.build -> 0.7.9
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://central.sonatype.com/repository/maven-snapshots")
-    }
-}
-
-buildscript {
-    configurations.classpath { resolutionStrategy.cacheChangingModulesFor(0, "seconds") }
-}
-
 plugins {
-    id("org.hiero.gradle.build") version "0.7.9-SNAPSHOT"
+    id("org.hiero.gradle.build") version "0.7.9"
     id("com.hedera.pbj.pbj-compiler") version "0.15.8" apply false
 }
 
