03eb22edf5a65dff2e393064ea91eaf3ced493a8
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-06-22 10:48:16 +0200

    Fix tests
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/PlatformConfigurationExtension.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/PlatformConfigurationExtension.java
@@ -15,6 +15,7 @@ import com.swirlds.platform.wiring.PlatformSchedulersConfig;
 import com.swirlds.virtualmap.config.VirtualMapConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.Set;
+import org.hiero.consensus.config.PlatformStatusConfig;
 import org.hiero.consensus.model.node.NodeId;
 
 /**
