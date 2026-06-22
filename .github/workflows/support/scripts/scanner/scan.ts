389036a4155f16d8dfdba10cbc1f7884c620f962
Josh Marinacci <joshua@marinacci.org>
2026-06-22 08:24:49 -0700

    check for invalid uses clauses
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- .github/workflows/support/scripts/scanner/scan.ts
+++ .github/workflows/support/scripts/scanner/scan.ts
@@ -1,9 +1,11 @@
 import path from "node:path"
 import {findFiles, loadWorkflow} from "./util.ts";
+import fs from "node:fs/promises"
 import type {Job} from "./util.ts";
 
 
 const WORKFLOWS_DIR = "../../.."
+const REPO_ROOT = "../../../../.."
 const HARDEN_RUNNER = "step-security/harden-runner"
 const INIT_GITHUB_JOB = "pandaswhocode/initialize-github-job"
 
@@ -24,14 +26,36 @@ function hasInitAndHarden(job: Job) {
 
 const isYaml = (f) => path.extname(f.name) === ".yaml"
 
+async function fileExists(uses: string) {
+    let pth = path.normalize(uses)
+    pth = path.resolve(REPO_ROOT,pth)
+    try {
+        let stats = await fs.stat(pth)
+        return stats.isFile()
+    } catch (e) {
+        return false
+    }
+}
+
 for(let file of await findFiles(WORKFLOWS_DIR, isYaml)) {
     const workflow = await loadWorkflow(file)
     // skip deprecated workflows
     if(workflow.name.startsWith("Deprecated:")) continue
+
+
     // find jobs with both init and harden
     for(let [key,job] of Object.entries(workflow.jobs)) {
         if(hasInitAndHarden(job)) {
             console.log(`workflow ${file.name} job: ${key} has both ${HARDEN_RUNNER} and ${INIT_GITHUB_JOB}`)
+        }
+    }
+
+    // Confirm all uses clauses map to valid files that exist and are readable
+    for(let [key,job] of Object.entries(workflow.jobs)) {
+        if(job.uses) {
+            if(! await fileExists(job.uses)) {
+                console.log(`workflow ${file.name} job: ${key} uses: ${job.uses} points to an invalid file`)
+            }
         }
     }
 }
