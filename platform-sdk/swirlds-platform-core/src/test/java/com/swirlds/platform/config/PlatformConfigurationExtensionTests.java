787d6d8a070c821034b27daced7073e1b8d17229
Artur Kugal <artur.kugal@swirldslabs.com>
2026-06-29 16:14:34 -0400

    Add testConfigTypes test
    Signed-off-by: Artur Kugal <artur.kugal@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/config/PlatformConfigurationExtensionTests.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/config/PlatformConfigurationExtensionTests.java
@@ -3,9 +3,17 @@ package com.swirlds.platform.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.swirlds.component.framework.WiringConfig;
 import com.swirlds.config.api.Configuration;
 import com.swirlds.config.api.ConfigurationBuilder;
 import com.swirlds.config.extensions.test.fixtures.ConfigUtils;
+import com.swirlds.logging.api.internal.configuration.InternalLoggingConfig;
+import com.swirlds.platform.builder.ModulesConfig;
+import com.swirlds.platform.health.OSHealthCheckConfig;
+import com.swirlds.platform.metrics.PlatformMetricsConfig;
+import com.swirlds.platform.uptime.UptimeConfig;
+import com.swirlds.platform.wiring.PlatformSchedulersConfig;
+import java.util.Arrays;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -27,5 +35,20 @@ class PlatformConfigurationExtensionTests {
                     .as("Config data for " + record.getName() + " should be registered.")
                     .isNotNull();
         }
+    }
+
+    @Test
+    void testConfigTypes() {
+        PlatformConfigurationExtension extension = new PlatformConfigurationExtension();
+
+        assertThat(extension.getConfigDataTypes())
+                .containsExactlyInAnyOrderElementsOf(Arrays.asList(
+                        ModulesConfig.class,
+                        OSHealthCheckConfig.class,
+                        PlatformMetricsConfig.class,
+                        PlatformSchedulersConfig.class,
+                        UptimeConfig.class,
+                        WiringConfig.class,
+                        InternalLoggingConfig.class));
     }
 }
