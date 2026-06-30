bf1600b0e926673dea19d9f02cdf5dd8769a6499
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-06-30 11:44:37 +0200

    Move platform exception tests
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/base-utility/src/testFixtures/java/org/hiero/base/utility/test/fixtures/assertions/ExceptionAssertions.java
+++ platform-sdk/base-utility/src/testFixtures/java/org/hiero/base/utility/test/fixtures/assertions/ExceptionAssertions.java
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-package com.swirlds.platform.test.fixtures;
+package org.hiero.base.utility.test.fixtures.assertions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
