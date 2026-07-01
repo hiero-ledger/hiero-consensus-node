49ca945df8a6230d4083a57f6ea73504328ed1c0
Anthony Petrov <anthony@swirldslabs.com>
2026-07-01 14:18:35 -0700

    spotless
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- platform-sdk/base-crypto/src/main/java/module-info.java
+++ platform-sdk/base-crypto/src/main/java/module-info.java
@@ -3,22 +3,8 @@ import com.swirlds.config.api.ConfigurationExtension;
 import org.hiero.base.crypto.config.CryptoConfigurationExtension;
 
 module org.hiero.base.crypto {
-    exports org.hiero.base.crypto;
     exports org.hiero.base.crypto.config;
-
-    /* Targeted exports */
-    exports org.hiero.base.crypto.engine to
-            com.swirlds.common,
-            com.swirlds.common.test.fixtures,
-            org.hiero.base.crypto.test.fixtures;
-
-    opens org.hiero.base.crypto to
-            com.swirlds.platform.core,
-            com.swirlds.common.test.fixtures,
-            com.swirlds.platform.core.test.fixtures,
-            org.hiero.base.crypto.test.fixtures,
-            org.hiero.otter.test,
-            com.fasterxml.jackson.databind;
+    exports org.hiero.base.crypto;
 
     requires transitive com.hedera.pbj.runtime;
     requires transitive com.swirlds.config.api;
@@ -34,6 +20,18 @@ module org.hiero.base.crypto {
     requires org.hyperledger.besu.nativelib.secp256k1;
     requires static transitive com.github.spotbugs.annotations;
 
+    /* Targeted exports */
+    exports org.hiero.base.crypto.engine to
+            com.swirlds.common,
+            com.swirlds.common.test.fixtures,
+            org.hiero.base.crypto.test.fixtures;
+    opens org.hiero.base.crypto to
+            com.swirlds.platform.core,
+            com.swirlds.common.test.fixtures,
+            com.swirlds.platform.core.test.fixtures,
+            org.hiero.base.crypto.test.fixtures,
+            org.hiero.otter.test,
+            com.fasterxml.jackson.databind;
     provides ConfigurationExtension with
             CryptoConfigurationExtension;
 }
