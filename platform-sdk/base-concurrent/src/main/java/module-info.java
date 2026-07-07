4aadfc846e7a2d35f94226ab6033854bb487d1dd
thenswan <nikita.lebedev@limechain.tech>
2026-07-07 19:00:39 +0300

    spotless

--- platform-sdk/base-concurrent/src/main/java/module-info.java
+++ platform-sdk/base-concurrent/src/main/java/module-info.java
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 module org.hiero.base.concurrent {
-    exports org.hiero.base.concurrent;
     exports org.hiero.base.concurrent.atomic;
     exports org.hiero.base.concurrent.futures;
     exports org.hiero.base.concurrent.interrupt;
-    exports org.hiero.base.concurrent.locks;
-    exports org.hiero.base.concurrent.locks.locked;
     exports org.hiero.base.concurrent.jctools.queues;
+    exports org.hiero.base.concurrent.locks.locked;
+    exports org.hiero.base.concurrent.locks;
+    exports org.hiero.base.concurrent;
 
     requires transitive com.swirlds.base;
     requires com.swirlds.logging;
