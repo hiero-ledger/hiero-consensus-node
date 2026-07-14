fcc319fc98b7f4099d80863b9ac947549cd14a96
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-07-15 00:08:22 +0200

    Repeat chaos test five times
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-otter-tests/src/testChaos/java/org/hiero/otter/chaos/ChaosTest.java
+++ platform-sdk/consensus-otter-tests/src/testChaos/java/org/hiero/otter/chaos/ChaosTest.java
@@ -9,6 +9,7 @@ import org.hiero.otter.fixtures.Network;
 import org.hiero.otter.fixtures.OtterTest;
 import org.hiero.otter.fixtures.TestEnvironment;
 import org.hiero.otter.fixtures.chaosbot.ChaosBotConfiguration;
+import org.junit.jupiter.api.RepeatedTest;
 
 /**
  * A test that runs chaos experiments on a network of nodes.
@@ -16,6 +17,7 @@ import org.hiero.otter.fixtures.chaosbot.ChaosBotConfiguration;
 class ChaosTest {
 
     @OtterTest(requires = Capability.RECONNECT)
+    @RepeatedTest(value = 5, failureThreshold = 1)
     void chaosTest(@NonNull final TestEnvironment env) {
         final Network network = env.network();
         network.addNodes(7);
