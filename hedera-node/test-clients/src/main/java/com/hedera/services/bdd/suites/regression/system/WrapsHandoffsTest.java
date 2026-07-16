4e4b26477caddcd753b30ae1ce7d4e28e6c9c131
Petar Tonev <petar.tonev@limechain.tech>
2026-07-16 14:07:34 +0300

    check all nodes logs and increase the log waiting period
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/system/WrapsHandoffsTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/system/WrapsHandoffsTest.java
@@ -2,6 +2,7 @@
 package com.hedera.services.bdd.suites.regression.system;
 
 import static com.hedera.services.bdd.junit.TestTags.WRAPS;
+import static com.hedera.services.bdd.junit.hedera.NodeSelector.allNodes;
 import static com.hedera.services.bdd.junit.hedera.NodeSelector.byNodeId;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
@@ -43,7 +44,7 @@ public class WrapsHandoffsTest implements LifecycleTest {
     private static final String INCREMENTAL_WRAPS_PROOF_STARTED = "Constructing incremental WRAPS proof";
     private static final String INCREMENTAL_WRAPS_PROOF_CONSTRUCTED = "FINISHED constructing incremental WRAPS proof";
     private static final Duration LEDGER_ID_TIMEOUT = Duration.ofMinutes(1);
-    private static final Duration WRAPS_PROOF_TIMEOUT = Duration.ofMinutes(15);
+    private static final Duration WRAPS_PROOF_TIMEOUT = Duration.ofMinutes(20);
     private static final Duration STAKE_PERIOD_DURATION = Duration.ofMinutes(16);
     private static final Duration LOG_POLL_INTERVAL = Duration.ofSeconds(1);
     private static final long TRANSFER_PACING_MS = 250L;
@@ -77,7 +78,7 @@ public class WrapsHandoffsTest implements LifecycleTest {
                                 () -> new SpecOperation[] {randomStakerTransfer(), sleepFor(TRANSFER_PACING_MS)},
                                 this::assertAllGetInfoResponsesIncludeExternalizedLedgerId),
                         untilHgcaaLogContainsText(
-                                        byNodeId(0),
+                                        allNodes(),
                                         GENESIS_WRAPS_PROOF_CONSTRUCTED,
                                         WRAPS_PROOF_TIMEOUT,
                                         LOG_POLL_INTERVAL,
@@ -93,7 +94,7 @@ public class WrapsHandoffsTest implements LifecycleTest {
                                         })
                                 .loggingOff(),
                         untilHgcaaLogContainsText(
-                                        byNodeId(0),
+                                        allNodes(),
                                         INCREMENTAL_WRAPS_PROOF_CONSTRUCTED,
                                         WRAPS_PROOF_TIMEOUT.plus(WRAPS_PROOF_TIMEOUT),
                                         LOG_POLL_INTERVAL,
