537d70584c88c9f2a18488cdbe24fd5e4bd4b12c
aderevets <artem.derevets@limechain.tech>
2026-07-20 17:26:58 +0300

    refactor(fees): delete legacy usage/pricing/calc fee packages
    Signed-off-by: aderevets <artem.derevets@limechain.tech>

--- hedera-node/hapi-fees/src/main/java/com/hedera/node/app/hapi/fees/usage/token/meta/TokenUnfreezeMeta.java
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-package com.hedera.node.app.hapi.fees.usage.token.meta;
-
-/** This is simply to get rid of code duplication with {@link TokenFreezeMeta} class. */
-public class TokenUnfreezeMeta extends TokenFreezeMeta {
-    public TokenUnfreezeMeta(final int bpt) {
-        super(bpt);
-    }
-}
