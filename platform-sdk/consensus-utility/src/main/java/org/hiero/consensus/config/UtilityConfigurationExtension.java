049e7b6e54b4cd2f1db5290bff73370c67b17734
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-07-24 11:41:27 +0200

    Fix unit tests
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/config/UtilityConfigurationExtension.java
+++ platform-sdk/consensus-utility/src/main/java/org/hiero/consensus/config/UtilityConfigurationExtension.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.consensus.config;
 
+import com.swirlds.component.framework.schedulers.builders.TaskSchedulerConfiguration;
 import com.swirlds.config.api.ConfigurationExtension;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.Set;
@@ -14,6 +15,7 @@ public class UtilityConfigurationExtension implements ConfigurationExtension {
     /**
      * {@inheritDoc}
      */
+    @Override
     @NonNull
     public Set<Class<? extends Record>> getConfigDataTypes() {
         return Set.of(
@@ -22,5 +24,14 @@ public class UtilityConfigurationExtension implements ConfigurationExtension {
                 RecycleBinConfig.class,
                 StatusMonitorWiringConfig.class,
                 UptimeConfig.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NonNull
+    public Set<ConverterPair<?>> getConverters() {
+        return Set.of(new ConverterPair<>(TaskSchedulerConfiguration.class, TaskSchedulerConfiguration::parse));
     }
 }
