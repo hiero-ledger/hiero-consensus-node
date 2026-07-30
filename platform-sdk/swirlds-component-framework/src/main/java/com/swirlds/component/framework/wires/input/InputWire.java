44ee2b6c677a04035887d220aadc7b34e967444c
Michael Heinrichs <netopyr@users.noreply.github.com>
2026-07-30 10:45:31 +0200

    Turn InputWire and OutputWire to interfaces plus some simplification.
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/swirlds-component-framework/src/main/java/com/swirlds/component/framework/wires/input/InputWire.java
+++ platform-sdk/swirlds-component-framework/src/main/java/com/swirlds/component/framework/wires/input/InputWire.java
@@ -4,34 +4,13 @@ package com.swirlds.component.framework.wires.input;
 import com.swirlds.component.framework.schedulers.TaskScheduler;
 import com.swirlds.component.framework.schedulers.builders.TaskSchedulerType;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.Objects;
-import java.util.function.Consumer;
 
 /**
  * An object that can insert work to be handled by a {@link TaskScheduler}.
  *
  * @param <IN> the type of data that passes into the wire
  */
-public abstract class InputWire<IN> {
-
-    private final TaskSchedulerInput<?> taskSchedulerInput;
-    private Consumer<Object> handler;
-    private final String name;
-    private final String taskSchedulerName;
-    private final TaskSchedulerType taskSchedulerType;
-
-    /**
-     * Constructor.
-     *
-     * @param taskScheduler the scheduler to insert data into
-     * @param name          the name of the input wire
-     */
-    protected InputWire(@NonNull final TaskScheduler<?> taskScheduler, @NonNull final String name) {
-        this.taskSchedulerInput = Objects.requireNonNull(taskScheduler);
-        this.name = Objects.requireNonNull(name);
-        this.taskSchedulerName = taskScheduler.getName();
-        this.taskSchedulerType = taskScheduler.getType();
-    }
+public interface InputWire<IN> {
 
     /**
      * Get the name of this input wire.
@@ -39,9 +18,7 @@ public abstract class InputWire<IN> {
      * @return the name of this input wire
      */
     @NonNull
-    public String getName() {
-        return name;
-    }
+    String getName();
 
     /**
      * Get the name of the task scheduler this input channel is bound to.
@@ -49,9 +26,7 @@ public abstract class InputWire<IN> {
      * @return the name of the wire this input channel is bound to
      */
     @NonNull
-    public String getTaskSchedulerName() {
-        return taskSchedulerName;
-    }
+    String getTaskSchedulerName();
 
     /**
      * Get the type of the task scheduler that this input channel is bound to.
@@ -59,18 +34,14 @@ public abstract class InputWire<IN> {
      * @return the type of the task scheduler that this input channel is bound to
      */
     @NonNull
-    public TaskSchedulerType getTaskSchedulerType() {
-        return taskSchedulerType;
-    }
+    TaskSchedulerType getTaskSchedulerType();
 
     /**
      * Add a task to the task scheduler. May block if back pressure is enabled.
      *
      * @param data the data to be processed by the task scheduler
      */
-    public void put(@NonNull final IN data) {
-        taskSchedulerInput.put(handler, data);
-    }
+    void put(@NonNull final IN data);
 
     /**
      * Add a task to the task scheduler. If backpressure is enabled and there is not immediately capacity available,
@@ -79,9 +50,7 @@ public abstract class InputWire<IN> {
      * @param data the data to be processed by the task scheduler
      * @return true if the data was accepted, false otherwise
      */
-    public boolean offer(@NonNull final IN data) {
-        return taskSchedulerInput.offer(handler, data);
-    }
+    boolean offer(@NonNull final IN data);
 
     /**
      * Inject data into the task scheduler, doing so even if it causes the number of unprocessed tasks to exceed the
@@ -90,19 +59,5 @@ public abstract class InputWire<IN> {
      *
      * @param data the data to be processed by the task scheduler
      */
-    public void inject(@NonNull final IN data) {
-        taskSchedulerInput.inject(handler, data);
-    }
-
-    /**
-     * Set the method that will handle data traveling over this wire.
-     *
-     * @param handler the method that will handle data traveling over this wire
-     */
-    protected void setHandler(@NonNull final Consumer<Object> handler) {
-        if (this.handler != null) {
-            throw new IllegalStateException("Handler already bound");
-        }
-        this.handler = Objects.requireNonNull(handler);
-    }
+    void inject(@NonNull final IN data);
 }
