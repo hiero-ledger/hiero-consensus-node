83da19c3635e2e3ed288f8c34918bc2818b3c6ee
Josh Marinacci <joshua@marinacci.org>
2026-08-04 07:56:39 -0700

    remove unneeded lines
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/crypto/FractionalFeeAllDebitsExemptTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/crypto/FractionalFeeAllDebitsExemptTest.java
@@ -75,8 +75,6 @@ public class FractionalFeeAllDebitsExemptTest {
                         // FeeB: collectorB collects; all collectors on this token are exempt
                         .withCustom(fractionalFee(1L, 10L, 0L, OptionalLong.empty(), COLLECTOR_B, true))
                         .signedBy(GENESIS, "keyA", "keyB"),
-                //                tokenAssociate(COLLECTOR_A, TOKEN),
-                //                tokenAssociate(COLLECTOR_B, TOKEN),
                 tokenAssociate(RECEIVER, TOKEN),
                 // Fund collectorA via treasury (treasury is skipped by the assessment loop entirely)
                 cryptoTransfer(moving(500L, TOKEN).between(TREASURY, COLLECTOR_A))
@@ -92,6 +90,5 @@ public class FractionalFeeAllDebitsExemptTest {
                         .payingWith(COLLECTOR_A)
                         .signedBy(GENESIS, "keyA")
                         .hasKnownStatus(ResponseCodeEnum.SUCCESS));
-        //                        .hasKnownStatus(ResponseCodeEnum.FAIL_INVALID));
     }
 }
