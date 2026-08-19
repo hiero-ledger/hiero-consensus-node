ad324f160d97636e22ec485055bbf074e4f5b5bb
Artur Kugal <artur.kugal@swirldslabs.com>
2026-08-19 13:35:15 -0400

    Exclude platform code changes
    Signed-off-by: Artur Kugal <artur.kugal@swirldslabs.com>

--- platform-sdk/swirlds-config-processor/src/test/java/com/swirlds/config/processor/DocumentationFactoryTest.java
+++ platform-sdk/swirlds-config-processor/src/test/java/com/swirlds/config/processor/DocumentationFactoryTest.java
@@ -5,6 +5,7 @@ import com.swirlds.config.processor.antlr.AntlrConfigRecordParser;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -17,12 +18,14 @@ public class DocumentationFactoryTest {
     private final String RECORD_DEFINITION =
             MarkdownSyntax.RECORD + MarkdownSyntax.asCode("com.swirlds.config.processor.TestConfig");
 
+    private final String JAVA_FILE = "TestConfig.java";
+
     @Test
     void testDocumentationCreation() throws Exception {
         // given
         final String docFileName = "test-config.md";
         final String javaFilePath =
-                DocumentationFactoryTest.class.getResource("TestConfig.java").getPath();
+                DocumentationFactoryTest.class.getResource(JAVA_FILE).getPath();
         final String content = Files.readString(Path.of(javaFilePath));
         final ConfigDataRecordDefinition definition =
                 AntlrConfigRecordParser.parse(content).get(0);
@@ -35,7 +38,7 @@ public class DocumentationFactoryTest {
         Assertions.assertTrue(Files.exists(docFilePath));
         final List<String> lines = Files.readAllLines(docFilePath);
         final List<String> nonEmptyLines =
-                lines.stream().filter(l -> !l.isEmpty()).toList();
+                lines.stream().filter(l -> !l.isEmpty()).collect(Collectors.toList());
         Assertions.assertEquals(MarkdownSyntax.H2_PREFIX + "test.saveStatePeriod", nonEmptyLines.get(0));
         Assertions.assertEquals(RECORD_DEFINITION, nonEmptyLines.get(1));
         Assertions.assertEquals(MarkdownSyntax.TYPE + MarkdownSyntax.asCode("int"), nonEmptyLines.get(2));
