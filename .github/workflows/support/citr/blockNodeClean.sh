d92e783b2477f71cd46b283392f2082cba803aec
Andrew Brandt <andrew.brandt@hashgraph.com>
2026-09-02 16:10:02 -0400

    updates to blockNodeClean script
    Signed-off-by: Andrew Brandt <andrew.brandt@hashgraph.com>

--- .github/workflows/support/citr/blockNodeClean.sh
+++ .github/workflows/support/citr/blockNodeClean.sh
@@ -4,17 +4,17 @@ NAMESPACE=$1
 MODE=${2}
 LIVEDATA_ROOT=/opt/hiero/block-node/data/live
 
-while [ true ]
+while 'true'
 do
 
   for pod in `kubectl -n ${NAMESPACE} get pods | grep 'block-node' | awk '{print $1}'`
   do
-    kubectl -n ${NAMESPACE} exec ${pod} -- bash -c "find $LIVEDATA_ROOT/ -type f -name '*.blk*' -mmin +59 -exec rm -f {} \;" >/dev/null 2>&1 &
+    kubectl -n ${NAMESPACE} exec ${pod} -- bash -c "find ${LIVEDATA_ROOT}/ -type f -name '*.blk*' -mmin +59 -exec rm -f {} \;" >/dev/null 2>&1 &
   done
 
   wait
 
-  if [ "${MODE}" = "once" ]
+  if [[ "${MODE}" == "once" ]]
   then
     break
   fi
